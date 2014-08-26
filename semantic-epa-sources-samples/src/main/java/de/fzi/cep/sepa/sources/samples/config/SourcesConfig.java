ccc4b8ab04cbb8424b31371524d0595741c2b77a
Dominik Riemer <riemer@fzi.de>
2014-08-26 09:24:11 +0000

    added event replay base URL
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@297 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-sources-samples/src/main/java/de/fzi/cep/sepa/sources/samples/config/SourcesConfig.java
+++ semantic-epa-sources-samples/src/main/java/de/fzi/cep/sepa/sources/samples/config/SourcesConfig.java
@@ -4,9 +4,11 @@ public class SourcesConfig {
 
 	public final static String serverUrl;
 	public final static String iconBaseUrl;
+	public final static String eventReplayURI;
 	
 	static {
 		serverUrl = "http://localhost:8089";
 		iconBaseUrl = "http://localhost:8080/semantic-epa-backend/img";
+		eventReplayURI = "http://89.215.116.44:8084";
 	}
 }
