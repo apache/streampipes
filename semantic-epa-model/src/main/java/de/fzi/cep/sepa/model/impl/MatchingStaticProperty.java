b09be4c99d0fb21cc4f218b6c58a2f658d8974bc
Dominik Riemer <riemer@fzi.de>
2014-09-01 21:23:02 +0000

    support for matching static properties
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@322 aa18e314-994e-4353-9a36-64d395711e41

---
+++ semantic-epa-model/src/main/java/de/fzi/cep/sepa/model/impl/MatchingStaticProperty.java
@@ -0,0 +1,56 @@
+package de.fzi.cep.sepa.model.impl;
+
+import java.net.URI;
+
+import javax.persistence.Entity;
+
+import com.clarkparsia.empire.annotation.Namespaces;
+import com.clarkparsia.empire.annotation.RdfProperty;
+import com.clarkparsia.empire.annotation.RdfsClass;
+
+@Namespaces({"sepa", "http://sepa.event-processing.org/sepa#",
+	 "dc",   "http://purl.org/dc/terms/"})
+@RdfsClass("sepa:MatchingStaticProperty")
+@Entity
+public class MatchingStaticProperty extends StaticProperty{
+
+	@RdfProperty("sepa:matchLeft")
+	URI matchLeft;
+	
+	@RdfProperty("sepa:matchRight")
+	URI matchRight;
+
+	public MatchingStaticProperty()
+	{
+		super();
+	}
+	
+	public MatchingStaticProperty(String name, String description)
+	{
+		super(name, description);
+	}
+	
+	public MatchingStaticProperty(String name, String description, URI matchLeft, URI matchRight) {
+		super(name, description);
+		this.matchLeft = matchLeft;
+		this.matchRight = matchRight;
+	}
+
+	public URI getMatchLeft() {
+		return matchLeft;
+	}
+
+	public void setMatchLeft(URI matchLeft) {
+		this.matchLeft = matchLeft;
+	}
+
+	public URI getMatchRight() {
+		return matchRight;
+	}
+
+	public void setMatchRight(URI matchRight) {
+		this.matchRight = matchRight;
+	}
+	
+	
+}
