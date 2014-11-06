5f3fff878eed8c3f0219340ae045652fcb7e4539
Dominik Riemer <riemer@fzi.de>
2014-11-06 17:17:04 +0000

    added gsonBuilder from semantic-epa-rest
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@430 aa18e314-994e-4353-9a36-64d395711e41

---
+++ semantic-epa-backend/semantic-epa-model-client/src/main/java/de/fzi/sepa/model/client/util/Utils.java
@@ -0,0 +1,26 @@
+package de.fzi.sepa.model.client.util;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import de.fzi.cep.sepa.model.client.input.FormInput;
+
+
+
+public class Utils {
+
+	public static Gson getGson()
+	{
+		GsonBuilder gsonBuilder = getGsonBuilder();
+		Gson gson = gsonBuilder.create();
+		return gson;
+	}
+	
+	public static GsonBuilder getGsonBuilder()
+	{
+		GsonBuilder gsonBuilder = new com.google.gson.GsonBuilder();
+		gsonBuilder.registerTypeAdapter(FormInput.class, new FormInputSerializer());
+		return gsonBuilder;	
+	}
+	
+}
