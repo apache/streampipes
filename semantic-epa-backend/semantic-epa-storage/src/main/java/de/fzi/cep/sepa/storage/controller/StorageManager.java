dfc6725405642e38fc6ed1328fd85ad31f6e9ecb
Dominik Riemer <riemer@fzi.de>
2015-01-13 13:10:15 +0000

    updated storage manager
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@494 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-backend/semantic-epa-storage/src/main/java/de/fzi/cep/sepa/storage/controller/StorageManager.java
+++ semantic-epa-backend/semantic-epa-storage/src/main/java/de/fzi/cep/sepa/storage/controller/StorageManager.java
@@ -27,7 +27,7 @@ public enum StorageManager {
 	INSTANCE;
 
 	private String SERVER = "http://localhost:8080/openrdf-sesame";
-	private String REPOSITORY_ID = "test-5";
+	private String REPOSITORY_ID = "test-6";
 	private String TEMP_REPOSITORY_ID = "temp-db";
 	
 	private EntityManager storageManager;
