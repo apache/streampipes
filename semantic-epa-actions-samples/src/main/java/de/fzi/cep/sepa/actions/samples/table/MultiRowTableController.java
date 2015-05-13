2098c64367f3c7a492513544ab36e43b5cb2c116
Dominik Riemer <riemer@fzi.de>
2015-05-13 22:29:59 +0000

    minor modifications
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@641 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-actions-samples/src/main/java/de/fzi/cep/sepa/actions/samples/table/MultiRowTableController.java
+++ semantic-epa-actions-samples/src/main/java/de/fzi/cep/sepa/actions/samples/table/MultiRowTableController.java
@@ -25,7 +25,7 @@ public class MultiRowTableController extends ActionController {
 	@Override
 	public SEC declareModel() {
 		SEC sec = new SEC("/table/multirow", "Multi-Row Table", "", "");
-		sec.setIconUrl(ActionConfig.iconBaseUrl + "/Map_Icon_HQ.png");
+		sec.setIconUrl(ActionConfig.iconBaseUrl + "/Table_Icon_HQ.png");
 		List<String> domains = new ArrayList<String>();
 		domains.add(Domain.DOMAIN_PERSONAL_ASSISTANT.toString());
 		domains.add(Domain.DOMAIN_PROASENSE.toString());
