c30b9f1c4e187641ffda19867cb39f1e464354e5
Dominik Riemer <riemer@fzi.de>
2015-05-21 08:29:40 +0000

    test fisheye integration
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@652 aa18e314-994e-4353-9a36-64d395711e41

---
+++ semantic-epa-model/src/main/java/de/fzi/cep/sepa/model/vocabulary/MhWirth.java
@@ -0,0 +1,8 @@
+package de.fzi.cep.sepa.model.vocabulary;
+
+public class MhWirth {
+	public static final String Rpm = "http://mhwirth.com/drillingRpm";
+	public static final String OilTemperature = "http://mhwirth.com/oilTemperature";
+	public static final String DrillingStatus = "http://mhwirth.com/drillingStatus";
+	public static final String Torque = "http://mhwirth.com/torqueLoad";
+}
