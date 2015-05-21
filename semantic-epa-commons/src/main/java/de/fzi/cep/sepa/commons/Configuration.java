a8c298c67c01581605e586b8732a041d77a90b78
Dominik Riemer <riemer@fzi.de>
2015-05-21 07:53:30 +0000

    removed unnecessary items from configuration
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@649 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-commons/src/main/java/de/fzi/cep/sepa/commons/Configuration.java
+++ semantic-epa-commons/src/main/java/de/fzi/cep/sepa/commons/Configuration.java
@@ -37,15 +37,6 @@ public class Configuration {
 	
 	public static final String CONTEXT_PATH = "/semantic-epa-backend";
 	
-	//public static final String EMPIRE_CONFIG_LOCATION = "/home/riemer/empire.config.properties";
-
-	public static final String EMPIRE_CONFIG_LOCATION = "c:\\workspace\\semantic-epa-parent\\semantic-epa-backend\\semantic-epa-storage\\src\\main\\resources\\empire.config.properties";
-
-	//public static final String EMPIRE_CONFIG_LOCATION = "/home/robin/FZI/CEP/semantic-epa-parent/semantic-epa-backend/semantic-epa-storage/src/main/resources/empire.config.properties";
-
-	
-	//public static final String EMPIRE_CONFIG_LOCATION = "e:\\Workspace Eclipse Luna\\semantic-epa-parent\\semantic-epa-backend\\semantic-epa-storage\\src\\main\\resources\\empire.config.properties";
-
 	public static final RDFFormat RDF_FORMAT = RDFFormat.JSONLD;
 	
 	public static RDFWriter getRioWriter(OutputStream stream) throws RDFHandlerException
