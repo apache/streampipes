f832a5cf0b2e3e18199226f103c383b3d1fcdd7a
Kaulfers <kaulfers@fzi.de>
2015-08-05 13:53:31 +0000

    Initial Commit
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@837 aa18e314-994e-4353-9a36-64d395711e41

---
+++ semantic-epa-backend/semantic-epa-server/src/main/StreamPipes_redesign/js/app.js
@@ -0,0 +1,30 @@
+'use strict';
+
+angular
+    .module('test', ['ngMaterial', 'ngMdIcons'])
+    .controller('AppCtrl', function ($scope, $timeout, $mdSidenav, $mdUtil, $log) {
+        $scope.toggleLeft = buildToggler('left');
+        $scope.toggleRight = buildToggler('right');
+        /**
+         * Build handler to open/close a SideNav; when animation finishes
+         * report completion in console
+         */
+        function buildToggler(navID) {
+            var debounceFn =  $mdUtil.debounce(function(){
+                $mdSidenav(navID)
+                    .toggle()
+                    .then(function () {
+                        $log.debug("toggle " + navID + " is done");
+                    });
+            },300);
+            return debounceFn;
+        }
+    })
+    .controller('LeftCtrl', function ($scope, $timeout, $mdSidenav, $log) {
+        $scope.close = function () {
+            $mdSidenav('left').close()
+                .then(function () {
+                    $log.debug("close LEFT is done");
+                });
+        };
+    });
