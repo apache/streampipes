ddd081cb1d1ca6e87d465e42cad8f44c954f4f33
Philipp Zehnder <zehnder@fzi.de>
2015-08-12 09:17:52 +0000

    Sources-Samples now waits for the startup of ActiveMQ
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@845 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-sources-samples/src/main/java/de/fzi/cep/sepa/sources/samples/util/ActiveMQPublisher.java
+++ semantic-epa-sources-samples/src/main/java/de/fzi/cep/sepa/sources/samples/util/ActiveMQPublisher.java
@@ -1,5 +1,5 @@
 package de.fzi.cep.sepa.sources.samples.util;
- 
+
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
 import javax.jms.JMSException;
@@ -7,47 +7,54 @@ import javax.jms.MessageProducer;
 import javax.jms.Session;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
-  
-public class ActiveMQPublisher implements AutoCloseable {
-	
-		private final String brokerUrl = "tcp://localhost:61616";
- 
-        private final Session session;
-        private final MessageProducer producer;
- 
-        public ActiveMQPublisher(String topicName) {
-                try {
-                		Connection connection = startJmsConnection();
-                        session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-                        producer = session.createProducer(session.createTopic(topicName));
-                } catch (JMSException e) {
-                        throw new AssertionError(e);
-                }
-        }
-   
-        public void send(String json) {
-                try {
-                        producer.send(session.createTextMessage(json));
-                } catch (JMSException e) {
-                        throw new IllegalStateException(e);
-                }
-        }
- 
-        @Override
-        public void close() throws JMSException {
-                producer.close();
-                session.close();
-        }
-        
 
-        private Connection startJmsConnection() {
-                try {
-                        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
-                        Connection connection = connectionFactory.createConnection();
-                        connection.start();
-                        return connection;
-                } catch (JMSException e) {
-                        throw new AssertionError("Failed to establish the JMS-Connection!", e);
-                }
-        }
+public class ActiveMQPublisher implements AutoCloseable {
+
+	private final String brokerUrl = "tcp://localhost:61616";
+
+	private final Session session;
+	private final MessageProducer producer;
+
+	public ActiveMQPublisher(String topicName) {
+		try {
+			Connection connection = startJmsConnection();
+			session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+			producer = session.createProducer(session.createTopic(topicName));
+		} catch (JMSException e) {
+			throw new AssertionError(e);
+		}
+	}
+
+	public void send(String json) {
+		try {
+			producer.send(session.createTextMessage(json));
+		} catch (JMSException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+
+	@Override
+	public void close() throws JMSException {
+		producer.close();
+		session.close();
+	}
+
+	private Connection startJmsConnection() {
+		boolean connect = false;
+		ConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+		Connection connection = null;
+
+		// TODO find a better solution
+		do {
+			try {
+				connection = connectionFactory.createConnection();
+				connection.start();
+				connect = true;
+			} catch (JMSException e) {
+				System.out.println("Trying to connect to ActiveMQ");
+			}
+
+		} while (!connect);
+		return connection;
+	}
 }
