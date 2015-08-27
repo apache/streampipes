777257ad8deaa6a0e461de08ced0ad4baae4449e
Dominik Riemer <riemer@fzi.de>
2015-08-27 18:32:09 +0000

    modified model descriptions, removed domain specification
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@885 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-backend/semantic-epa-model-client/src/main/java/de/fzi/cep/sepa/model/client/SEPAClient.java
+++ semantic-epa-backend/semantic-epa-model-client/src/main/java/de/fzi/cep/sepa/model/client/SEPAClient.java
@@ -1,42 +1,20 @@
 package de.fzi.cep.sepa.model.client;
 
-import java.util.List;
-
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.OneToMany;
 
 @Entity
 public class SEPAClient extends ConsumableSEPAElement {
 	
-	@OneToMany(cascade=CascadeType.ALL)
-	private List<String> domains;
-	
 	private int inputNodes;
 	
-	
-	//private List<OutputStrategy<? extends OutputStrategyParameters>> outputStragegy;
-	
-	public SEPAClient(String name, String description, List<String> domains)
+	public SEPAClient(String name, String description)
 	{
 		super(name, description);
-		this.domains = domains;
 	}
 	
-	public SEPAClient(String name, String description, List<String> domains, String iconName)
+	public SEPAClient(String name, String description, String iconName)
 	{
 		super(name, description, iconName);
-		this.domains = domains;
-	}
-
-
-	public List<String> getDomains() {
-		return domains;
-	}
-
-
-	public void setDomains(List<String> domains) {
-		this.domains = domains;
 	}
 
 	public int getInputNodes() {
@@ -46,17 +24,5 @@ public class SEPAClient extends ConsumableSEPAElement {
 	public void setInputNodes(int inputNodes) {
 		this.inputNodes = inputNodes;
 	}
-
-
-
-	/*
-	public OutputStrategy getOutputStragegy() {
-		return outputStragegy;
-	}
-
-	public void setOutputStragegy(OutputStrategy outputStragegy) {
-		this.outputStragegy = outputStragegy;
-	}
-*/
 
 }
--- semantic-epa-backend/semantic-epa-model-client/src/main/java/de/fzi/cep/sepa/model/client/SourceClient.java
+++ semantic-epa-backend/semantic-epa-model-client/src/main/java/de/fzi/cep/sepa/model/client/SourceClient.java
@@ -1,30 +1,15 @@
 package de.fzi.cep.sepa.model.client;
 
-import java.util.List;
-
 public class SourceClient extends SEPAElement {
 
-	private List<String> domain;
-
-	public SourceClient(String name, String description, List<String> domain)
+	public SourceClient(String name, String description)
 	{
 		super(name, description);
-		this.domain = domain;
 	}
 	
-	public SourceClient(String name, String description, List<String> domain, String iconName)
+	public SourceClient(String name, String description, String iconName)
 	{
 		super(name, description, iconName);
-		this.domain = domain;
 	}
-
-	public List<String> getDomain() {
-		return domain;
-	}
-
-	public void setDomain(List<String> domain) {
-		this.domain = domain;
-	}
-	
 	
 }
