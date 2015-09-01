726434f92dc144b8186088132f7bb4697e7010f9
Dominik Riemer <riemer@fzi.de>
2015-09-01 16:34:32 +0000

    bug fix
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@970 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-actions-samples/src/test/java/de/fzi/cep/sepa/actions/test/TestKafkaProducer.java
+++ semantic-epa-actions-samples/src/test/java/de/fzi/cep/sepa/actions/test/TestKafkaProducer.java
@@ -7,7 +7,7 @@ public class TestKafkaProducer {
 
 	public static void main(String[] args)
 	{
-		ProaSenseInternalProducer producer = new ProaSenseInternalProducer(Configuration.getBrokerConfig().getKafkaUrl(), "eu.proasense.internal.sp.internal.outgoing.10000");
+		ProaSenseInternalProducer producer = new ProaSenseInternalProducer(Configuration.getInstance().getBrokerConfig().getKafkaUrl(), "eu.proasense.internal.sp.internal.outgoing.10000");
 		producer.send("abc".getBytes());
 		producer.shutdown();
 	}
