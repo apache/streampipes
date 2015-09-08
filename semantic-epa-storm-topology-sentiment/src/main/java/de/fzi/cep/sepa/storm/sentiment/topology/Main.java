4f1c3a650f12c92d0c75a5a02c82c6065502f7f1
Philipp Zehnder <zehnder@fzi.de>
2015-09-08 09:13:29 +0000

    Changed Port of Zookeeper
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1044 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-storm-topology-sentiment/src/main/java/de/fzi/cep/sepa/storm/sentiment/topology/Main.java
+++ semantic-epa-storm-topology-sentiment/src/main/java/de/fzi/cep/sepa/storm/sentiment/topology/Main.java
@@ -10,23 +10,26 @@ import de.fzi.cep.sepa.storm.topology.SepaTopologyBuilder;
 
 public class Main {
 
-	public static void main(String[] args) throws AlreadyAliveException, InvalidTopologyException
-	{
-		StormTopology sentimentDetectionTopology = SepaTopologyBuilder.buildSimpleTopology(new SentimentDetectionBolt("sentiment"), "ipe-koi04.fzi.de:2181");
-		
+	public static void main(String[] args) throws AlreadyAliveException, InvalidTopologyException {
+		String nimbus_host = "ipe-koi05.fzi.de";
+		String storm_zookeeper_host_port = "ipe-koi05.fzi.de:49181";
+
+		StormTopology sentimentDetectionTopology = SepaTopologyBuilder
+				.buildSimpleTopology(new SentimentDetectionBolt("sentiment"), storm_zookeeper_host_port);
+
 		// Develop settings
-//		Config conf = new Config();
-//		conf.setDebug(true);
-//		LocalCluster cluster = new LocalCluster();
-//		cluster.submitTopology("sentiment-detection", conf, sentimentDetectionTopology);
-		
-//	 Production settings
+		// Config conf = new Config();
+		// conf.setDebug(true);
+		// LocalCluster cluster = new LocalCluster();
+		// cluster.submitTopology("sentiment-detection", conf,
+		// sentimentDetectionTopology);
+
+		// Production settings
 		Config productionConfig = new Config();
-		productionConfig.setDebug(true);
-		productionConfig.put(Config.NIMBUS_HOST, "ipe-koi04.fzi.de");
-		productionConfig.put(Config.NIMBUS_THRIFT_PORT,49627);
-//		System.setProperty("storm.jar", "/home/philipp/Downloads/apache-storm-0.9.5/bin/storm"); 
+		productionConfig.put(Config.NIMBUS_HOST, nimbus_host);
+		productionConfig.put(Config.NIMBUS_THRIFT_PORT, 49627);
+		System.setProperty("storm.jar", "/home/philipp/Downloads/apache-storm-0.9.5/bin/storm");
 		StormSubmitter.submitTopology("sentiment-detection", productionConfig, sentimentDetectionTopology);
-		
+
 	}
 }
