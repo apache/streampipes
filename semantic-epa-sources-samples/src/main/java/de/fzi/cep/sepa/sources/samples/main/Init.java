9609f9ba40eeccc8dc0fd476f2c58b03ad7a9c62
Dominik Riemer <riemer@fzi.de>
2015-10-15 16:05:49 +0000

    sample sources can be activated in client configuration file
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1227 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-sources-samples/src/main/java/de/fzi/cep/sepa/sources/samples/main/Init.java
+++ semantic-epa-sources-samples/src/main/java/de/fzi/cep/sepa/sources/samples/main/Init.java
@@ -3,6 +3,7 @@ package de.fzi.cep.sepa.sources.samples.main;
 import java.util.ArrayList;
 import java.util.List;
 
+import de.fzi.cep.sepa.commons.config.ClientConfiguration;
 import de.fzi.cep.sepa.desc.ModelSubmitter;
 import de.fzi.cep.sepa.desc.declarer.SemanticEventProducerDeclarer;
 import de.fzi.cep.sepa.sources.samples.ddm.DDMProducer;
@@ -11,7 +12,6 @@ import de.fzi.cep.sepa.sources.samples.enriched.EnrichedEventProducer;
 import de.fzi.cep.sepa.sources.samples.hella.MontracProducer;
 import de.fzi.cep.sepa.sources.samples.hella.MouldingMachineProducer;
 import de.fzi.cep.sepa.sources.samples.hella.VisualInspectionProducer;
-import de.fzi.cep.sepa.sources.samples.mobile.MobileStreamProducer;
 import de.fzi.cep.sepa.sources.samples.proveit.ProveITEventProducer;
 import de.fzi.cep.sepa.sources.samples.ram.RamProducer;
 import de.fzi.cep.sepa.sources.samples.random.RandomDataProducer;
@@ -29,19 +29,27 @@ public class Init  {
 	public void declare() {
 		List<SemanticEventProducerDeclarer> declarers = new ArrayList<SemanticEventProducerDeclarer>();
 
-		declarers.add(new TwitterStreamProducer());
-//		declarers.add(new DDMProducer());
-//		declarers.add(new DrillBitProducer());
-//		declarers.add(new EnrichedEventProducer());
-//		declarers.add(new RamProducer());
-//		declarers.add(new MobileStreamProducer());
-		declarers.add(new RandomDataProducer());
-		declarers.add(new NYCTaxiProducer());
-//		declarers.add(new ProveITEventProducer());	
-		declarers.add(new VisualInspectionProducer());
-		declarers.add(new MontracProducer());
-		declarers.add(new MouldingMachineProducer());
+		ClientConfiguration config = ClientConfiguration.INSTANCE;
+		
+		if (config.isTwitterActive()) declarers.add(new TwitterStreamProducer());
+		if (config.isMhwirthReplayActive())
+		{
+			declarers.add(new DDMProducer());
+			declarers.add(new DrillBitProducer());
+			declarers.add(new EnrichedEventProducer());
+			declarers.add(new RamProducer());
+		}
+		if (config.isRandomNumberActive()) declarers.add(new RandomDataProducer());
+		if (config.isTaxiActive()) declarers.add(new NYCTaxiProducer());
+		if (config.isProveItActive()) declarers.add(new ProveITEventProducer());	
+		if (config.isHellaReplayActive())
+		{
+			declarers.add(new VisualInspectionProducer());
+			declarers.add(new MontracProducer());
+			declarers.add(new MouldingMachineProducer());
 
+		}
+		
 		try {
 			ModelSubmitter.submitProducer(declarers);
 		} catch (Exception e) {
