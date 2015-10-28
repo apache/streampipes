24f7092631a9a50b2767f3933b453442d1425067
Dominik Riemer <riemer@fzi.de>
2015-10-28 15:07:12 +0000

    modified virtual sensor model
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1273 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-backend/semantic-epa-model-client/src/main/java/de/fzi/cep/sepa/model/client/VirtualSensor.java
+++ semantic-epa-backend/semantic-epa-model-client/src/main/java/de/fzi/cep/sepa/model/client/VirtualSensor.java
@@ -2,4 +2,20 @@ package de.fzi.cep.sepa.model.client;
 
 public class VirtualSensor extends ElementComposition {
 
+	protected String createdBy;
+	
+	public VirtualSensor()
+	{
+		super();
+	}
+
+	public String getCreatedBy() {
+		return createdBy;
+	}
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
+	}
+	
+	
 }
