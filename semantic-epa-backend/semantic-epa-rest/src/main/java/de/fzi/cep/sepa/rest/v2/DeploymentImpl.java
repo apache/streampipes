ade70566baa6c79507f71a12301ee7fb222e5297
Dominik Riemer <riemer@fzi.de>
2015-10-29 09:44:21 +0000

    added rest api to receive jsonld-model, modified deployment api
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1286 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-backend/semantic-epa-rest/src/main/java/de/fzi/cep/sepa/rest/v2/DeploymentImpl.java
+++ semantic-epa-backend/semantic-epa-rest/src/main/java/de/fzi/cep/sepa/rest/v2/DeploymentImpl.java
@@ -2,26 +2,37 @@ package de.fzi.cep.sepa.rest.v2;
 
 import java.io.File;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-import de.fzi.cep.sepa.manager.generation.ArchetypeGenerator;
+import com.sun.jersey.multipart.FormDataParam;
+
+import de.fzi.cep.sepa.manager.generation.ArchetypeManager;
+import de.fzi.cep.sepa.model.client.deployment.DeploymentConfiguration;
+import de.fzi.cep.sepa.model.impl.graph.SepaDescription;
+import de.fzi.cep.sepa.model.util.GsonSerializer;
+import de.fzi.cep.sepa.rest.api.AbstractRestInterface;
 
 
 @Path("/v2/users/{username}/deploy")
-public class DeploymentImpl {
+public class DeploymentImpl extends AbstractRestInterface {
 
-	@GET
+	@POST
 	@Path("/storm")
 	@Produces("application/zip")
-	public Response getFile() {
+	@Consumes(MediaType.MULTIPART_FORM_DATA)
+	public Response getFile(@FormDataParam("config") String deploymentConfig, @FormDataParam("model") String model) {
 	    
-		ArchetypeGenerator gen = new ArchetypeGenerator("de.fzi.test", "test-test", "TestTest");
+		DeploymentConfiguration config = fromJson(deploymentConfig, DeploymentConfiguration.class);
 		
-		File f = gen.toZip();
+		SepaDescription sepa = GsonSerializer.getGsonWithIds().fromJson(model, SepaDescription.class);
+		
+		File f = new ArchetypeManager(config, sepa).getGeneratedFile();
 
 	    if (!f.exists()) {
 	        throw new WebApplicationException(404);
---
+++ semantic-epa-backend/semantic-epa-rest/src/main/java/de/fzi/cep/sepa/rest/v2/SepaOntologyImpl.java
@@ -0,0 +1,118 @@
+package de.fzi.cep.sepa.rest.v2;
+
+import java.net.URISyntaxException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+
+import de.fzi.cep.sepa.messages.Notifications;
+import de.fzi.cep.sepa.model.impl.graph.SecDescription;
+import de.fzi.cep.sepa.model.impl.graph.SepDescription;
+import de.fzi.cep.sepa.model.impl.graph.SepaDescription;
+import de.fzi.cep.sepa.model.util.GsonSerializer;
+import de.fzi.cep.sepa.rest.api.AbstractRestInterface;
+import de.fzi.cep.sepa.rest.api.v2.SepaOntology;
+import de.fzi.cep.sepa.storage.controller.StorageManager;
+
+@Path("/v2/ontology")
+public class SepaOntologyImpl extends AbstractRestInterface implements SepaOntology {
+
+	@Override
+	@Path("/sources")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getStreams() {
+		List<SepDescription> result = new ArrayList<>();
+		List<SepDescription> sesameSeps = StorageManager.INSTANCE.getStorageAPI().getAllSEPs();
+		
+		for(SepDescription sep : sesameSeps)
+			result.add(new SepDescription(sep));
+		return GsonSerializer.getGson().toJson(result);
+	}
+
+	@Override
+	@Path("/sepas")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getSepas() {
+		JsonArray result = new JsonArray();
+		List<SepaDescription> sesameSepas = StorageManager.INSTANCE.getStorageAPI().getAllSEPAs();
+		
+		for(SepaDescription sepa : sesameSepas)
+			result.add(getHeader(sepa.getUri(), sepa.getName()));
+		return GsonSerializer.getGson().toJson(result);
+		
+	}
+
+	@Override
+	@Path("/actions")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getActions() {
+		List<SecDescription> result = new ArrayList<>();
+		List<SecDescription> sesameSecs = StorageManager.INSTANCE.getStorageAPI().getAllSECs();
+		
+		for(SecDescription sec : sesameSecs)
+			result.add(new SecDescription(sec));
+		return GsonSerializer.getGson().toJson(result);
+	}
+
+	@Override
+	public String getStream(String streamId, @QueryParam("keepIds") boolean keepIds) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Path("/sepas/{sepaId}")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	@Override
+	public String getSepa(@PathParam("sepaId") String sepaId, @QueryParam("keepIds") boolean keepIds) {
+		
+		try {
+			SepaDescription sepaDescription = new SepaDescription(StorageManager.INSTANCE.getStorageAPI().getSEPAById(sepaId));
+			return getGson(keepIds).toJson(sepaDescription);
+		} catch (URISyntaxException e) {
+			return toJson(Notifications.error("Error"));
+		}
+	}
+
+	@Override
+	public String getAction(String actionId, @QueryParam("keepIds") boolean keepIds) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	public static void main(String[] args)
+	{
+		new SepaOntologyImpl().getSepas();
+	}
+	
+	private JsonObject getHeader(String uri, String name)
+	{
+		JsonObject jsonObj = new JsonObject();
+		jsonObj.add("uri", new JsonPrimitive(uri));
+		jsonObj.add("name", new JsonPrimitive(name));
+		
+		return jsonObj;
+	}
+	
+	private Gson getGson(boolean keepIds)
+	{
+		if (keepIds) return GsonSerializer.getGsonWithIds();
+		else return GsonSerializer.getGsonWithoutIds();
+	}
+	
+
+}
