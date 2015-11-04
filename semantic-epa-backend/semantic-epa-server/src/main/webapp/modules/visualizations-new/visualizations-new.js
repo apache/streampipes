f97584c4deaf20ddbb3b96d5870c959fb8a0f641
Dominik Riemer <riemer@fzi.de>
2015-11-04 10:28:31 +0000

    changed conflicting controller name
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1301 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-backend/semantic-epa-server/src/main/webapp/modules/visualizations-new/visualizations-new.js
+++ semantic-epa-backend/semantic-epa-server/src/main/webapp/modules/visualizations-new/visualizations-new.js
@@ -1,5 +1,5 @@
 angular.module('streamPipesApp')
-.controller('SensorCtrl', function($rootScope, $scope, $timeout, $log, $location, $http, restApi, $mdToast, $animate, $mdDialog) {
+.controller('VizCtrl', function($rootScope, $scope, $timeout, $log, $location, $http, restApi, $mdToast, $animate, $mdDialog) {
 
 	
 });
