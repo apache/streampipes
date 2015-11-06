8eba63e54583b99924538c16d6b5f25e195dbf92
Dominik Riemer <riemer@fzi.de>
2015-11-06 15:33:16 +0000

    bug fix (invocation graphs used wrong rdfid)
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1302 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-model/src/main/java/de/fzi/cep/sepa/model/impl/graph/SepaInvocation.java
+++ semantic-epa-model/src/main/java/de/fzi/cep/sepa/model/impl/graph/SepaInvocation.java
@@ -53,7 +53,7 @@ public class SepaInvocation extends InvocableSEPAElement implements Serializable
 		this.setStaticProperties(sepa.getStaticProperties());
 		this.setOutputStrategies(sepa.getOutputStrategies());
 		this.setBelongsTo(sepa.getRdfId().toString());
-		this.setUri(belongsTo +"/" +getElementId());		
+		//this.setUri(belongsTo +"/" +getElementId());		
 	}
 	
 	public SepaInvocation(SepaInvocation other)
