531bd819b082c53b9254975870c4400109a523c8
Dominik Riemer <riemer@fzi.de>
2016-01-26 21:30:19 +0000

    added timestamp enrichment for flink
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1509 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-flink-samples/src/main/java/de/fzi/cep/sepa/flink/samples/Init.java
+++ semantic-epa-flink-samples/src/main/java/de/fzi/cep/sepa/flink/samples/Init.java
@@ -5,13 +5,14 @@ import java.util.Arrays;
 import de.fzi.cep.sepa.desc.ModelSubmitter;
 import de.fzi.cep.sepa.flink.samples.classification.number.NumberClassificationController;
 import de.fzi.cep.sepa.flink.samples.elasticsearch.ElasticSearchController;
+import de.fzi.cep.sepa.flink.samples.enrich.timestamp.TimestampController;
 import de.fzi.cep.sepa.flink.samples.wordcount.WordCountController;
 
 public class Init {
 
 	public static void main(String[] args) {
 		ModelSubmitter.submitMixed(Arrays.asList(new WordCountController(), new ElasticSearchController(),
-				new NumberClassificationController()), 8094);
+				new NumberClassificationController(), new TimestampController()), 8094);
 
 	}
 }
