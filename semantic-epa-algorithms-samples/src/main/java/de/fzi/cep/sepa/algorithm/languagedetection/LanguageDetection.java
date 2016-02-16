2fdd32b3f761f3dcc7ddc4e79323101f3d6358a2
Dominik Riemer <riemer@fzi.de>
2016-02-16 21:50:23 +0000

    updated language detection
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1568 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-algorithms-samples/src/main/java/de/fzi/cep/sepa/algorithm/languagedetection/LanguageDetection.java
+++ semantic-epa-algorithms-samples/src/main/java/de/fzi/cep/sepa/algorithm/languagedetection/LanguageDetection.java
@@ -1,11 +1,20 @@
 package de.fzi.cep.sepa.algorithm.languagedetection;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import com.cybozu.labs.langdetect.Detector;
 import com.cybozu.labs.langdetect.DetectorFactory;
 import com.cybozu.labs.langdetect.LangDetectException;
+import com.google.common.base.Charsets;
+import com.google.common.io.Resources;
 
 import de.fzi.cep.sepa.model.impl.graph.SepaInvocation;
 import de.fzi.cep.sepa.runtime.EPEngine;
@@ -14,6 +23,8 @@ import de.fzi.cep.sepa.runtime.param.EngineParameters;
 
 public class LanguageDetection implements EPEngine<LanguageDetectionParameters>{
 
+	private static final String PROFILE_FOLDER = "./profiles";
+	
 	private OutputCollector collector;
 	private Map<String, String> mappingPropertyNames;
 	private String outputPropertyName;
@@ -21,13 +32,30 @@ public class LanguageDetection implements EPEngine<LanguageDetectionParameters>{
 	
 	public LanguageDetection() {
 		 try {
-			DetectorFactory.loadProfile("C:\\workspace\\semantic-epa-parent\\semantic-epa-algorithms-samples\\src\\main\\resources\\profiles");
+			DetectorFactory.loadProfile(getProfiles());
 			mappingPropertyNames = new HashMap<>();
-		} catch (LangDetectException e) {
-			// TODO Auto-generated catch block
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
+	
+	private List<String> getProfiles() throws IOException {
+		
+		List<String> languages = new ArrayList<>();
+		
+		ClassLoader loader = LanguageDetection.class.getClassLoader();
+        InputStream in = loader.getResourceAsStream(PROFILE_FOLDER);
+        BufferedReader rdr = new BufferedReader(new InputStreamReader(in));
+        String line;
+        while ((line = rdr.readLine()) != null) {
+        	URL fileUrl = Resources.getResource("./profiles/" +line);
+        	languages.add(Resources.toString(fileUrl, Charsets.UTF_8));
+        }
+        rdr.close();
+        
+        return languages;
+	}
+	
 	
 	@Override
 	public void bind(EngineParameters<LanguageDetectionParameters> parameters,
