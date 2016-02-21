3cb44e37756b2008f9bf97d9aa30a40c31149c29
Dominik Riemer <riemer@fzi.de>
2016-02-21 13:00:51 +0000

    added flink field renamer
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1598 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-flink-samples/src/main/java/de/fzi/cep/sepa/flink/samples/Init.java
+++ semantic-epa-flink-samples/src/main/java/de/fzi/cep/sepa/flink/samples/Init.java
@@ -7,6 +7,7 @@ import de.fzi.cep.sepa.flink.samples.classification.number.NumberClassificationC
 import de.fzi.cep.sepa.flink.samples.elasticsearch.ElasticSearchController;
 import de.fzi.cep.sepa.flink.samples.enrich.timestamp.TimestampController;
 import de.fzi.cep.sepa.flink.samples.hasher.FieldHasherController;
+import de.fzi.cep.sepa.flink.samples.rename.FieldRenamerController;
 import de.fzi.cep.sepa.flink.samples.wordcount.WordCountController;
 
 public class Init {
@@ -16,7 +17,8 @@ public class Init {
 				new ElasticSearchController(),
 				new NumberClassificationController(), 
 				new TimestampController(),
-				new FieldHasherController()), 8094);
+				new FieldHasherController(),
+				new FieldRenamerController()), 8094);
 
 	}
 }
