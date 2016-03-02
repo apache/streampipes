e5974c83dcfd7ee21a1f3a3b0455ff263085a5ac
Dominik Riemer <riemer@fzi.de>
2016-03-02 21:17:57 +0000

    modified geo vocabulary
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1633 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-model/src/main/java/de/fzi/cep/sepa/model/vocabulary/Geo.java
+++ semantic-epa-model/src/main/java/de/fzi/cep/sepa/model/vocabulary/Geo.java
@@ -4,4 +4,5 @@ public class Geo {
 
 	public static final String lat = "http://www.w3.org/2003/01/geo/wgs84_pos#lat";
 	public static final String lng = "http://www.w3.org/2003/01/geo/wgs84_pos#long";
+	public static final String alt = "http://www.w3.org/2003/01/geo/wgs84_pos#alt";
 }
