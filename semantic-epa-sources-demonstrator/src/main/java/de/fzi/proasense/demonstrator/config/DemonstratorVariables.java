51775d1346aa3306290d0716b1e63884e3fa81a2
Philipp Zehnder <zehnder@fzi.de>
2016-03-03 16:59:30 +0000

    Updated flow rate sensor names
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1642 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-sources-demonstrator/src/main/java/de/fzi/proasense/demonstrator/config/DemonstratorVariables.java
+++ semantic-epa-sources-demonstrator/src/main/java/de/fzi/proasense/demonstrator/config/DemonstratorVariables.java
@@ -2,13 +2,13 @@ package de.fzi.proasense.demonstrator.config;
 
 public enum DemonstratorVariables {
 
-    SIENENS_FLOWRATESENSOR1("flowrate1", "Flow Rate Sensor", "","de.fzi.proasense.demonstrator.siemens.flowrate.sensor1"),
-    SIENENS_FLOWRATESENSOR2("flowrate2", "Flow Rate Sensor", "","de.fzi.proasense.demonstrator.siemens.flowrate.sensor2"),
+    SIENENS_FLOWRATESENSOR1("flowrate1", "Flow Rate Sensor Siemens 1", "","de.fzi.proasense.demonstrator.siemens.flowrate.sensor1"),
+    SIENENS_FLOWRATESENSOR2("flowrate2", "Flow Rate Sensor Siemens 2", "","de.fzi.proasense.demonstrator.siemens.flowrate.sensor2"),
     SIEMENS_LEVELSENSOR("level", "Level Sensor", "","de.fzi.proasense.demonstrator.siemens.level.sensor1"),
 
     FESTO_CONTAINERB101("container101", "Container101", "","de.fzi.proasense.demonstrator.festo.container.b101"),
     FESTO_CONTAINERB102("container102", "Container102", "","de.fzi.proasense.demonstrator.festo.container.b102"),
-    FESTO_FLOWRATE("flowrate", "Flow Rate Sensor", "","de.fzi.proasense.demonstrator.festo.flowrate"),
+    FESTO_FLOWRATE("flowrate", "Flow Rate Sensor Festo 1", "","de.fzi.proasense.demonstrator.festo.flowrate"),
     FESTO_PRESSURE_TANK("pressure", "Pressure Tank Sensor", "","de.fzi.proasense.demonstrator.festo.pressuretank"),
     ;
 
