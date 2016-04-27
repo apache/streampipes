1d0559b5acd4380beb44d7fcfb03a095962baaff
Philipp Zehnder <zehnder@fzi.de>
2016-04-27 08:25:42 +0000

    Changed Strings to classes
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1695 aa18e314-994e-4353-9a36-64d395711e41

--- archetypes/streampipes-flink-code-generation/src/main/java/de/fzi/cep/sepa/streampipes_flink_code_generation/JFC.java
+++ archetypes/streampipes-flink-code-generation/src/main/java/de/fzi/cep/sepa/streampipes_flink_code_generation/JFC.java
@@ -1,6 +1,17 @@
 package de.fzi.cep.sepa.streampipes_flink_code_generation;
 
+
+import java.util.Map;
+
+import org.apache.flink.api.common.functions.FlatMapFunction;
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.util.Collector;
+
 import com.squareup.javapoet.ClassName;
+
+import de.fzi.cep.sepa.flink.AbstractFlinkAgentDeclarer;
+import de.fzi.cep.sepa.flink.FlinkDeploymentConfig;
+import de.fzi.cep.sepa.flink.FlinkSepaRuntime;
 
 /**
  * Java File Classes (JFC)
@@ -9,18 +20,18 @@ import com.squareup.javapoet.ClassName;
  */
 public abstract class JFC {
 
-	public static ClassName MAP = ClassName.get("java.util", "Map");
+	public static ClassName MAP = ClassName.get(Map.class);
 
 	public static ClassName STRING = ClassName.get("", "String");
 	public static ClassName OVERRIDE = ClassName.get("", "Override");
 	public static ClassName OBJECT = ClassName.get("", "Object");
 	public static ClassName EXCEPTION = ClassName.get("", "Exception");
 
-	public static ClassName DATA_STREAM = ClassName.get("org.apache.flink.streaming.api.datastream", "DataStream");
-	public static ClassName FLAT_MAP_FUNCTION = ClassName.get("org.apache.flink.api.common.functions", "FlatMapFunction");
-	public static ClassName COLLECTOR = ClassName.get("org.apache.flink.util", "Collector");
+	public static ClassName DATA_STREAM = ClassName.get(DataStream.class);
+	public static ClassName FLAT_MAP_FUNCTION = ClassName.get(FlatMapFunction.class);
+	public static ClassName COLLECTOR = ClassName.get(Collector.class);
 
-	public static ClassName FLINK_DEPLOYMENT_CONFIG = ClassName.get("de.fzi.cep.sepa.flink", "FlinkDeploymentConfig");
-	public static ClassName FLINK_SEPA_RUNTIME = ClassName.get("de.fzi.cep.sepa.flink", "FlinkSepaRuntime");
-	public static ClassName ABSTRACT_FLINK_AGENT_DECLARER = ClassName.get("de.fzi.cep.sepa.flink", "AbstractFlinkAgentDeclarer");
+	public static ClassName FLINK_DEPLOYMENT_CONFIG = ClassName.get(FlinkDeploymentConfig.class);
+	public static ClassName FLINK_SEPA_RUNTIME = ClassName.get(FlinkSepaRuntime.class);
+	public static ClassName ABSTRACT_FLINK_AGENT_DECLARER = ClassName.get(AbstractFlinkAgentDeclarer.class);
 }
