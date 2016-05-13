6925d78443c2669ca5067478ac207cc41eeab579
Philipp Zehnder <zehnder@fzi.de>
2016-05-13 12:29:06 +0000

    Added equals method to Response
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1798 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-model/src/main/java/de/fzi/cep/sepa/model/impl/Response.java
+++ semantic-epa-model/src/main/java/de/fzi/cep/sepa/model/impl/Response.java
@@ -32,5 +32,41 @@ public class Response {
 	public String getOptionalMessage() {
 		return optionalMessage;
 	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((elementId == null) ? 0 : elementId.hashCode());
+		result = prime * result + ((optionalMessage == null) ? 0 : optionalMessage.hashCode());
+		result = prime * result + (success ? 1231 : 1237);
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Response other = (Response) obj;
+		if (elementId == null) {
+			if (other.elementId != null)
+				return false;
+		} else if (!elementId.equals(other.elementId))
+			return false;
+		if (optionalMessage == null) {
+			if (other.optionalMessage != null)
+				return false;
+		} else if (!optionalMessage.equals(other.optionalMessage))
+			return false;
+		if (success != other.success)
+			return false;
+		return true;
+	}
+	
+	
 	
 }
