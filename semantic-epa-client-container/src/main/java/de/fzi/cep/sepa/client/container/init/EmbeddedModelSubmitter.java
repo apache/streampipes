8bc20f729a15f5a8a502c5e76c9b6e720f664890
Philipp Zehnder <zehnder@fzi.de>
2016-05-27 10:41:55 +0000

    Minor refactoring
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1891 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-client-container/src/main/java/de/fzi/cep/sepa/client/container/init/EmbeddedModelSubmitter.java
+++ semantic-epa-client-container/src/main/java/de/fzi/cep/sepa/client/container/init/EmbeddedModelSubmitter.java
@@ -7,25 +7,8 @@ import javax.servlet.ServletContextListener;
 
 public abstract class EmbeddedModelSubmitter extends ModelSubmitter implements ServletContextListener {
 
-    //TODO make dynamic and load the value from config
-//    public static String getBaseUri() {
-//        return "http://localhost:8081/stream-story/api/v1.1.1/";
-//    }
-
     public void contextInitialized(ServletContextEvent arg0)
     {
         init();
     }
-
-
-    public void contextDestroyed(ServletContextEvent arg0)
-    {
-    }
-
-//    /**
-//     * This Method needs to be implemented to instantiate an client container
-//     * Use the DeclarersSingleton to register the declarers
-//     */
-//    public abstract void init();
-
 }
--- semantic-epa-client-standalone/src/main/java/de/fzi/cep/sepa/client/standalone/init/StandaloneModelSubmitter.java
+++ semantic-epa-client-standalone/src/main/java/de/fzi/cep/sepa/client/standalone/init/StandaloneModelSubmitter.java
@@ -2,7 +2,6 @@ package de.fzi.cep.sepa.client.standalone.init;
 
 
 import de.fzi.cep.sepa.client.api.*;
-import de.fzi.cep.sepa.client.init.DeclarersSingleton;
 import org.eclipse.jetty.server.Server;
 import de.fzi.cep.sepa.client.init.ModelSubmitter;
 import org.glassfish.jersey.jetty.JettyHttpContainerFactory;
@@ -10,18 +9,32 @@ import org.glassfish.jersey.server.ResourceConfig;
 
 import javax.ws.rs.core.UriBuilder;
 import java.net.URI;
+import java.util.HashSet;
+import java.util.Set;
 
 public abstract class StandaloneModelSubmitter extends ModelSubmitter {
 
     public void init() {
-//        URI baseUri = UriBuilder.fromUri(getBaseUri()).build();
         URI baseUri = UriBuilder
-                .fromUri("http://localhost/")
-                .port(8080).build();
-        ResourceConfig config = new ResourceConfig(Element.class, InvocableElement.class, SecElement.class,
-                SepaElement.class, SepElement.class, WelcomePage.class);
+                .fromUri(getBaseUri())
+                .build();
+
+        ResourceConfig config = new ResourceConfig(getApiClasses());
 
         Server server = JettyHttpContainerFactory.createServer(baseUri, config);
+    }
+
+    private Set<Class<?>> getApiClasses() {
+        Set<Class<? extends Object>> allClasses = new HashSet<>();
+
+        allClasses.add(Element.class);
+        allClasses.add(InvocableElement.class);
+        allClasses.add(SecElement.class);
+        allClasses.add(SepaElement.class);
+        allClasses.add(SepElement.class);
+        allClasses.add(WelcomePage.class);
+
+        return  allClasses;
     }
 
 }
