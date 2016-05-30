0daa2c1f68de404e3c78d7f5d51ee99e019736df
Dominik Riemer <riemer@fzi.de>
2016-05-30 08:53:06 +0000

    modified package declaration
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1898 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-client/src/test/java/de/fzi/cep/sepa/init/DeclarersSingletonTest.java
+++ semantic-epa-client/src/test/java/de/fzi/cep/sepa/init/DeclarersSingletonTest.java
@@ -1,4 +1,4 @@
-package de.fzi.cep.sepa.client.container.init;
+package de.fzi.cep.sepa.init;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
@@ -6,11 +6,11 @@ import static org.junit.Assert.assertTrue;
 import java.util.ArrayList;
 import java.util.List;
 
-import de.fzi.cep.sepa.client.init.DeclarersSingleton;
 import org.junit.Test;
 
 import de.fzi.cep.sepa.client.declarer.Declarer;
 import de.fzi.cep.sepa.client.declarer.SemanticEventProcessingAgentDeclarer;
+import de.fzi.cep.sepa.client.init.DeclarersSingleton;
 import de.fzi.cep.sepa.model.impl.Response;
 import de.fzi.cep.sepa.model.impl.graph.SepaDescription;
 import de.fzi.cep.sepa.model.impl.graph.SepaInvocation;
