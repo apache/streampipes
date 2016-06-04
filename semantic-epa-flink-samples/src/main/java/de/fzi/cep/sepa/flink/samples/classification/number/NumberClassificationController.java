a7b802e577d44f98b7b33f03493b7ced226eda5b
Dominik Riemer <riemer@fzi.de>
2016-06-04 10:56:04 +0000

    trying to resolve maven dependency conflict
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1923 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-flink-samples/src/main/java/de/fzi/cep/sepa/flink/samples/classification/number/NumberClassificationController.java
+++ semantic-epa-flink-samples/src/main/java/de/fzi/cep/sepa/flink/samples/classification/number/NumberClassificationController.java
@@ -1,37 +1,76 @@
 package de.fzi.cep.sepa.flink.samples.classification.number;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.google.common.io.Resources;
-
-import de.fzi.cep.sepa.commons.exceptions.SepaParseException;
+import de.fzi.cep.sepa.client.util.StandardTransportFormat;
 import de.fzi.cep.sepa.flink.AbstractFlinkAgentDeclarer;
 import de.fzi.cep.sepa.flink.FlinkDeploymentConfig;
 import de.fzi.cep.sepa.flink.FlinkSepaRuntime;
 import de.fzi.cep.sepa.flink.samples.Config;
+import de.fzi.cep.sepa.model.builder.EpRequirements;
+import de.fzi.cep.sepa.model.impl.EventSchema;
+import de.fzi.cep.sepa.model.impl.EventStream;
+import de.fzi.cep.sepa.model.impl.eventproperty.EventProperty;
+import de.fzi.cep.sepa.model.impl.eventproperty.EventPropertyPrimitive;
 import de.fzi.cep.sepa.model.impl.graph.SepaDescription;
 import de.fzi.cep.sepa.model.impl.graph.SepaInvocation;
 import de.fzi.cep.sepa.model.impl.output.AppendOutputStrategy;
+import de.fzi.cep.sepa.model.impl.output.OutputStrategy;
 import de.fzi.cep.sepa.model.impl.staticproperty.CollectionStaticProperty;
 import de.fzi.cep.sepa.model.impl.staticproperty.DomainStaticProperty;
+import de.fzi.cep.sepa.model.impl.staticproperty.FreeTextStaticProperty;
+import de.fzi.cep.sepa.model.impl.staticproperty.StaticProperty;
 import de.fzi.cep.sepa.model.impl.staticproperty.SupportedProperty;
-import de.fzi.cep.sepa.model.vocabulary.SO;
 import de.fzi.cep.sepa.model.util.SepaUtils;
-import de.fzi.cep.sepa.client.util.DeclarerUtils;
+import de.fzi.cep.sepa.model.vocabulary.SO;
+import de.fzi.cep.sepa.model.vocabulary.XSD;
 
 public class NumberClassificationController extends AbstractFlinkAgentDeclarer<NumberClassificationParameters> {
 
 	@Override
 	public SepaDescription declareModel() {
-		try {
-			return DeclarerUtils.descriptionFromResources(Resources.getResource("numberclassification.jsonld"),
-					SepaDescription.class);
-		} catch (SepaParseException e) {
-			e.printStackTrace();
-			return null;
-		}
+		List<EventProperty> eventProperties = new ArrayList<EventProperty>();		
+		EventProperty e1 = EpRequirements.domainPropertyReq(SO.Number);
+		eventProperties.add(e1);
+		EventSchema schema1 = new EventSchema();
+		schema1.setEventProperties(eventProperties);
+		
+		EventStream stream1 = new EventStream();
+		stream1.setEventSchema(schema1);
+		
+		SepaDescription desc = new SepaDescription("classification_number", "Flink Number classification", "Label your data. Based on Apache Flink.");
+		
+		desc.addEventStream(stream1);
+		
+		List<StaticProperty> staticProperties = new ArrayList<>();
+		staticProperties.add(new FreeTextStaticProperty("to_classify", "Field to classify", "Name of the field that should be classified"));
+		
+		List<SupportedProperty> supportedProperties = new ArrayList<>();
+		supportedProperties.add(new SupportedProperty(SO.MinValue, true));
+		supportedProperties.add(new SupportedProperty(SO.MaxValue, true));
+		supportedProperties.add(new SupportedProperty(SO.Text, true));
+		
+		DomainStaticProperty dsp = new DomainStaticProperty("Class", "Label for range", "Define a value", supportedProperties);
+	
+		CollectionStaticProperty classificationOptions = new CollectionStaticProperty("classification_options", "Classification options", "", Arrays.asList(dsp), "de.fzi.cep.sepa.model.impl.staticproperty.DomainStaticProperty");
+		staticProperties.add(classificationOptions);
+		
+		List<OutputStrategy> strategies = new ArrayList<OutputStrategy>();
+		AppendOutputStrategy outputStrategy = new AppendOutputStrategy();
+
+		List<EventProperty> appendProperties = new ArrayList<EventProperty>();
+		appendProperties.add(new EventPropertyPrimitive(XSD._string.toString(),
+				"output_label", "", de.fzi.cep.sepa.commons.Utils.createURI("http://schema.org/Text")));
+		
+		outputStrategy.setEventProperties(appendProperties);
+		strategies.add(outputStrategy);
+		desc.setOutputStrategies(strategies);
+		desc.setSupportedGrounding(StandardTransportFormat.getSupportedGrounding());
+		
+		return desc;
 	}
 
 	@Override
