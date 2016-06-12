c9574e4a833dc222d81000c2838492309675f8cb
Dominik Riemer <riemer@fzi.de>
2016-06-12 21:26:36 +0000

    modified model editor
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1951 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-server/src/main/webapp/js/directives/directives-general.js
+++ semantic-epa-server/src/main/webapp/js/directives/directives-general.js
@@ -66,7 +66,30 @@ angular
     			
        		}
     	}
-    });
+    }).directive('nagPrism', ['$compile', function($compile) {
+        return {
+            restrict: 'A',
+            transclude: true,
+            scope: {
+              source: '@'
+            },
+            link: function(scope, element, attrs, controller, transclude) {
+                scope.$watch('source', function(v) {
+                  element.find("code").html(v);
+
+                  Prism.highlightElement(element.find("code")[0]);
+                });
+
+                transclude(function(clone) {
+                  if (clone.html() !== undefined) {
+                    element.find("code").html(clone.html());
+                    $compile(element.contents())(scope.$parent);
+                  }
+                });
+            },
+            template: "<code></code>"
+        };
+    }]);
 
 
 
