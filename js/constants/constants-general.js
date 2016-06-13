32f75b56708767ae2cd2bf8bbf0c9456782b1e91
Dominik Riemer <riemer@fzi.de>
2016-06-13 16:40:02 +0000

    removed unused js dependencies
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@1971 aa18e314-994e-4353-9a36-64d395711e41

---
+++ js/constants/constants-general.js
@@ -0,0 +1,38 @@
+ angular
+    .module('streamPipesApp')
+    .constant("apiConstants", {
+        url: "http://localhost",
+        port: "8080",
+        contextPath : "/semantic-epa-backend",
+        api : "/api/v2",
+		streamEndpointOptions : {
+			endpoint: ["Dot", {radius:12}],
+			connectorStyle: {strokeStyle: "#BDBDBD", outlineColor : "#9E9E9E", lineWidth: 5},
+			connector: "Straight",
+			isSource: true,
+			anchor:"Right",
+			type : "token",
+			connectorOverlays: [
+				["Arrow", {width: 20, length: 10, location: 0.5, id: "arrow"}],
+			]
+		},
+
+		sepaEndpointOptions : {
+			endpoint: ["Dot", {radius:12}],
+			connectorStyle: {strokeStyle: "#BDBDBD", outlineColor : "#9E9E9E", lineWidth: 5},
+			connector: "Straight",
+			isSource: true,
+			anchor: "Right",
+			type : "empty",
+			connectorOverlays: [
+				["Arrow", {width: 25, length: 20, location: 0.5, id: "arrow"}],
+			]
+		},
+
+		leftTargetPointOptions : {
+			endpoint: ["Dot", {radius:12}],
+			type : "empty",
+			anchor: "Left",
+			isTarget: true
+		}
+	});
