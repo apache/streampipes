dce058473b8a5eee41fe509f396f7246b98cb25d
Dominik Riemer <riemer@fzi.de>
2016-06-20 11:33:43 +0000

    modified file retrieval
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@2021 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-appstore-deployer/src/main/java/de/fzi/cep/sepa/appstore/deployer/api/AppDeployer.java
+++ semantic-epa-appstore-deployer/src/main/java/de/fzi/cep/sepa/appstore/deployer/api/AppDeployer.java
@@ -1,19 +1,24 @@
 package de.fzi.cep.sepa.appstore.deployer.api;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
 
 import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.fluent.Request;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.entity.ContentType;
+import org.apache.http.impl.nio.client.CloseableHttpAsyncClient;
+import org.apache.http.impl.nio.client.HttpAsyncClients;
+import org.apache.http.nio.client.methods.ZeroCopyConsumer;
+import org.apache.http.nio.protocol.BasicAsyncRequestProducer;
 
 import com.google.gson.Gson;
 
@@ -33,32 +38,47 @@ public class AppDeployer {
 	public String installApp(String bundleString) {
 		Gson gson = new Gson();
 		BundleInfo bundle = gson.fromJson(bundleString, BundleInfo.class);
+		 CloseableHttpAsyncClient httpclient = HttpAsyncClients.createDefault();
+		 httpclient.start();
 		try {
-			InputStream is = Request.Get(bundle.getAppStoreUrl() +"/" +bundle.getBundleId() +"/install")
-				.execute()
-				.returnContent()
-				.asStream();
+//			InputStream is = Request.Get(bundle.getAppStoreUrl() +"/" +bundle.getBundleId() +"/install")
+//				.execute()
+//				.returnContent()
+//				.asStream();
+			
 			String filePath = getBundleFilename(bundle.getLocation());
 
-			FileOutputStream fos = new FileOutputStream(filePath);
-			int inByte;
-			while((inByte = is.read()) != -1)
-			     fos.write(inByte);
-			is.close();
-			fos.close();
+			ZeroCopyConsumer<File> consumer = new ZeroCopyConsumer<File>(new File(filePath)) {
+
+				@Override
+				protected File process(HttpResponse response, File file,
+						ContentType contentType) throws Exception {
+					return file;
+				}
+			};
+			
+			BasicAsyncRequestProducer producer = new BasicAsyncRequestProducer(new HttpHost("ipe-koi15.fzi.de", 8080), new HttpGet(bundle.getAppStoreUrl() +"/" +bundle.getBundleId() +"/install"));
+			Future<File> future = httpclient.execute(producer, consumer, null);
+			File result = future.get();
+			
+			httpclient.close();
 			return gson.toJson(new InstallationStatus(bundle, true));
 			
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-			return gson.toJson(new InstallationStatus(bundle, false, e.getMessage()));
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 			return gson.toJson(new InstallationStatus(bundle, false, e.getMessage()));
-		}
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+			return gson.toJson(new InstallationStatus(bundle, false, e.getMessage()));
+		} catch (ExecutionException e) {
+			return gson.toJson(new InstallationStatus(bundle, false, e.getMessage()));
+		} 
 		
 	}
 	
-	@DELETE
+	@POST
+	@Path("/uninstall")
 	@Consumes(MediaType.APPLICATION_JSON)
 	@Produces(MediaType.APPLICATION_JSON)
 	public String uninstallApp(String bundleString) {
