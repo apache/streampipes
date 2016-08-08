9f6b128e0acc20f6e0edb741c61022da6b410ff3
Philipp Zehnder <zehnder@fzi.de>
2016-08-08 16:50:16 +0000

    Working on dashboard
    git-svn-id: https://ipesvn.fzi.de/svn-open/icep/semantic-epa-parent@2048 aa18e314-994e-4353-9a36-64d395711e41

--- semantic-epa-server/src/main/webapp/modules/dashboard/dashboard.js
+++ semantic-epa-server/src/main/webapp/modules/dashboard/dashboard.js
@@ -1,7 +1,89 @@
 
 angular.module('streamPipesApp')
-	.controller('DashCtrl', ['$rootScope', '$scope', 'RandomTopNDataModel', function($rootScope, $scope, RandomTopNDataModel) {
-		// var widget = {"name" : "pipelineName", "consumerUrl" : "pipelineURL", "size" : {"width" :"50%", "height" : "100%"}, "style" : { }, "template" : "<div>Template from templateUrl<div >{{percentage}}</div></div>"};
+	.controller('DashCtrl', ['$rootScope', '$scope', '$http', 'RandomTopNDataModel', '$mdDialog', 'Widgets', function($rootScope, $scope, $http, RandomTopNDataModel, $mdDialog, Widgets) {
+		var couchDbServer = 'http://127.0.0.1:5984';
+
+		$scope.rerender = true;
+		var possibleVisualizations = [];
+
+		$http.get(couchDbServer + '/visualization/_all_docs?include_docs=true')
+			.success(function(data) {
+				possibleVisualizations = data.rows;
+			});
+
+
+
+		$scope.addWidget = function() {
+
+			$mdDialog.show({
+				controller: AddWidgetController,
+				templateUrl: 'modules/dashboard/add-widget-template/add-widget-template.html',
+				parent: angular.element(document.body),
+				clickOutsideToClose:true,
+				locals : {
+					possibleVisualizations: possibleVisualizations
+				}
+			});
+
+		};
+
+		// TODO Helper to add new Widgets to the dashboard
+		// A better solution is needed
+		var rerenderDashboard = function() {
+			$scope.rerender = false;
+			setTimeout(function() {
+				$scope.$apply(function () {
+					$scope.rerender = true;
+				});
+			}, 100);
+		}
+
+		function AddWidgetController($scope, $mdDialog, possibleVisualizations) {
+
+			$scope.page = 'select-viz';
+
+			$scope.possibleVisualizations = possibleVisualizations;
+			$scope.selectedVis = {};
+
+			$scope.possibleVisTypes = ['table'];
+			$scope.selectedVisType = '';
+
+			$scope.next = function() {
+				if ($scope.page == 'select-viz') {
+					$scope.page = 'select-type';
+				} else if ($scope.page == 'select-type') {
+					$scope.page = 'select-scheme';
+				} else {
+					var selectedProperties = [];
+					$scope.selectedVis.schema.eventProperties.forEach(function(entry) {
+						if (entry.isSelected) {
+							selectedProperties.push(entry);
+						}
+					});
+
+					var widget = {};
+					widget.selectedProperties = selectedProperties;
+					widget.visType = $scope.selectedVisType;
+					widget.vis = $scope.selectedVis;
+					widget.id = $scope.selectedVis._id;
+
+					Widgets.add(widget);
+
+					widgetDefinitions.push(
+						Widgets.getWidgetDefinition(widget.id)
+						);
+
+					rerenderDashboard();
+
+					$mdDialog.cancel();
+				}
+			}
+
+			$scope.cancel = function() {
+				$mdDialog.cancel();
+			};
+		}
+
 
 		var widgetDefinitions = [
 			{
@@ -14,14 +96,7 @@ angular.module('streamPipesApp')
 				style: {
 					width: '33%'
 				}
-			}, {
-				name: 'wt-top-n',
-				dataAttrName: 'data',
-				dataModelType: RandomTopNDataModel,
-				style: {
-					width: '30%'
-				}
-			 }
+			}
 		];
 
 
@@ -30,10 +105,11 @@ angular.module('streamPipesApp')
 				name: widgetDef.name
 			};
 		});
+
 		$scope.dashboardOptions = {
 			widgetButtons: true,
 			widgetDefinitions: widgetDefinitions,
 			defaultWidgets: defaultWidgets
 		};
-		
+
 	}]);
--- semantic-epa-server/src/main/webapp/modules/dashboard/templates/topn/services/randmotopndatamodel.js
+++ semantic-epa-server/src/main/webapp/modules/dashboard/templates/topn/services/randmotopndatamodel.js
@@ -4,27 +4,28 @@ angular.module('streamPipesApp').factory('RandomTopNDataModel', function (Widget
     function RandomTopNDataModel() {
     }
 
+    var client;
     RandomTopNDataModel.prototype = Object.create(WidgetDataModel.prototype);
 
     RandomTopNDataModel.prototype.init = function () {
 
         var couchDbServer = 'http://127.0.0.1:5984';
 
-        var couchDbVizualisationId = 'c5491ef6630a4af694bddcdd5deb010d';
+        var couchDbVizualisationId = '5cfcd7327fd04a549130dba7187e487e';
         
         //TODO find better solution
         var login = 'admin';
         var passcode = 'admin';
         var self = this;
 
-        $http.get(couchDbServer + '/visualization/' + 'c5491ef6630a4af694bddcdd5deb010d')
+        $http.get(couchDbServer + '/visualization/' + couchDbVizualisationId)
             .success(function(data) {
 
                 //TODO should we write this into the couchdb?
                 var brokerUrl = 'ws://ipe-koi04.fzi.de:61614';
                 var inputTopic = '/topic/' + data['pipelineId'];
 
-                var client = Stomp.client(brokerUrl);
+                client = Stomp.client(brokerUrl);
 
                 // Uncomment these lines to get all the wesocket messages to the console
                 //  client.debug = function (str) {
@@ -32,36 +33,30 @@ angular.module('streamPipesApp').factory('RandomTopNDataModel', function (Widget
                 //  };
 
                 var dataArray = [];
+                var dataArrayLength = 5;
 
                 // the client is notified when it is connected to the server.
                 var onConnect = function (frame) {
 
                     client.subscribe(inputTopic, function (message) {
                         dataArray.push(JSON.parse(message.body));
+                        if (dataArray.length > dataArrayLength) {
+                            dataArray = dataArray.slice(Math.max(dataArray.length - dataArrayLength, 1));
+                        }
                         self.updateScope(dataArray);
                     });
                 };
 
                 client.connect(login, passcode, onConnect);
             });
-
-
-
-
-        // this.intervalPromise = $interval(function () {
-        //     var topTen = _.map(_.range(0, 10), function (index) {
-        //         return {
-        //             name: 'item' + index,
-        //             value: Math.floor(Math.random() * 100)
-        //         };
-        //     });
-        //     this.updateScope(topTen);
-        // }.bind(this), 500);
     };
 
     RandomTopNDataModel.prototype.destroy = function () {
         WidgetDataModel.prototype.destroy.call(this);
-        $interval.cancel(this.intervalPromise);
+        
+        client.disconnect(function() {
+           console.log("Disconnected websocket connection");
+        })
     };
 
     return RandomTopNDataModel;
