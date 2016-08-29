a015592870386e5e30950052b800fb6adaa8c681
Philipp Zehnder <zehnder@fzi.de>
2016-08-29 12:43:02 +0200

    Added broker information to the couchdb

--- semantic-epa-actions-samples/src/main/java/de/fzi/cep/sepa/actions/dashboard/DashboardParameters.java
+++ semantic-epa-actions-samples/src/main/java/de/fzi/cep/sepa/actions/dashboard/DashboardParameters.java
@@ -2,16 +2,22 @@ package de.fzi.cep.sepa.actions.dashboard;
 
 import de.fzi.cep.sepa.model.impl.EventSchema;
 import de.fzi.cep.sepa.model.impl.graph.SecInvocation;
+import de.fzi.cep.sepa.commons.config.ClientConfiguration;
 
 public class DashboardParameters {
     private String pipelineId;
     private EventSchema schema;
+    private String broker;
 
     public DashboardParameters(SecInvocation invocationGraph) {
         this.schema = invocationGraph.getInputStreams().get(0).getEventSchema();
         this.pipelineId = invocationGraph.getCorrespondingPipeline();
+        this.broker = removeProtocol(ClientConfiguration.INSTANCE.getJmsUrl());
     }
 
+    private String removeProtocol(String url) {
+       return url.replaceFirst("^(tcp://|ws://)","");
+    }
     public String getPipelineId() {
         return pipelineId;
     }
@@ -26,5 +32,13 @@ public class DashboardParameters {
 
     public void setSchema(EventSchema schema) {
         this.schema = schema;
+    }
+
+    public String getBroker() {
+        return broker;
+    }
+
+    public void setBroker(String broker) {
+        this.broker = broker;
     }
 }
