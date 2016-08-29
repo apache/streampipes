6a1e15f89815d794ee49850caab8fa3e7e7e514b
Philipp Zehnder <zehnder@fzi.de>
2016-08-29 12:50:43 +0200

    Added broker information to the chouchdb

---
+++ semantic-epa-server/src/main/webapp/modules/dashboard/services/socketConnectionDataModel.js
@@ -0,0 +1,58 @@
+angular.module('streamPipesApp').factory('SocketConnectionDataModel', ['WidgetDataModel', '$http', function(WidgetDataModel, $http) {
+	 function SocketConnectionDataModel(id) {
+		 var id = id;
+		 this.id = id;
+	 }
+
+	 SocketConnectionDataModel.prototype = Object.create(WidgetDataModel.prototype);
+	 SocketConnectionDataModel.prototype.init = function () {
+
+		 //TODO make dynamic
+		 var couchDbServer = 'http://127.0.0.1:5984';
+
+		 //TODO find better solution
+		 var login = 'admin';
+		 var passcode = 'admin';
+		 var self = this;
+
+		 $http.get(couchDbServer + '/visualization/' + this.id)
+			 .success(function(data) {
+
+				 var topic = 'ws://' + data['broker'] + '/topic/' + data['pipelineId'];
+
+				 client = Stomp.client(topic);
+
+				 // Uncomment these lines to get all the wesocket messages to the console
+				 //  client.debug = function (str) {
+				 //  	console.log(str);
+				 //  };
+
+				 var dataArray = [];
+				 var dataArrayLength = 5;
+
+				 // the client is notified when it is connected to the server.
+				 var onConnect = function (frame) {
+
+					 client.subscribe(inputTopic, function (message) {
+						 dataArray.push(JSON.parse(message.body));
+						 if (dataArray.length > dataArrayLength) {
+							 dataArray = dataArray.slice(Math.max(dataArray.length - dataArrayLength, 1));
+						 }
+						 self.updateScope(dataArray);
+					 });
+				 };
+
+				 client.connect(login, passcode, onConnect);
+			 });
+	 };
+
+	 SocketConnectionDataModel.prototype.destroy = function () {
+		 WidgetDataModel.prototype.destroy.call(this);
+
+		 client.disconnect(function() {
+			 console.log("Disconnected websocket connection");
+		 })
+	 };
+
+	 return SocketConnectionDataModel;
+ }]);
--- semantic-epa-server/src/main/webapp/modules/dashboard/services/widgets.js
+++ semantic-epa-server/src/main/webapp/modules/dashboard/services/widgets.js
@@ -1,6 +1,6 @@
 'use strict';
 
-angular.module('streamPipesApp').factory('Widgets', function (WidgetDataModel, $http) {
+angular.module('streamPipesApp').factory('Widgets', ['$http','SocketConnectionDataModel', function ($http, SocketConnectionDataModel) {
 
 		var widgets = new Array();
     var client;
@@ -30,71 +30,9 @@ angular.module('streamPipesApp').factory('Widgets', function (WidgetDataModel, $
 			}
 		}
 
-		//TODO put SocketConnectionDataModel in an extra class
-    function SocketConnectionDataModel(id) {
-			var id = id;
-			this.id = id;
-    }
-
-    SocketConnectionDataModel.prototype = Object.create(WidgetDataModel.prototype);
-    SocketConnectionDataModel.prototype.init = function () {
-
-			//console.log(this.id);
-
-        var couchDbServer = 'http://127.0.0.1:5984';
-
-				//var couchDbVizualisationId = '5cfcd7327fd04a549130dba7187e487e';
-        //var couchDbVizualisationId = id;
-        
-        //TODO find better solution
-        var login = 'admin';
-        var passcode = 'admin';
-        var self = this;
-
-        $http.get(couchDbServer + '/visualization/' + this.id)
-            .success(function(data) {
-
-                //TODO should we write this into the couchdb?
-                var brokerUrl = 'ws://ipe-koi04.fzi.de:61614';
-                var inputTopic = '/topic/' + data['pipelineId'];
-
-                client = Stomp.client(brokerUrl + inputTopic);
-
-                // Uncomment these lines to get all the wesocket messages to the console
-                //  client.debug = function (str) {
-                //  	console.log(str);
-                //  };
-
-                var dataArray = [];
-                var dataArrayLength = 5;
-
-                // the client is notified when it is connected to the server.
-                var onConnect = function (frame) {
-
-                    client.subscribe(inputTopic, function (message) {
-                        dataArray.push(JSON.parse(message.body));
-                        if (dataArray.length > dataArrayLength) {
-                            dataArray = dataArray.slice(Math.max(dataArray.length - dataArrayLength, 1));
-                        }
-                        self.updateScope(dataArray);
-                    });
-                };
-
-                client.connect(login, passcode, onConnect);
-            });
-    };
-
-    SocketConnectionDataModel.prototype.destroy = function () {
-        WidgetDataModel.prototype.destroy.call(this);
-        
-        client.disconnect(function() {
-           console.log("Disconnected websocket connection");
-        })
-    };
-
 		return {
 			add: createNewWidget,
 			get: getWidgetById,	
 			getWidgetDefinition: getWidgetDashboardDefinition
 		};
-});
+}]);
