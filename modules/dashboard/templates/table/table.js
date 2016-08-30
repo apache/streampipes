62de82ffa4a19653f6882d6787f224017a29515b
Philipp Zehnder <zehnder@fzi.de>
2016-08-30 17:39:45 +0200

    The table example works now

--- modules/dashboard/templates/table/table.js
+++ modules/dashboard/templates/table/table.js
@@ -1,38 +1,42 @@
 'use strict';
 
 angular.module('streamPipesApp')
-    .directive('tableWidget',['Widgets', function (Widgets) {
-        return {
-            restrict: 'A',
-            replace: true,
-            templateUrl: 'modules/dashboard/templates/table/table.html',
-            scope: {
-							data: '=',
-							widgetId: '@'
-            },
-            controller: function ($scope) {
-                $scope.tableOptions = {
-                    initialSorts: [
-                        { id: 'value', dir: '-' }
-                    ]
-                };
-
-								//Add the colums that where selected by the user
-								var widgetConfig = Widgets.get($scope.widgetId);
-								$scope.columns = [];
-								angular.forEach(widgetConfig.selectedProperties, function(prop) {
-									var name = prop.properties.runtimeName;
-									$scope.columns.push({ id: name, key: name, label: name });
-								});
+	.directive('tableWidget',['Widgets', function (Widgets) {
+		return {
+			restrict: 'A',
+			replace: true,
+			templateUrl: 'modules/dashboard/templates/table/table.html',
+			scope: {
+				data: '=',
+				widgetId: '@'
+			},
+			controller: function ($scope) {
+				$scope.tableOptions = {
+					initialSorts: [
+						{ id: 'value', dir: '-' }
+					]
+				};
 
 
-            },
-            link: function postLink(scope) {
-                scope.$watch('data', function (data) {
-                    if (data) {
-                        scope.items = data;
-                    }
-                });
-            }
-        };
-    }]);
+				//Add the colums that where selected by the user
+				var widgetConfig = Widgets.get($scope.widgetId);
+				$scope.columns = [];
+				angular.forEach(widgetConfig.vis.schema.eventProperties, function(prop) {
+
+					if (prop.isSelected) {
+						var name = prop.properties.runtimeName;
+						$scope.columns.push({ id: name, key: name, label: name });
+					}
+				});
+
+
+			},
+			link: function postLink(scope) {
+				scope.$watch('data', function (data) {
+					if (data) {
+						scope.items = data;
+					}
+				});
+			}
+		};
+	}]);
