0caf2c5c91849b6ab5328edf5c807846d3ca61fc
Philipp Zehnder <zehnder@fzi.de>
2016-08-31 13:08:40 +0200

    Removed some unused code

--- modules/dashboard/templates/table/tableConfig.js
+++ modules/dashboard/templates/table/tableConfig.js
@@ -1,36 +1,12 @@
 'use strict';
 
 angular.module('streamPipesApp')
-    .directive('spTableWidgetConfig',function () {
-        return {
-						restrict: 'E',
-            //replace: true,
-						templateUrl: 'modules/dashboard/templates/table/tableConfig.html',
-						//template: '<h1>This is awesome!</h1>',
-						scope: {
-							wid: '='
-						}
-						//controller: function ($scope) {
-								//$scope.widget = Widgets.get($scope.widgetId);
-
-					//var selectedProperties = [];
-					//$scope.selectedVis.schema.eventProperties.forEach(function(entry) {
-						//if (entry.isSelected) {
-							//selectedProperties.push(entry);
-						//}
-					//});
-
-					//console.log('Yeahh');
-								//var lala = $scope.widget;
-					
-				
-						//}
-            //link: function postLink(scope) {
-                //scope.$watch('data', function (data) {
-                    //if (data) {
-                        //scope.items = data;
-                    //}
-                //});
-            //}
-				};
-    });
+	.directive('spTableWidgetConfig',function () {
+		return {
+			restrict: 'E',
+			templateUrl: 'modules/dashboard/templates/table/tableConfig.html',
+			scope: {
+				wid: '='
+			}
+		};
+	});
