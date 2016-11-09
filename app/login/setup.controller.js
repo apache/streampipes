1018244d769a0f8d532077fa7a535d09da2e27ca
Dominik Riemer <riemer@fzi.de>
2016-11-09 07:57:28 +0100

    modified setup dialog

--- app/login/setup.controller.js
+++ app/login/setup.controller.js
@@ -1,73 +1,80 @@
-SetupCtr.$inject = ['$scope', '$location', 'restApi', '$mdToast'];
+SetupCtr.$inject = ['$scope', '$rootScope', '$location', 'restApi', '$mdToast'];
 
-export default function SetupCtr($scope, $location, restApi, $mdToast) {
+export default function SetupCtr($scope, $rootScope, $location, restApi, $mdToast) {
 
-		$scope.installationFinished = false;
-		$scope.installationSuccessful = false;
-		$scope.installationResults = [{}];
-		$scope.loading = false;
+    $scope.installationFinished = false;
+    $scope.installationSuccessful = false;
+    $scope.installationResults = [{}];
+    $scope.loading = false;
 
 
-		$scope.setup = {
-			couchDbHost: $location.host(),
-			couchDbProtocol : 'http',
-			couchDbPort : 5984,
-			couchDbUserDbName : 'users',
-			couchDbPipelineDbName : 'pipeline',
-			couchDbConnectionDbName : 'connection',
-			couchDbMonitoringDbName : 'monitoring',
-			couchDbNotificationDbName : 'notification',
-			sesameUrl: 'http://localhost:8030/openrdf-sesame',
-			sesameDbName : 'test-6',
-			kafkaProtocol : 'http',
-			kafkaHost : $location.host(),
-			kafkaPort : 9092,
-			zookeeperProtocol : 'http',
-			zookeeperHost : $location.host(),
-			zookeeperPort : 2181,
-			jmsProtocol : 'tcp',
-			jmsHost : $location.host(),
-			jmsPort : 61616,
-			adminUsername: '',
-			adminEmail: '' ,
-			adminPassword: '',
-			streamStoryUrl : 'http://' +$location.host() +'/streamstory',
-			panddaUrl : 'http://' +$location.host() +':90/pandda_v2_2',
-			hippoUrl : 'http://' +$location.host() +'/kpimodeller',
-			humanInspectionReportUrl : '',
-			humanMaintenanceReportUrl : '',
-			appConfig : 'StreamPipes',
-			marketplaceUrl : '',
-			podUrls : []
-		};
+    $scope.setup = {
+        couchDbHost: $location.host(),
+        couchDbProtocol: 'http',
+        couchDbPort: 5984,
+        couchDbUserDbName: 'users',
+        couchDbPipelineDbName: 'pipeline',
+        couchDbConnectionDbName: 'connection',
+        couchDbMonitoringDbName: 'monitoring',
+        couchDbNotificationDbName: 'notification',
+        sesameUrl: 'http://' + $location.host() + ':8030/openrdf-sesame',
+        sesameDbName: 'test-6',
+        kafkaProtocol: 'http',
+        kafkaHost: $location.host(),
+        kafkaPort: 9092,
+        zookeeperProtocol: 'http',
+        zookeeperHost: $location.host(),
+        zookeeperPort: 2181,
+        jmsProtocol: 'tcp',
+        jmsHost: $location.host(),
+        jmsPort: 61616,
+        adminUsername: '',
+        adminEmail: '',
+        adminPassword: '',
+        streamStoryUrl: 'http://' + $location.host() + '/streamstory',
+        panddaUrl: 'http://' + $location.host() + ':90/pandda_v2_2',
+        hippoUrl: 'http://' + $location.host() + '/kpimodeller',
+        humanInspectionReportUrl: '',
+        humanMaintenanceReportUrl: '',
+        appConfig: 'ProaSense',
+        marketplaceUrl: '',
+        podUrls: []
+    };
 
-		$scope.configure = function() {
-			$scope.loading = true;
-			restApi.setupInstall($scope.setup).success(function(data) {
-				$scope.installationFinished = true;
-				$scope.installationResults = data;
-				$scope.loading = false;
-			}).error(function(data) {
-				$scope.loading = false;
-				$scope.showToast("Fatal error, contact administrator");
-			});
-		}
+    $scope.configure = function () {
+        $scope.loading = true;
+        restApi.setupInstall($scope.setup).success(function (data) {
+            $scope.installationResults = data;
 
-		$scope.showToast = function(string) {
-			$mdToast.show(
-				$mdToast.simple()
-					.content(string)
-					.position("right")
-					.hideDelay(3000)
-			);
-		};
+            restApi.configured()
+                .then(function (response) {
+                    if (response.data.configured) {
+                        $rootScope.appConfig = response.data.appConfig;
+                        $scope.installationFinished = true;
+                        $scope.loading = false;
+                    }
+                }).error(function (data) {
+                $scope.loading = false;
+                $scope.showToast("Fatal error, contact administrator");
+            });
+        });
+    }
 
-		$scope.addPod = function(podUrls) {
-			if (podUrls == undefined) podUrls = [];
-			podUrls.push("localhost");
-		}
+    $scope.showToast = function (string) {
+        $mdToast.show(
+            $mdToast.simple()
+                .content(string)
+                .position("right")
+                .hideDelay(3000)
+        );
+    };
 
-		$scope.removePod = function(podUrls, index) {
-			podUrls.splice(index, 1);
-		}
-	};
+    $scope.addPod = function (podUrls) {
+        if (podUrls == undefined) podUrls = [];
+        podUrls.push("localhost");
+    }
+
+    $scope.removePod = function (podUrls, index) {
+        podUrls.splice(index, 1);
+    }
+};
