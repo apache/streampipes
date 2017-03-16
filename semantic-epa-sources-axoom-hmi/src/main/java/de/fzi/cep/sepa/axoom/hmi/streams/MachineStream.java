d1051d623e32eea70f5c586d75e31d84a379114d
Dominik Riemer <riemer@fzi.de>
2017-03-16 23:03:47 +0100

    added first version of axoom stream definitions

---
+++ semantic-epa-sources-axoom-hmi/src/main/java/de/fzi/cep/sepa/axoom/hmi/streams/MachineStream.java
@@ -0,0 +1,16 @@
+package de.fzi.cep.sepa.axoom.hmi.streams;
+
+import de.fzi.cep.sepa.model.impl.EventStream;
+import de.fzi.cep.sepa.model.impl.graph.SepDescription;
+import de.fzi.cep.sepa.sources.AbstractAlreadyExistingStream;
+
+/**
+ * Created by riemer on 16.03.2017.
+ */
+public class MachineStream extends AbstractAlreadyExistingStream {
+
+  @Override
+  public EventStream declareModel(SepDescription sep) {
+    return null;
+  }
+}
---
+++ semantic-epa-sources-axoom-hmi/src/main/java/de/fzi/cep/sepa/axoom/hmi/vocabulary/AxoomVocabulary.java
@@ -0,0 +1,17 @@
+package de.fzi.cep.sepa.axoom.hmi.vocabulary;
+
+/**
+ * Created by riemer on 16.03.2017.
+ */
+public class AxoomVocabulary {
+
+  public static final String PREFIX = "http://axoom.com/vocab/";
+
+  public static final String Name = PREFIX +"name";
+  public static final String MachineId = PREFIX +"machineId";
+  public static final String MilkIncluded = PREFIX +"milkIncluded";
+  public static final String SugarIncluded = PREFIX +"sugarIncluded";
+  public static final String MachineName = PREFIX +"machineName";
+  public static final String MachineDisplayName = PREFIX +"machineDisplayName";
+  public static final String MachineModel = PREFIX +"machineModel";
+}
