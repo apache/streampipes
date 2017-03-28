93c84e674ff6f3e0998ef1f3a8e7d57b93e69f70
Dominik Riemer <riemer@fzi.de>
2017-03-28 18:10:10 +0200

    use jgitflow for releases

--- semantic-epa-flink-samples/src/main/java/de/fzi/cep/sepa/flink/samples/elasticsearch/ElasticSearchIndexRequestBuilder.java
+++ semantic-epa-flink-samples/src/main/java/de/fzi/cep/sepa/flink/samples/elasticsearch/ElasticSearchIndexRequestBuilder.java
@@ -10,26 +10,26 @@ import java.util.Map;
 
 public class ElasticSearchIndexRequestBuilder implements ElasticsearchSinkFunction<Map<String, Object>> {
 
-	private String indexName;
-	private String typeName;
-	
-	public ElasticSearchIndexRequestBuilder(String indexName, String typeName) {
-		this.indexName = indexName;
-		this.typeName = typeName;
-	}
-	
-	private static final long serialVersionUID = 1L;
+  private String indexName;
+  private String typeName;
 
-    public IndexRequest createIndexRequest(Map<String, Object> element) {
+  public ElasticSearchIndexRequestBuilder(String indexName, String typeName) {
+    this.indexName = indexName;
+    this.typeName = typeName;
+  }
 
-        return Requests.indexRequest()
-                .index(indexName)
-                .type(typeName)
-                .source(element);
-    }
+  private static final long serialVersionUID = 1L;
 
-	@Override
-	public void process(Map<String, Object> stringObjectMap, RuntimeContext runtimeContext, RequestIndexer requestIndexer) {
-		requestIndexer.add(createIndexRequest(stringObjectMap));
-	}
+  private IndexRequest createIndexRequest(Map<String, Object> element) {
+
+    return Requests.indexRequest()
+            .index(indexName)
+            .type(typeName)
+            .source(element);
+  }
+
+  @Override
+  public void process(Map<String, Object> stringObjectMap, RuntimeContext runtimeContext, RequestIndexer requestIndexer) {
+    requestIndexer.add(createIndexRequest(stringObjectMap));
+  }
 }
