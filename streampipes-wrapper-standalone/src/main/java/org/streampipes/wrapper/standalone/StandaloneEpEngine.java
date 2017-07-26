accf15d72fa189e3ab5ff51cab86be8bbcb6d73b
Dominik Riemer <riemer@fzi.de>
2017-07-26 14:40:53 +0200

    added standalone ep engine

---
+++ streampipes-wrapper-standalone/src/main/java/org/streampipes/wrapper/standalone/StandaloneEpEngine.java
@@ -0,0 +1,45 @@
+package org.streampipes.wrapper.standalone;
+
+import org.streampipes.model.impl.graph.SepaInvocation;
+import org.streampipes.wrapper.BindingParameters;
+import org.streampipes.wrapper.EPEngine;
+import org.streampipes.wrapper.EngineParameters;
+import org.streampipes.wrapper.OutputCollector;
+
+import java.util.Map;
+import java.util.Optional;
+
+/**
+ * Created by riemer on 26.07.2017.
+ */
+public abstract class StandaloneEpEngine<B extends BindingParameters> implements EPEngine<B> {
+
+  private Optional<OutputCollector> collectorOpt;
+
+  protected SepaInvocation graph;
+
+  @Override
+  public void bind(EngineParameters<B> parameters, OutputCollector collector, SepaInvocation graph) {
+    this.collectorOpt = Optional.of(collector);
+    onInvocation(parameters, graph);
+  }
+
+  @Override
+  public void onEvent(Map<String, Object> event, String sourceInfo) {
+      if (collectorOpt.isPresent()) {
+        onEvent(event, sourceInfo, collectorOpt.get());
+      } else {
+        throw new IllegalArgumentException("");
+      }
+  }
+
+  @Override
+  public void discard() {
+    this.collectorOpt = Optional.empty();
+  }
+
+  public abstract void onInvocation(EngineParameters<B> params, SepaInvocation graph);
+
+  public abstract void onEvent(Map<String, Object> event, String sourceInfo, OutputCollector
+          collector);
+}
