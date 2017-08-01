dca622e668c4f2588adf0259bdac920916be5403
Philipp Zehnder <zehnder@fzi.de>
2017-08-01 09:39:10 +0200

    Configured StreamPipes for the Testfeld instacne

--- webpack.config.js
+++ webpack.config.js
@@ -42,27 +42,27 @@ let config = {
         contentBase: `${__dirname}/`,
         port: 8082,
         proxy: {
-            '/streampipes-backend': {
-                target: 'http://localhost:8030',
-                secure: false
-            },
-            '/visualizablepipeline': {
-                target: 'http://localhost:5984',
-                secure: false
-            },
-            '/dashboard': {
-                target: 'http://localhost:5984',
-                secure: false
-            },
-            '/pipeline': {
-                target: 'http://localhost:5984',
-                secure: false
-            },
-            '/streampipes/ws': {
-                target: 'ws://ipe-koi04.fzi.de:61614',
-                ws: true,
-                secure: false
-            }
+						'/streampipes-backend': {
+								target: 'http://localhost:8030',
+								secure: false
+						},
+						'/visualizablepipeline': {
+								target: 'http://localhost:5984',
+								secure: false
+						},
+						'/dashboard': {
+								target: 'http://localhost:5984',
+								secure: false
+						},
+						'/pipeline': {
+								target: 'http://localhost:5984',
+								secure: false
+						},
+						'/streampipes/ws': {
+								target: 'ws://ipe-koi15.fzi.de:61614',
+								ws: true,
+								secure: false
+						}
         }
         //inline: true
     },
