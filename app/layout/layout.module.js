503d6a64e391dcf50315b0dc572e4fd3eef80562
Philipp Zehnder <zehnder@fzi.de>
2017-09-26 15:48:43 +0200

    Removed old header navbar

--- app/layout/layout.module.js
+++ app/layout/layout.module.js
@@ -2,18 +2,18 @@ import angular from 'npm/angular';
 import ngCookies from 'npm/angular-cookies';
 
 import spServices from '../services/services.module'
-import TopNavCtrl from './top-nav.controller'
 import AppCtrl from './app.controller'
 import LeftCtrl from './left.controller'
 import SettingsCtrl from './settings.controller'
 
-import iframeAutoSize from './iframe-auto-size.directive'
-import ngRightClick from './ng-right-click.directive'
-import userAvatar from './user-avatar.directive'
-
 export default angular.module('sp.layout', [spServices, ngCookies])
-	.controller('TopNavCtrl', TopNavCtrl)
 	.controller('AppCtrl', AppCtrl)
+    .component('heroDetail', {
+        templateUrl: 'heroDetail.html',
+        bindings: {
+            hero: '='
+        }
+    })
 	.controller('LeftCtrl', LeftCtrl)
 	.controller('SettingsCtrl', SettingsCtrl)
 	.directive('iframeAutoSize', SettingsCtrl)
