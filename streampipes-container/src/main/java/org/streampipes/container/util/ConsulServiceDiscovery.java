bc22e2c4adde502928efd37731d338b1a63f0cd3
tex <tex@fzi.de>
2017-11-10 13:00:33 +0100

    Work in process

--- streampipes-container/src/main/java/org/streampipes/container/util/ConsulServiceDiscovery.java
+++ streampipes-container/src/main/java/org/streampipes/container/util/ConsulServiceDiscovery.java
@@ -4,15 +4,29 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
+import com.orbitz.consul.AgentClient;
+import com.orbitz.consul.Consul;
+import com.orbitz.consul.HealthClient;
+import com.orbitz.consul.cache.ConsulCache;
+import com.orbitz.consul.cache.ServiceHealthCache;
+import com.orbitz.consul.cache.ServiceHealthKey;
+import com.orbitz.consul.model.agent.Agent;
+import com.orbitz.consul.model.health.ServiceHealth;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.util.List;
+import java.util.Map;
 
 public class ConsulServiceDiscovery {
 
     private static final String HEALTH_CHECK_INTERVAL = "10s";
     //private static final String HEALTH_CHECK_TTL = "15s";
 
-    private static final String CONSUL_URL_REST_API = "http://localhost:8500/v1/";
+
+    //TODO: Change URL
+    private static final String CONSUL_URL = "http://localhost:8500/";
+    private static final String CONSUL_URL_REST_API = CONSUL_URL + "v1/";
     private static final String CONSUL_URL_REGISTER_SERVICE = CONSUL_URL_REST_API + "agent/service/register";
 
     static Logger LOG = LoggerFactory.getLogger(ConsulServiceDiscovery.class);
@@ -29,6 +43,32 @@ public class ConsulServiceDiscovery {
         } catch (UnirestException e) {
             LOG.error("Register Service: " + serviceName, " - " + e.toString());
         }
+    }
+
+    public static void subcribeHealthService() {
+        Consul consul = Consul.builder().withUrl(CONSUL_URL).build();
+        HealthClient healthClient = consul.healthClient();
+        Agent agent = consul.agentClient().getAgent();
+
+        String serviceName = "pe-flink-samples";
+
+        ServiceHealthCache svHealth = ServiceHealthCache.newCache(healthClient, serviceName);
+
+        svHealth.addListener(new ConsulCache.Listener<ServiceHealthKey, ServiceHealth>() {
+            @Override
+            public void notify(Map<ServiceHealthKey, ServiceHealth> map) {
+                System.out.println("ad");
+            }
+        });
+    }
+
+    //Test
+    public static void getActivePEServicesRdfEndPoints() {
+        Consul consul = Consul.builder().withUrl(CONSUL_URL).build();
+        HealthClient healthClient = consul.healthClient();
+
+        List<ServiceHealth> nodes = healthClient.getHealthyServiceInstances("pe-flink-samples").getResponse();
+        System.out.println("ad");
     }
 
     private static int registerServiceHttpClient(String body) throws UnirestException {
--- streampipes-container/src/test/java/org/streampipes/container/util/TestConsulServiceDiscovery.java
+++ streampipes-container/src/test/java/org/streampipes/container/util/TestConsulServiceDiscovery.java
@@ -3,12 +3,18 @@ package org.streampipes.container.util;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import org.junit.Test;
 
+import static org.streampipes.container.util.ConsulServiceDiscovery.getActivePEServicesRdfEndPoints;
+import static org.streampipes.container.util.ConsulServiceDiscovery.subcribeHealthService;
+
 public class TestConsulServiceDiscovery {
 
      public static void main(String[] args) throws UnirestException {
-        ConsulServiceDiscovery.registerPeService("t2",
+     /*   ConsulServiceDiscovery.registerPeService("t2",
                                                     "t2",
                                                         "http://141.21.14.94",
                                                         8090);
+                                                        */
+         //getActivePEServicesRdfEndPoints();
+         subcribeHealthService();
     }
 }
