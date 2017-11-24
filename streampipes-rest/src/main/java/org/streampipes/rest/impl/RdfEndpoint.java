b48d1f257400babaf259f19edb03fc803f1414b2
Dominik Riemer <riemer@fzi.de>
2017-11-24 17:45:09 +0100

    added endpoints to get items rest interface

--- streampipes-rest/src/main/java/org/streampipes/rest/impl/RdfEndpoint.java
+++ streampipes-rest/src/main/java/org/streampipes/rest/impl/RdfEndpoint.java
@@ -7,9 +7,6 @@ import org.streampipes.rest.annotation.GsonWithIds;
 import org.streampipes.rest.api.IRdfEndpoint;
 import org.streampipes.storage.controller.StorageManager;
 
-import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
-import javax.ws.rs.core.Response;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.LinkedList;
@@ -17,93 +14,103 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 /**
  * Created by riemer on 05.10.2016.
  */
 @Path("/v2/users/{username}/rdfendpoints")
 public class RdfEndpoint extends AbstractRestInterface implements IRdfEndpoint {
 
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @GsonWithIds
-    @Override
-    public Response getAllEndpoints() {
-        //TODO: return the endpoint of passing services
-        List<String> endpoints = ConsulUtil.getActivePEServicesEndPoints();
-        List<org.streampipes.model.client.endpoint.RdfEndpoint> servicerdRdfEndpoints = new LinkedList<>();
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @GsonWithIds
+  @Override
+  public Response getAllEndpoints() {
+    //TODO: return the endpoint of passing services
+    return ok(getEndpoints());
+  }
 
-        for(String endpoint: endpoints ) {
-            org.streampipes.model.client.endpoint.RdfEndpoint rdfEndpoint =
-                    new org.streampipes.model.client.endpoint.RdfEndpoint(endpoint);
-            servicerdRdfEndpoints.add(rdfEndpoint);
-        }
-        List<org.streampipes.model.client.endpoint.RdfEndpoint> databasedRdfEndpoints = StorageManager
-                .INSTANCE
-                .getRdfEndpointStorage()
-                .getRdfEndpoints();
+  @POST
+  @Produces(MediaType.APPLICATION_JSON)
+  @GsonWithIds
+  @Override
+  public Response addRdfEndpoint(org.streampipes.model.client.endpoint.RdfEndpoint rdfEndpoint) {
+    StorageManager.INSTANCE
+            .getRdfEndpointStorage()
+            .addRdfEndpoint(rdfEndpoint);
 
-        List<org.streampipes.model.client.endpoint.RdfEndpoint> concatList =
-                Stream.of(databasedRdfEndpoints, servicerdRdfEndpoints)
-                .flatMap(Collection::stream)
-                .collect(Collectors.toList());
+    return Response.status(Response.Status.OK).build();
+  }
 
-        return ok(concatList);
+
+  @DELETE
+  @Path("/{rdfEndpointId}")
+  @Produces(MediaType.APPLICATION_JSON)
+  @GsonWithIds
+  @Override
+  public Response removeRdfEndpoint(@PathParam("rdfEndpointId") String rdfEndpointId) {
+    StorageManager.INSTANCE
+            .getRdfEndpointStorage()
+            .removeRdfEndpoint(rdfEndpointId);
+
+    return Response.status(Response.Status.OK).build();
+  }
+
+  @GET
+  @Path("/items")
+  @Produces(MediaType.APPLICATION_JSON)
+  @GsonWithIds
+  @Override
+  public Response getEndpointContents(@PathParam("username") String username) {
+    List<org.streampipes.model.client.endpoint.RdfEndpoint> endpoints = getEndpoints();
+
+    List<RdfEndpointItem> items = Operations.getEndpointUriContents(endpoints);
+    items.forEach(item -> item.setInstalled(isInstalled(item.getUri(), username)));
+    return ok(items);
+
+  }
+
+  private List<org.streampipes.model.client.endpoint.RdfEndpoint> getEndpoints() {
+    List<String> endpoints = ConsulUtil.getActivePEServicesEndPoints();
+    List<org.streampipes.model.client.endpoint.RdfEndpoint> servicerdRdfEndpoints = new LinkedList<>();
+
+    for (String endpoint : endpoints) {
+      org.streampipes.model.client.endpoint.RdfEndpoint rdfEndpoint =
+              new org.streampipes.model.client.endpoint.RdfEndpoint(endpoint);
+      servicerdRdfEndpoints.add(rdfEndpoint);
     }
+    List<org.streampipes.model.client.endpoint.RdfEndpoint> databasedRdfEndpoints = StorageManager
+            .INSTANCE
+            .getRdfEndpointStorage()
+            .getRdfEndpoints();
 
-    @POST
-    @Produces(MediaType.APPLICATION_JSON)
-    @GsonWithIds
-    @Override
-    public Response addRdfEndpoint(org.streampipes.model.client.endpoint.RdfEndpoint rdfEndpoint) {
-        StorageManager.INSTANCE
-                .getRdfEndpointStorage()
-                .addRdfEndpoint(rdfEndpoint);
+    List<org.streampipes.model.client.endpoint.RdfEndpoint> concatList =
+            Stream.of(databasedRdfEndpoints, servicerdRdfEndpoints)
+                    .flatMap(Collection::stream)
+                    .collect(Collectors.toList());
 
-        return Response.status(Response.Status.OK).build();
-    }
+    return concatList;
+  }
 
+  private boolean isInstalled(String elementUri, String username) {
+    return getAllUserElements(username)
+            .stream()
+            .anyMatch(e -> e.equals(elementUri));
+  }
 
-    @DELETE
-    @Path("/{rdfEndpointId}")
-    @Produces(MediaType.APPLICATION_JSON)
-    @GsonWithIds
-    @Override
-    public Response removeRdfEndpoint(@PathParam("rdfEndpointId") String rdfEndpointId) {
-        StorageManager.INSTANCE
-                .getRdfEndpointStorage()
-                .removeRdfEndpoint(rdfEndpointId);
-
-        return Response.status(Response.Status.OK).build();
-    }
-
-    @GET
-    @Path("/items")
-    @Produces(MediaType.APPLICATION_JSON)
-    @GsonWithIds
-    @Override
-    public Response getEndpointContents(@PathParam("username") String username) {
-        List<org.streampipes.model.client.endpoint.RdfEndpoint> endpoints = StorageManager
-                .INSTANCE
-                .getRdfEndpointStorage()
-                .getRdfEndpoints();
-
-        List<RdfEndpointItem> items = Operations.getEndpointUriContents(endpoints);
-        items.forEach(item -> item.setInstalled(isInstalled(item.getUri(), username)));
-        return ok(items);
-
-    }
-
-    private boolean isInstalled(String elementUri, String username) {
-        return getAllUserElements(username)
-                .stream()
-                .anyMatch(e -> e.equals(elementUri));
-    }
-
-    private List<String> getAllUserElements(String username) {
-        List<String> elementUris = new ArrayList<>();
-        elementUris.addAll(getUserService().getOwnSourceUris(username));
-        elementUris.addAll(getUserService().getOwnActionUris(username));
-        elementUris.addAll(getUserService().getOwnSepaUris(username));
-        return elementUris;
-    }
+  private List<String> getAllUserElements(String username) {
+    List<String> elementUris = new ArrayList<>();
+    elementUris.addAll(getUserService().getOwnSourceUris(username));
+    elementUris.addAll(getUserService().getOwnActionUris(username));
+    elementUris.addAll(getUserService().getOwnSepaUris(username));
+    return elementUris;
+  }
 }
