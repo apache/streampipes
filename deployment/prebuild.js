87053d2abc44f99f69371c4a03017e8f0115ae6b
Samuel Abt <abt@fzi.de>
2017-11-30 17:45:48 +0100

    test push

--- deployment/prebuild.js
+++ deployment/prebuild.js
@@ -2,9 +2,15 @@ yaml = require('js-yaml');
 fs   = require('fs');
 
 try {
-    console.log(process.env.CI_COMMIT_REF_NAME)
-    const config = yaml.safeLoad(fs.readFileSync('deployment/' + process.env.CI_COMMIT_REF_NAME + '/config.yml', 'utf8'));
-    console.log('It works');
+    const branchName = process.env.CI_COMMIT_REF_NAME;
+    const config = yaml.safeLoad(fs.readFileSync('deployment/' + branchName + '/config.yml', 'utf8'));
+    console.log('Valid Config-File');
+    console.log('Current Branch: ' + branchName);
+
+    fs.writeFileSync('img/login/background.png', fs.readFileSync(config['loginBackgroundImage']));
+    fs.writeFileSync('img/login/logo.png', fs.readFileSync(config['logo']));
+    console.log('Moved Images')
+
 } catch (e) {
     console.log(e);
 }
