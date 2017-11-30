cce3fd3df1cc3ffd0e1e9d483aa5d13ae0d3d2df
Samuel Abt <abt@fzi.de>
2017-11-30 17:28:59 +0100

    removed unnecessary environment variable

--- deployment/prebuild.js
+++ deployment/prebuild.js
@@ -2,8 +2,8 @@ yaml = require('js-yaml');
 fs   = require('fs');
 
 try {
-    console.log(process.env.BRANCH_NAME)
-    const config = yaml.safeLoad(fs.readFileSync('deployment/' + process.env.BRANCH_NAME + '/config.yml', 'utf8'));
+    console.log(process.env.CI_COMMIT_REF_NAME)
+    const config = yaml.safeLoad(fs.readFileSync('deployment/' + process.env.CI_COMMIT_REF_NAME + '/config.yml', 'utf8'));
     console.log('It works');
 } catch (e) {
     console.log(e);
