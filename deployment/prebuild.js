e7cb4ee892097fdb4f6842da0119ae50172b408b
Samuel Abt <abt@fzi.de>
2017-12-01 16:33:43 +0100

    test push

--- deployment/prebuild.js
+++ deployment/prebuild.js
@@ -1,16 +1,34 @@
 yaml = require('js-yaml');
-fs   = require('fs');
+fs = require('fs');
 
-try {
-    const branchName = process.env.CI_COMMIT_REF_NAME;
-    const config = yaml.safeLoad(fs.readFileSync('deployment/' + branchName + '/config.yml', 'utf8'));
-    console.log('Valid Config-File');
-    console.log('Current Branch: ' + branchName);
+console.log('Pre-Build Script started.');
 
-    fs.writeFileSync('img/login/background.png', fs.readFileSync(config['loginBackgroundImage']));
-    fs.writeFileSync('img/login/logo.png', fs.readFileSync(config['logo']));
-    console.log('Moved Images')
+let branchName = process.env.CI_COMMIT_REF_NAME;
 
-} catch (e) {
-    console.log(e);
+if (branchName === undefined) {
+    console.log('Environment Variable invalid. Using Config for dev-Branch.');
+    branchName = 'dev';
+} else {
+    console.log('Environment Variable valid. Using Config for ' + branchName + '-Branch.');
 }
+
+if (!fs.existsSync('deployment/' + branchName + '/config.yml', 'utf8')) {
+    console.log('Could not find Config for ' + branchName + '-Branch. Using Config for dev-Branch.');
+    branchName = 'dev';
+}
+
+let config = {};
+try {
+    config = yaml.safeLoad(fs.readFileSync('deployment/' + branchName + '/config.ym', 'utf8'));
+} catch (error) {
+    console.log('Invalid Config-File. Pre-Build failed.');
+    process.exit(1);
+}
+
+console.log(config);
+
+/*
+fs.writeFileSync('img/login/background.png', fs.readFileSync(config['loginBackgroundImage']));
+fs.writeFileSync('img/login/logo.png', fs.readFileSync(config['logo']));
+console.log('Moved Images')
+*/
