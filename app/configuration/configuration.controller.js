aeb308de3b639aa1c51296930679141b6512a437
Samuel Abt <abt@fzi.de>
2017-12-05 16:51:46 +0100

    fix ValueType mismatch in Configuration

--- app/configuration/configuration.controller.js
+++ app/configuration/configuration.controller.js
@@ -8,12 +8,27 @@ export default function ConfigurationCtrl($scope, ConfigurationRestService) {
 
     $scope.getConfigurations = function () {
         ConfigurationRestService.get().then(function (services) {
+            for (let service of services.data) {
+                for (let config of service.configs) {
+                    if (config.valueType === 'xs:integer') {
+                        config.value = parseInt(config.value);
+                    } else if (config.valueType === 'xs:double') {
+                        config.value = parseFloat('xs:double');
+                    } else if (config.valueType === 'xs:boolean') {
+                        config.value = (config.value === 'true');
+                    }
+                }
+            }
             $scope.services = services.data
         });
     }
 
     $scope.updateConfiguration = function (serviceDetails) {
-        ConfigurationRestService.update(serviceDetails).then(function (response) {
+        let serviceDetailsToString = angular.copy(serviceDetails);
+        for (let config of serviceDetailsToString.configs) {
+            config.value = config.value + '';
+        }
+        ConfigurationRestService.update(serviceDetailsToString).then(function (response) {
 
         });
     }
