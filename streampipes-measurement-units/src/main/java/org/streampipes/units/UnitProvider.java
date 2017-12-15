e8d1dd0dbb5b933b1fafba984d0086af873ca357
tex <tex@fzi.de>
2017-12-15 11:18:27 +0100

    Add Tests for measurementUnitConverter add logs in jqudt

--- streampipes-measurement-units/src/main/java/org/streampipes/units/UnitProvider.java
+++ streampipes-measurement-units/src/main/java/org/streampipes/units/UnitProvider.java
@@ -2,14 +2,19 @@ package org.streampipes.units;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.NoSuchElementException;
 
 import com.github.jqudt.Unit;
 import com.github.jqudt.onto.UnitFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public enum UnitProvider {
 
 	INSTANCE;
-	
+
+	static Logger LOG = LoggerFactory.getLogger(UnitCollector.class);
+
 	private List<Unit> availableUnitTypes = new ArrayList<>();
 	private List<Unit> availableUnits = new ArrayList<>();
 	
@@ -35,9 +40,15 @@ public enum UnitProvider {
 	}
 
 	public Unit getUnitByLabel(String label) {
-		return availableUnits.stream()
-				.filter(unit -> unit.getLabel().equals(label))
-				.findFirst()
-				.get();
+		try {
+			return availableUnits.stream()
+					.filter((Unit unit) -> unit.getLabel().equals(label))
+					.findFirst()
+					.get();
+		} catch (NoSuchElementException e) {
+			LOG.error("No unit with label \"" + label + "\" found");
+			return null;
+		}
+
 	}
 }
