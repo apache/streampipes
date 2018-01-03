be7fa031997265e7d268e8c28f2529ae7f120c64
Dominik Riemer <riemer@fzi.de>
2018-01-03 21:21:38 +0100

    testing stopping of flink jobs instead of cancelling them

--- streampipes-wrapper-flink/src/main/java/org/streampipes/wrapper/flink/FlinkJobController.java
+++ streampipes-wrapper-flink/src/main/java/org/streampipes/wrapper/flink/FlinkJobController.java
@@ -13,7 +13,6 @@ import org.apache.flink.runtime.instance.ActorGateway;
 import org.apache.flink.runtime.jobgraph.JobStatus;
 import org.apache.flink.runtime.leaderretrieval.LeaderRetrievalService;
 import org.apache.flink.runtime.messages.JobManagerMessages;
-import org.apache.flink.runtime.messages.JobManagerMessages.CancelJob;
 import org.apache.flink.runtime.messages.JobManagerMessages.RunningJobsStatus;
 import org.apache.flink.runtime.util.LeaderRetrievalUtils;
 import scala.Some;
@@ -93,7 +92,7 @@ public class FlinkJobController {
 		
 		try {
 			ActorGateway jobManager = getJobManagerGateway();
-			Future<Object> response = jobManager.ask(new CancelJob(jobId), askTimeout);
+			Future<Object> response = jobManager.ask(new JobManagerMessages.StopJob(jobId), askTimeout);
 			Await.result(response, askTimeout);
 			return true;
 		}
