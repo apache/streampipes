211eb494bf6acdec5d7af55a3a9f6b7053dbaf3f
Samuel Abt <abt@fzi.de>
2018-02-02 16:28:20 +0100

    refactor HomeModule

--- app/home/home.module.js
+++ app/home/home.module.js
@@ -1,7 +1,7 @@
-import angular from 'npm/angular';
+import angular from 'angular';
 
-import { HomeCtrl } from './home.controller'
-import { HomeService} from "./home.service";
+import {HomeCtrl} from './home.controller'
+import {HomeService} from "./home.service";
 
 export default angular.module('sp.home', [])
 	.controller('HomeCtrl', HomeCtrl)
