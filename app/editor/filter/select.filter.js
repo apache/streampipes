f4b22e546c6eefdbe6c48096f5ddbe9de233b934
Dominik Riemer <riemer@fzi.de>
2018-02-27 23:31:39 +0100

    add basic support for pe-provided static properties, several bug fixes after refactoring

--- app/editor/filter/select.filter.js
+++ app/editor/filter/select.filter.js
@@ -4,6 +4,9 @@ class SelectFilter {
         return (pipelineElements, selectedOptions) => {
             var filteredElements = [];
             angular.forEach(pipelineElements, pe => {
+                if (pe.category.length === 0) {
+                    filteredElements.push(pe);
+                }
                 angular.forEach(selectedOptions, so => {
                     if (pe.category.indexOf(so) !== -1) {
                         filteredElements.push(pe);
