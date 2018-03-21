d166e41a2fcb5387e27eda0725fabe611312f883
Samuel Abt <abt@fzi.de>
2018-03-21 16:26:32 +0100

    fix dialog-builder

--- src/app/editor/services/editor-dialog-manager.service.ts
+++ src/app/editor/services/editor-dialog-manager.service.ts
@@ -1,5 +1,7 @@
 import * as angular from 'angular';
 
+declare const require: any;
+
 import {CustomizeController} from "../dialog/customize-pipeline-element/customize.controller";
 import {MatchingErrorController} from "../dialog/matching-error/matching-error.controller";
 import {TopicSelectionDialog} from "../dialog/topic/topic-selection-modal.controller";
@@ -19,7 +21,7 @@ export class EditorDialogManager {
     }
 
     showMatchingErrorDialog(elementData) {
-        var dialogContent = this.DialogBuilder.getDialogTemplate(MatchingErrorController, 'app/editor/dialog/matching-error/matching-error.tmpl.html');
+        var dialogContent = this.DialogBuilder.getDialogTemplate(MatchingErrorController, require('../dialog/matching-error/matching-error.tmpl.html'));
         dialogContent.locals = {
             elementData: elementData
         }
@@ -27,7 +29,7 @@ export class EditorDialogManager {
     }
 
     showCustomizeDialog(elementData, sourceEndpoint, sepa) {
-        var dialogContent = this.DialogBuilder.getDialogTemplate(CustomizeController, 'app/editor/dialog/customize-pipeline-element/customizeElementDialog.tmpl.html');
+        var dialogContent = this.DialogBuilder.getDialogTemplate(CustomizeController, require('../dialog/customize-pipeline-element/customizeElementDialog.tmpl.html'));
         dialogContent.locals = {
             elementData: elementData,
             sourceEndpoint: sourceEndpoint,
@@ -37,7 +39,7 @@ export class EditorDialogManager {
     };
 
     showCustomizeStreamDialog(streamDescription) {
-        var dialogContent = this.DialogBuilder.getDialogTemplate(TopicSelectionDialog, 'app/editor/dialog/topic/topic-selection-modal.tmpl.html');
+        var dialogContent = this.DialogBuilder.getDialogTemplate(TopicSelectionDialog, require('../dialog/topic/topic-selection-modal.tmpl.html'));
         dialogContent.locals = {
             streamDescription: streamDescription
         }
@@ -45,7 +47,7 @@ export class EditorDialogManager {
     }
 
     showSavePipelineDialog(pipelineNew) {
-        var dialogContent = this.DialogBuilder.getDialogTemplate(SavePipelineController, 'app/editor/dialog/save-pipeline/submitPipelineModal.tmpl.html');
+        var dialogContent = this.DialogBuilder.getDialogTemplate(SavePipelineController, require('../dialog/save-pipeline/submitPipelineModal.tmpl.html'));
         dialogContent.locals = {
             pipeline: pipelineNew
         }
