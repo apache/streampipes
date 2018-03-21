99d371ad2c27c30d37c494614194de2fc5c45e90
Dominik Riemer <riemer@fzi.de>
2018-03-21 16:49:34 +0100

    add image visualization

--- src/app/dashboard/templates/image/image.directive.ts
+++ src/app/dashboard/templates/image/image.directive.ts
@@ -14,6 +14,7 @@ export default function imageWidget(WidgetInstances) {
         controller: function ($scope) {
             WidgetInstances.get($scope.widgetId).then(function(data) {
                 $scope.widgetConfig = data.visualisation.schema.config;
+                $scope.selectedImageMapping = data.visualisation.schema.selectedNumberProperty.properties.runtimeName;
 
             })
 
