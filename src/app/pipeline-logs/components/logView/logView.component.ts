862db83386c380f50edd1b43b283b1508011c58d
tex <tex@fzi.de>
2018-03-26 17:19:50 +0200

    Implements LogView

--- src/app/pipeline-logs/components/logView/logView.component.ts
+++ src/app/pipeline-logs/components/logView/logView.component.ts
@@ -1,14 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
     selector: 'logView',
     templateUrl: './logView.component.html',
     styleUrls: ['./logView.component.css']
 })
-export class LogViewComponent {
+export class LogViewComponent  {
+
+    startDate = new Date();
+    endDate = new Date();
+
+    public logs = [{"timestamp": "12.12.12", "level": "INFO", "type":"USERLOG", "message": "Test"},
+        {"timestamp": "12.12.12", "level": "INFO", "type":"USERLOG", "message": "Test"},
+        {"timestamp": "12.12.12", "level": "INFO", "type":"USERLOG", "message": "Test"},
+        {"timestamp": "12.12.12", "level": "INFO", "type":"USERLOG", "message": "Test"},
+        {"timestamp": "12.12.12", "level": "INFO", "type":"USERLOG", "message": "T asdfasd fs dfa sfas sa dfas fas d sdfa est"},
+        {"timestamp": "12.12.12", "level": "INFO", "type":"USERLOG", "message": "Tesasd fas fasd fasd fas dfast"},
+        {"timestamp": "12.12.12", "level": "INFO123213", "type":"USERLOG", "message": "Test"},
+        {"timestamp": "12.12.12", "level": "INFO", "type":"USERLOG", "message": "Test33333 asdfasdfasdf asdf as fs"},
+        {"timestamp": "12.12.12", "level": "INFO", "type":"USERLOG", "message": "Test"},
+        {"timestamp": "12.12.12", "level": "ERROR", "type":"USERLOG", "message": "Test"},
+        {"timestamp": "12.12.12", "level": "INFO", "type":"USERLOG", "message": "Test"},
+        {"timestamp": "12.12.12", "level": "ERROR", "type":"USERLOG", "message": "Teddasdfas das dfas da sdfst"},{"timestamp": "12.12.12", "level": "INFO", "type":"USERLOG", "message": "Test"},
+        {"timestamp": "12.12.12", "level": "INFO", "type":"USERLOG", "message": "Test"},
+        {"timestamp": "12.12.12", "level": "INFO", "type":"USERLOG", "message": "T asdfas dfasd fsad as dfas dfasfest"},
+        {"timestamp": "12.12.12", "level": "INFO", "type":"USERLOG", "message": "Test"},
+
+        {"timestamp": "12.12.12", "level": "INFO", "type":"USERLOG", "message": "Test"}]
 
     constructor() {
 
     }
+
+    load() {
+
+    }
+
+
+
+
+
 
 }
--- src/app/pipeline-logs/pipeline-logs.module.ts
+++ src/app/pipeline-logs/pipeline-logs.module.ts
@@ -3,8 +3,18 @@ import { NgModule } from '@angular/core';
 import { PipelineLogsComponent } from './pipeline-logs.component';
 import { LogViewRestService } from './components/logView/services/logView-rest.service';
 import { LogViewComponent } from './components/logView/logView.component';
+import { MatIconModule, MatInputModule} from '@angular/material';
+import { NguiDatetimePickerModule } from '@ngui/datetime-picker';
+import { BrowserModule } from '@angular/platform-browser';
+
 
 @NgModule({
+    imports: [
+        BrowserModule,
+        MatInputModule,
+        MatIconModule,
+        NguiDatetimePickerModule
+    ],
     declarations: [
         PipelineLogsComponent,
         LogViewComponent
