6a1df9c109abc1d508602ea13b5731d624177d2c
Philipp Zehnder <zehnder@fzi.de>
2018-04-13 08:58:22 +0200

    Updated list matcher

--- streampipes-pipeline-management/src/main/java/org/streampipes/manager/matching/v2/ListPropertyMatch.java
+++ streampipes-pipeline-management/src/main/java/org/streampipes/manager/matching/v2/ListPropertyMatch.java
@@ -20,17 +20,24 @@ package org.streampipes.manager.matching.v2;
 import org.streampipes.model.client.matching.MatchingResultMessage;
 import org.streampipes.model.schema.EventPropertyList;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ListPropertyMatch implements Matcher<EventPropertyList, EventPropertyList> {
 
 	@Override
 	public boolean match(EventPropertyList offer, EventPropertyList requirement, List<MatchingResultMessage> errorLog) {
-		if (requirement.getEventProperties() == null || requirement.getEventProperties().size() == 0) {
+		if (requirement.getEventProperties() == null) {
 			return false;
+		} else if (requirement.getEventProperties().size() == 0) {
+			if (requirement.getDomainProperties() != null && offer.getDomainProperties() != null && requirement.getDomainProperties().size() > 0) {
+				return new DomainPropertyMatch().match(offer.getDomainProperties(), requirement.getDomainProperties(), errorLog);
+			} else {
+				return false;
+			}
 		} else {
 			return new PropertyMatch().match(offer.getEventProperties().get(0),
-							requirement.getEventProperties().get(0), errorLog);
+					requirement.getEventProperties().get(0), errorLog);
 		}
 	}
 }
