0e634d6e274aa6079915e02a49c5d281c707b38d
tex <tex@fzi.de>
2018-04-25 16:37:45 +0200

    Bug Fix

--- streampipes-logging/src/main/java/org/streampipes/logging/impl/EventStatisticLogger.java
+++ streampipes-logging/src/main/java/org/streampipes/logging/impl/EventStatisticLogger.java
@@ -2,19 +2,21 @@ package org.streampipes.logging.impl;
 
 import org.slf4j.LoggerFactory;
 
-public class EventStatisticLogger {
+import java.io.Serializable;
+
+public class EventStatisticLogger implements Serializable {
 
 
     private String prefix;
 
    // public static void log(org.streampipes.model.base.InvocableStreamPipesEntity graph) {
     public static void log(String name, String correspondingPipeline, String source) {
-        String prefix =  "SYSTEMLOG - EVENT STATISTIC" + " - "
+        String prefix =  "SYSTEMLOG EVENT STATISTIC" + " - "
                 // + "serviceName: " + peConfig.getName() + " - "
                 + "correspondingPipeline: " + correspondingPipeline + " - "
                 + "peURI: " + source + " - ";
 
-        org.slf4j.Logger logger = LoggerFactory.getLogger(name);
+        org.slf4j.Logger logger = LoggerFactory.getLogger(name.replaceAll("\\s+",""));
         logger.info(prefix + 1);
 
     }
