bc88c2edd754cb4d69044af18c64d1d10746e16f
tex <tex@fzi.de>
2018-06-12 18:34:43 +0200

    remove the pipeline-log component

--- src/app/pipeline-logs/components/logView/model/log.model.ts
+++
@@ -1,7 +0,0 @@
-export interface Log {
-    timestamp: string;
-    level: string;
-    type: string;
-    message: string;
-    sourceID: string;
-}
--- src/app/pipeline-logs/components/logView/model/logRequest.model.ts
+++
@@ -1,5 +0,0 @@
-export interface LogRequest {
-    sourceID: string;
-    dateFrom;
-    dateTo;
-}
