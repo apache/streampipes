79bbec39346f2307bf67d14ab68d96896fffceff
Constantin Merbecks <constantin.merbecks@gmail.com>
2018-07-04 16:35:39 +0200

    Cypress.cookie.debug added, Cypress version 3.0.2, authetication problem not fixed

--- cypress/integration/login/login.spec.js
+++ cypress/integration/login/login.spec.js
@@ -11,25 +11,29 @@ describe('Login Streampipes', function () {
     });
     it('Start Login', function () {
         cy.get('button').contains('Login').parent().click();
+        Cypress.Cookies.debug(true)
     });
     it('Go to Pipeline Editor', function() {
         //cy.wait(10000);
+        Cypress.Cookies.debug(true)
         cy.get('#ic_dashboard_24px_cache14').parent().click();
     });
     it('Cancel User Guide', function() {
-        cy.wait(10000);  
+        Cypress.Cookies.debug(true)
+        cy.wait(5000);  
         cy.get('.ng-binding.ng-scope').contains('Cancel').parent().click();
         
     });
     it('Drag n Drop', function() {
-        var images = Cypress.$("img[src$='http://141.21.12.190/assets/img/pe_icons/icon-flowrate-1.png']")
-        console.log(images);
-        images.parent().parent().parent().click();
+        Cypress.Cookies.debug(true)
+        // var images = Cypress.$("img[src$='http://141.21.12.190/assets/img/pe_icons/icon-flowrate-1.png']")
+        // console.log(images);
+        // images.parent().parent().parent().click();
         // cy.get('button').contains("Select categories").click();
         // cy.get('button').contains("Vehicle Source").click({force: true});
-        cy.get('pipeline-element').contains("VP")
-        .trigger('mousedown',0,0, {force: true})
-        .trigger('mousemove', 119, 300, {force: true})
-        .trigger('mouseup', 119, 300, {force: true});
+        cy.get('pipeline-element').contains("VP").parent().parent()
+        .trigger('mousedown',0,0)
+        .trigger('mousemove', 119, 300)
+        .trigger('mouseup', 119, 300);
     })
 });
