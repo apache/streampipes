dbee8e3ac45e91594e6f02ec7bc441aa2876d875
Dominik Riemer <riemer@fzi.de>
2018-08-22 15:00:02 +0200

    Add more features to test utils

--- streampipes-test-utils/src/main/java/org/streampipes/test/generator/EventStreamGenerator.java
+++ streampipes-test-utils/src/main/java/org/streampipes/test/generator/EventStreamGenerator.java
@@ -17,8 +17,13 @@
 package org.streampipes.test.generator;
 
 import org.streampipes.model.SpDataStream;
+import org.streampipes.model.schema.EventPropertyPrimitive;
 import org.streampipes.model.schema.EventSchema;
 import org.streampipes.test.generator.grounding.EventGroundingGenerator;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class EventStreamGenerator {
 
@@ -26,6 +31,18 @@ public class EventStreamGenerator {
     SpDataStream stream = new SpDataStream();
     stream.setEventSchema(new EventSchema());
     stream.setEventGrounding(EventGroundingGenerator.makeDummyGrounding());
+
+    return stream;
+  }
+
+  public static SpDataStream makeStreamWithProperties(List<String> runtimeNames) {
+    SpDataStream stream = makeEmptyStream();
+    stream
+            .getEventSchema()
+            .setEventProperties(runtimeNames
+                    .stream()
+                    .map(r -> new EventPropertyPrimitive("", r, "", new ArrayList<>()))
+                    .collect(Collectors.toList()));
 
     return stream;
   }
