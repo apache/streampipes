319395ef681c6679dd1dbb279e182aaf4a62c899
Dominik Riemer <riemer@fzi.de>
2018-08-27 22:40:50 +0200

    Add geospatial enrichment module for flink

---
+++ streampipes-processors-geo-flink/src/main/java/org/streampipes/processor/geo/flink/config/ConfigKeys.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright 2018 FZI Forschungszentrum Informatik
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.streampipes.processor.geo.flink.config;
+
+public class ConfigKeys {
+    final static String HOST = "SP_HOST";
+    final static String PORT = "SP_PORT";
+    final static String FLINK_HOST = "SP_FLINK_HOST";
+    final static String FLINK_PORT = "SP_FLINK_PORT";
+    final static String ICON_HOST = "SP_ICON_HOST";
+    final static String ICON_PORT = "SP_ICON_PORT";
+    final static String SERVICE_NAME = "SP_SERVICE_NAME";
+    final static String DEBUG = "SP_FLINK_DEBUG";
+}
