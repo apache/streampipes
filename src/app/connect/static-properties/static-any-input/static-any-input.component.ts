fc384545cec36d3cc06f8a88fef6044383a83267
Samuel Abt <samuel.abt@student.kit.edu>
2018-09-06 15:09:09 +0200

    change from multiselect dropdown to multiple checkboxes

--- src/app/connect/static-properties/static-any-input/static-any-input.component.ts
+++ src/app/connect/static-properties/static-any-input/static-any-input.component.ts
@@ -10,19 +10,6 @@ export class StaticAnyInput implements OnInit {
   @Input()
   staticProperty: AnyStaticProperty;
 
-  changeSelection(event) {
-    for (let option of this.staticProperty.options) {
-      option.selected = false;
-    }
-    for (let selectedOption of event.value) {
-      for (let option of this.staticProperty.options) {
-        if (selectedOption.id === option.id) {
-          option.selected = true;
-        }
-      }
-    }
-  }
-
   ngOnInit() {
     for (let option of this.staticProperty.options) {
       option.selected = false;
