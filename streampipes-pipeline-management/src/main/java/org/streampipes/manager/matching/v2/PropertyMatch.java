87db02c22001666bc912d1e942cd31a0496bed0f
Dominik Riemer <riemer@fzi.de>
2018-11-05 20:54:08 +0100

    Fix #58: Return matching error details for incompatible property types

--- streampipes-pipeline-management/src/main/java/org/streampipes/manager/matching/v2/PropertyMatch.java
+++ streampipes-pipeline-management/src/main/java/org/streampipes/manager/matching/v2/PropertyMatch.java
@@ -28,65 +28,69 @@ import java.util.List;
 
 public class PropertyMatch extends AbstractMatcher<EventProperty, EventProperty> {
 
-	public PropertyMatch() {
-		super(MatchingResultType.PROPERTY_MATCH);
-		// TODO Auto-generated constructor stub
-	}
+  public PropertyMatch() {
+    super(MatchingResultType.PROPERTY_MATCH);
+    // TODO Auto-generated constructor stub
+  }
 
-	@Override
-	public boolean match(EventProperty offer, EventProperty requirement, List<MatchingResultMessage> errorLog) {
-		// return true if the requirement is an any property
-	    if (isAnyProperty(requirement)) {
-			return true;
-		}
+  @Override
+  public boolean match(EventProperty offer, EventProperty requirement, List<MatchingResultMessage> errorLog) {
+    // return true if the requirement is an any property
+    if (isAnyProperty(requirement)) {
+      return true;
+    }
 
-		if (!matchesType(offer, requirement)) {
-			return false;
-		}
-		else {
-			if (isPrimitive(requirement)) return new PrimitivePropertyMatch().match(toPrimitive(offer), toPrimitive(requirement), errorLog);
-			else if (isList(requirement)) return new ListPropertyMatch().match(toList(offer), toList(requirement), errorLog);
-			else if (isNested(requirement)) return new NestedPropertyMatch().match(toNested(offer), toNested(requirement), errorLog);
-			else return false;
-		}
-	}
+    if (!matchesType(offer, requirement)) {
+      buildErrorMessage(errorLog, "The required type " + requirement.getClass().getSimpleName() +
+              " is not present in the connected stream.");
+      return false;
+    } else {
+      if (isPrimitive(requirement)) {
+        return new PrimitivePropertyMatch().match(toPrimitive(offer), toPrimitive(requirement), errorLog);
+      } else if (isList(requirement)) {
+        return new ListPropertyMatch().match(toList(offer), toList(requirement), errorLog);
+      } else if (isNested(requirement)) {
+        return new NestedPropertyMatch().match(toNested(offer), toNested(requirement), errorLog);
+      } else {
+        return false;
+      }
+    }
+  }
 
-	private boolean isAnyProperty(EventProperty eventProperty) {
-		return eventProperty instanceof EventPropertyPrimitive &&
-				eventProperty.getDomainProperties() == null &&
-				((EventPropertyPrimitive) eventProperty).getMeasurementUnit() == null &&
-				((EventPropertyPrimitive) eventProperty).getRuntimeType() == null;
-	}
-	
-	private EventPropertyNested toNested(EventProperty property) {
-		return (EventPropertyNested) property;
-	}
+  private boolean isAnyProperty(EventProperty eventProperty) {
+    return eventProperty instanceof EventPropertyPrimitive &&
+            eventProperty.getDomainProperties() == null &&
+            ((EventPropertyPrimitive) eventProperty).getMeasurementUnit() == null &&
+            ((EventPropertyPrimitive) eventProperty).getRuntimeType() == null;
+  }
 
-	private EventPropertyList toList(EventProperty property) {
-		return (EventPropertyList) property;
-	}
+  private EventPropertyNested toNested(EventProperty property) {
+    return (EventPropertyNested) property;
+  }
 
-	private EventPropertyPrimitive toPrimitive(EventProperty property) {
-		return (EventPropertyPrimitive) property;
-	}
+  private EventPropertyList toList(EventProperty property) {
+    return (EventPropertyList) property;
+  }
 
-	private boolean isList(EventProperty requirement) {
-		return requirement instanceof EventPropertyList;
-	}
-	
-	private boolean isNested(EventProperty requirement) {
-		return requirement instanceof EventPropertyNested;
-	}
-	
-	private boolean isPrimitive(EventProperty requirement) {
-		return requirement instanceof EventPropertyPrimitive;
-	}
+  private EventPropertyPrimitive toPrimitive(EventProperty property) {
+    return (EventPropertyPrimitive) property;
+  }
 
-	public boolean matchesType(EventProperty offer, EventProperty requirement) {
-		return offer.getClass().getCanonicalName().equals(requirement.getClass().getCanonicalName());
-	}
-	
-	
-	
+  private boolean isList(EventProperty requirement) {
+    return requirement instanceof EventPropertyList;
+  }
+
+  private boolean isNested(EventProperty requirement) {
+    return requirement instanceof EventPropertyNested;
+  }
+
+  private boolean isPrimitive(EventProperty requirement) {
+    return requirement instanceof EventPropertyPrimitive;
+  }
+
+  public boolean matchesType(EventProperty offer, EventProperty requirement) {
+    return offer.getClass().getCanonicalName().equals(requirement.getClass().getCanonicalName());
+  }
+
 
 }
