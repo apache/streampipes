92b9d474133d9ae81a0db4c4908d45a33d316efa
Philipp Zehnder <zehnder@fzi.de>
2019-01-30 14:09:25 +0100

    Working on new map

--- src/app/dashboard/templates/map/map.directive.ts
+++ src/app/dashboard/templates/map/map.directive.ts
@@ -1,6 +1,7 @@
 import { WidgetInstances } from '../../widget-instances.service'
 'use strict';
 mapWidget.$inject = ['WidgetInstances', 'NgMap'];
+declare let L;
 
 export default function mapWidget(WidgetInstances, NgMap) {
     return {
@@ -12,17 +13,28 @@ export default function mapWidget(WidgetInstances, NgMap) {
             widgetId: '@'
         },
         controller: function ($scope) {
-            WidgetInstances.get($scope.widgetId).then(function (data) {
-                $scope.widgetConfig = data.visualisation.schema.config;
+            var map = L.map('map', {
+                center: [51.505, -0.09],
+                zoom: 13
             });
+
+
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+
+            L.marker([51.505, -0.09]).addTo(map);
+            // WidgetInstances.get($scope.widgetId).then(function (data) {
+            //     $scope.widgetConfig = data.visualisation.schema.config;
+            // });
         },
         link: function postLink(scope) {
-            scope.$watch('data', function (data) {
-                if (data) {
-                    scope.currentLocation = [data[scope.widgetConfig.selectedLongitudeMapping.properties.runtimeName],
-                        data[scope.widgetConfig.selectedLatitudeMapping.properties.runtimeName]];
-                }
-            });
+            // scope.$watch('data', function (data) {
+            //     if (data) {
+            //         scope.currentLocation = [data[scope.widgetConfig.selectedLongitudeMapping.properties.runtimeName],
+            //             data[scope.widgetConfig.selectedLatitudeMapping.properties.runtimeName]];
+            //     }
+            // });
         }
     };
 };
