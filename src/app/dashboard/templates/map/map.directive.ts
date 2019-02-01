3a51c2962714747a87e508d22059630022a37692
Philipp Zehnder <zehnder@fzi.de>
2019-02-01 14:12:31 +0100

    Working on map directive

--- src/app/dashboard/templates/map/map.directive.ts
+++ src/app/dashboard/templates/map/map.directive.ts
@@ -13,7 +13,11 @@ export default function mapWidget(WidgetInstances, NgMap) {
             widgetId: '@'
         },
         controller: function ($scope) {
-            var map = L.map('map', {
+            WidgetInstances.get($scope.widgetId).then(function (data) {
+                $scope.widgetConfig = data.visualisation.schema.config;
+            });
+
+            $scope.map = L.map('map', {
                 center: [51.505, -0.09],
                 zoom: 13
             });
@@ -21,20 +25,28 @@ export default function mapWidget(WidgetInstances, NgMap) {
 
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            }).addTo(map);
+            }).addTo($scope.map);
 
-            L.marker([51.505, -0.09]).addTo(map);
             // WidgetInstances.get($scope.widgetId).then(function (data) {
             //     $scope.widgetConfig = data.visualisation.schema.config;
             // });
         },
         link: function postLink(scope) {
-            // scope.$watch('data', function (data) {
-            //     if (data) {
-            //         scope.currentLocation = [data[scope.widgetConfig.selectedLongitudeMapping.properties.runtimeName],
-            //             data[scope.widgetConfig.selectedLatitudeMapping.properties.runtimeName]];
-            //     }
-            // });
+            scope.$watch('data', function (data) {
+                if (data) {
+                    console.log(data);
+                    var lat = data[scope.widgetConfig.selectedLatitudeMapping.properties.runtimeName];
+                    console.log(lat);
+                    var long = data[scope.widgetConfig.selectedLongitudeMapping.properties.runtimeName];
+                    console.log(long);
+
+                    L.marker([lat, long]).addTo(scope.map);
+                    // L.setView([lat, long], 13).addTo(scope.map);
+
+                    scope.map.panTo(new L.LatLng(lat, long));
+
+                }
+            });
         }
     };
 };
