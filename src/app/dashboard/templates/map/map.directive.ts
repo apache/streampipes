44711f42207b84590540e2ff23ddcc1d78069d80
Philipp Zehnder <zehnder@fzi.de>
2019-02-03 20:20:23 +0100

    Working on new map

--- src/app/dashboard/templates/map/map.directive.ts
+++ src/app/dashboard/templates/map/map.directive.ts
@@ -13,40 +13,59 @@ export default function mapWidget(WidgetInstances, NgMap) {
             widgetId: '@'
         },
         controller: function ($scope) {
+            $scope.refocus = true;
+            $scope.markers = [];
+
+            // $scope.mapId = "map_" + Math.floor(Math.random() * 9999);
             WidgetInstances.get($scope.widgetId).then(function (data) {
                 $scope.widgetConfig = data.visualisation.schema.config;
             });
 
-            $scope.map = L.map('map', {
+            $scope.map = L.map("map", {
                 center: [51.505, -0.09],
                 zoom: 13
             });
-
 
             L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
                 attribution: '© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
             }).addTo($scope.map);
 
+            this.toggleRefocus = function () {
+               console.log("Toggle")
+                $scope.refocus = !$scope.refocus;
+            }
             // WidgetInstances.get($scope.widgetId).then(function (data) {
             //     $scope.widgetConfig = data.visualisation.schema.config;
             // });
         },
         link: function postLink(scope) {
+
             scope.$watch('data', function (data) {
                 if (data) {
-                    console.log(data);
-                    var lat = data[scope.widgetConfig.selectedLatitudeMapping.properties.runtimeName];
-                    console.log(lat);
-                    var long = data[scope.widgetConfig.selectedLongitudeMapping.properties.runtimeName];
-                    console.log(long);
 
-                    L.marker([lat, long]).addTo(scope.map);
-                    // L.setView([lat, long], 13).addTo(scope.map);
+                    var lat = data[scope.widgetConfig.selectedLatitudeMapping.properties.runtimeName];
+                    var long = data[scope.widgetConfig.selectedLongitudeMapping.properties.runtimeName];
+                    var text = "<h4>Id: " + data[scope.widgetConfig.selectedLabelMapping.properties.runtimeName] + "</h4></br>";
+                    for (var key in data) {;
+                        text =  text.concat("<b>" +key +"</b>" +  ": " + data[key] + "<br>");
+                    }
+
+                    var marker = L.marker([lat, long])
+                        .addTo(scope.map)
+                        .bindPopup(text);
+
+                    scope.markers.push(marker);
+
+                    if (scope.refocus) {
+                        var group = new L.featureGroup(scope.markers);
+                        scope.map.fitBounds(group.getBounds());
+                    }
 
                     scope.map.panTo(new L.LatLng(lat, long));
 
                 }
             });
-        }
+        },
+        controllerAs: 'ctrl'
     };
 };
