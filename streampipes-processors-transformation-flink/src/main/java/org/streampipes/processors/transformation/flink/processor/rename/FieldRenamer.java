b0d5579d724b4b92b52b2b4007e9a40761c0f051
tex <tex@fzi.de>
2019-03-01 11:56:23 +0100

    update rename runtinename

--- streampipes-processors-transformation-flink/src/main/java/org/streampipes/processors/transformation/flink/processor/rename/FieldRenamer.java
+++ streampipes-processors-transformation-flink/src/main/java/org/streampipes/processors/transformation/flink/processor/rename/FieldRenamer.java
@@ -37,7 +37,9 @@ public class FieldRenamer implements FlatMapFunction<Event, Event> {
   @Override
   public void flatMap(Event in,
                       Collector<Event> out) throws Exception {
-    in.getFieldBySelector(oldPropertyName).rename(newPropertyName);
+    AbstractField<?> propertyValue = in.getFieldBySelector(oldPropertyName);
+    in.removeFieldBySelector(oldPropertyName);
+    in.addField(newPropertyName, propertyValue);
     out.collect(in);
   }
 
