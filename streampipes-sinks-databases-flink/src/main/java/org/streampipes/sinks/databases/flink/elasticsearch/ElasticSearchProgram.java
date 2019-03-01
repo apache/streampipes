bdee5448ba30c7e4ef0a9000d35edfb7aecb8dcf
tex <tex@fzi.de>
2019-03-01 08:34:53 +0100

    Set elasticsearch buffer to 10

--- streampipes-sinks-databases-flink/src/main/java/org/streampipes/sinks/databases/flink/elasticsearch/ElasticSearchProgram.java
+++ streampipes-sinks-databases-flink/src/main/java/org/streampipes/sinks/databases/flink/elasticsearch/ElasticSearchProgram.java
@@ -61,8 +61,8 @@ public class ElasticSearchProgram extends FlinkDataSinkRuntime<ElasticSearchPara
                 "http"));
 
         Map<String, String> userConfig = new HashMap<>();
-        // This instructs the sink to emit after every element, otherwise they would be buffered
-        userConfig.put(ElasticsearchSink.CONFIG_KEY_BULK_FLUSH_MAX_ACTIONS, "1");
+        // This instructs the sink to emit after 10 elements
+        userConfig.put(ElasticsearchSink.CONFIG_KEY_BULK_FLUSH_MAX_ACTIONS, "10");
 
         convertedStream[0].flatMap(new FlatMapFunction<Event, Map<String, Object>>() {
 
