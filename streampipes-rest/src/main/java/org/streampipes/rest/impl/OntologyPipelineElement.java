d9246d34be3d59870840f1c08dc4d1cfd813c68c
Dominik Riemer <riemer@fzi.de>
2019-03-08 00:38:17 +0100

    Fix some minor code style issues

--- streampipes-rest/src/main/java/org/streampipes/rest/impl/OntologyPipelineElement.java
+++ streampipes-rest/src/main/java/org/streampipes/rest/impl/OntologyPipelineElement.java
@@ -22,11 +22,11 @@ import com.google.gson.JsonArray;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonPrimitive;
 import org.streampipes.model.client.messages.Notifications;
+import org.streampipes.model.graph.DataProcessorDescription;
 import org.streampipes.model.graph.DataSinkDescription;
 import org.streampipes.model.graph.DataSourceDescription;
-import org.streampipes.model.graph.DataProcessorDescription;
-import org.streampipes.rest.shared.annotation.GsonWithIds;
 import org.streampipes.rest.api.IOntologyPipelineElement;
+import org.streampipes.rest.shared.annotation.GsonWithIds;
 import org.streampipes.serializers.json.GsonSerializer;
 import org.streampipes.storage.management.StorageManager;
 
@@ -45,110 +45,114 @@ import javax.ws.rs.core.Response;
 @Path("/v2/ontology")
 public class OntologyPipelineElement extends AbstractRestInterface implements IOntologyPipelineElement {
 
-	@Override
-	@Path("/sources")
-	@GET
-	@GsonWithIds
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getStreams() {
-		List<DataSourceDescription> result = new ArrayList<>();
-		List<DataSourceDescription> sesameSeps = StorageManager.INSTANCE.getStorageAPI().getAllSEPs();
-		
-		for(DataSourceDescription sep : sesameSeps)
-			result.add(new DataSourceDescription(sep));
-		return ok(result);
-	}
-	
-	@Path("/sources/{sourceId}")
-	@GET
-	@GsonWithIds
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getSourceDetails(@PathParam("sourceId") String sepaId, @QueryParam("keepIds") boolean keepIds) {
-		
-		try {
-			DataSourceDescription sepaDescription = new DataSourceDescription(StorageManager.INSTANCE.getStorageAPI().getSEPById(sepaId));
-			return ok(sepaDescription);
-		} catch (URISyntaxException e) {
-			return ok(Notifications.error("Error"));
-		}
-	}
+  @Override
+  @Path("/sources")
+  @GET
+  @GsonWithIds
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getStreams() {
+    List<DataSourceDescription> result = new ArrayList<>();
+    List<DataSourceDescription> sesameSeps = StorageManager.INSTANCE.getStorageAPI().getAllSEPs();
 
-	@Override
-	@Path("/sepas")
-	@GsonWithIds
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getSepas() {
-		JsonArray result = new JsonArray();
-		List<DataProcessorDescription> sesameSepas = StorageManager.INSTANCE.getStorageAPI().getAllSEPAs();
-		
-		for(DataProcessorDescription sepa : sesameSepas)
-			result.add(getHeader(sepa.getUri(), sepa.getName()));
-		return ok(result);
-		
-	}
+    for (DataSourceDescription sep : sesameSeps) {
+      result.add(new DataSourceDescription(sep));
+    }
+    return ok(result);
+  }
 
-	@Override
-	@Path("/actions")
-	@GET
-	@GsonWithIds
-	@Produces(MediaType.APPLICATION_JSON)
-	public Response getActions() {
-		List<DataSinkDescription> result = new ArrayList<>();
-		List<DataSinkDescription> sesameSecs = StorageManager.INSTANCE.getStorageAPI().getAllSECs();
-		
-		for(DataSinkDescription sec : sesameSecs)
-			result.add(new DataSinkDescription(sec));
-		return ok(result);
-	}
+  @Path("/sources/{sourceId}")
+  @GET
+  @GsonWithIds
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getSourceDetails(@PathParam("sourceId") String sepaId, @QueryParam("keepIds") boolean keepIds) {
 
-	@Override
-	public Response getStream(String streamId, @QueryParam("keepIds") boolean keepIds) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    try {
+      DataSourceDescription sepaDescription = new DataSourceDescription(StorageManager.INSTANCE.getStorageAPI().getSEPById(sepaId));
+      return ok(sepaDescription);
+    } catch (URISyntaxException e) {
+      return ok(Notifications.error("Error"));
+    }
+  }
 
-	@Path("/sepas/{sepaId}")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Override
-	public Response getSepa(@PathParam("sepaId") String sepaId, @QueryParam("keepIds") boolean keepIds) {
-		
-		try {
-			DataProcessorDescription dataProcessorDescription = new DataProcessorDescription(StorageManager.INSTANCE.getStorageAPI().getSEPAById(sepaId));
-			return ok(dataProcessorDescription);
-		} catch (URISyntaxException e) {
-			return ok(Notifications.error("Error"));
-		}
-	}
+  @Override
+  @Path("/sepas")
+  @GsonWithIds
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getSepas() {
+    JsonArray result = new JsonArray();
+    List<DataProcessorDescription> sesameSepas = StorageManager.INSTANCE.getStorageAPI().getAllSEPAs();
 
-	@Path("/actions/{actionId}")
-	@GET
-	@Produces(MediaType.APPLICATION_JSON)
-	@Override
-	public Response getAction(@PathParam("actionId") String actionId, @QueryParam("keepIds") boolean keepIds) {
-		try {
-			DataSinkDescription dataSinkDescription = new DataSinkDescription(StorageManager.INSTANCE.getStorageAPI().getSECById(actionId));
-			return ok(dataSinkDescription);
-		} catch (URISyntaxException e) {
-			return ok(Notifications.error("Error"));
-		}
-	}
+    for (DataProcessorDescription sepa : sesameSepas) {
+      result.add(getHeader(sepa.getUri(), sepa.getName()));
+    }
+    return ok(result);
 
-	private JsonObject getHeader(String uri, String name)
-	{
-		JsonObject jsonObj = new JsonObject();
-		jsonObj.add("uri", new JsonPrimitive(uri));
-		jsonObj.add("name", new JsonPrimitive(name));
-		
-		return jsonObj;
-	}
-	
-	private Gson getGson(boolean keepIds)
-	{
-		if (keepIds) return GsonSerializer.getGsonWithIds();
-		else return GsonSerializer.getGsonWithoutIds();
-	}
-	
+  }
+
+  @Override
+  @Path("/actions")
+  @GET
+  @GsonWithIds
+  @Produces(MediaType.APPLICATION_JSON)
+  public Response getActions() {
+    List<DataSinkDescription> result = new ArrayList<>();
+    List<DataSinkDescription> sesameSecs = StorageManager.INSTANCE.getStorageAPI().getAllSECs();
+
+    for (DataSinkDescription sec : sesameSecs) {
+      result.add(new DataSinkDescription(sec));
+    }
+    return ok(result);
+  }
+
+  @Override
+  public Response getStream(String streamId, @QueryParam("keepIds") boolean keepIds) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Path("/sepas/{sepaId}")
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Override
+  public Response getSepa(@PathParam("sepaId") String sepaId, @QueryParam("keepIds") boolean keepIds) {
+
+    try {
+      DataProcessorDescription dataProcessorDescription = new DataProcessorDescription(StorageManager.INSTANCE.getStorageAPI().getSEPAById(sepaId));
+      return ok(dataProcessorDescription);
+    } catch (URISyntaxException e) {
+      return ok(Notifications.error("Error"));
+    }
+  }
+
+  @Path("/actions/{actionId}")
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Override
+  public Response getAction(@PathParam("actionId") String actionId, @QueryParam("keepIds") boolean keepIds) {
+    try {
+      DataSinkDescription dataSinkDescription = new DataSinkDescription(StorageManager.INSTANCE.getStorageAPI().getSECById(actionId));
+      return ok(dataSinkDescription);
+    } catch (URISyntaxException e) {
+      return ok(Notifications.error("Error"));
+    }
+  }
+
+  private JsonObject getHeader(String uri, String name) {
+    JsonObject jsonObj = new JsonObject();
+    jsonObj.add("uri", new JsonPrimitive(uri));
+    jsonObj.add("name", new JsonPrimitive(name));
+
+    return jsonObj;
+  }
+
+  private Gson getGson(boolean keepIds) {
+    if (keepIds) {
+      return GsonSerializer.getGsonWithIds();
+    } else {
+      return GsonSerializer.getGsonWithoutIds();
+    }
+  }
+
 
 }
