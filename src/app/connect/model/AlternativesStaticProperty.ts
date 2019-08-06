f649a79b2720f820833a1cf32e651ea08e09eed1
tex <tex@fzi.de>
2019-08-06 11:20:44 +0200

    ce#231 fix alternatives type

--- src/app/connect/model/AlternativesStaticProperty.ts
+++ src/app/connect/model/AlternativesStaticProperty.ts
@@ -1,12 +1,13 @@
 import {RdfsClass} from '../../platform-services/tsonld/RdfsClass';
 import {StaticProperty} from './StaticProperty';
 import {RdfProperty} from '../../platform-services/tsonld/RdfsProperty';
+import {AlternativeStaticProperty} from './AlternativeStaticProperty';
 
 @RdfsClass('sp:StaticPropertyAlternatives')
 export class AlternativesStaticProperty extends StaticProperty {
 
     @RdfProperty('sp:hasStaticPropertyAlternative')
-    public alternatives: SpeechRecognitionAlternative[];
+    public alternatives: AlternativeStaticProperty[];
 
 
     constructor(id: string) {
