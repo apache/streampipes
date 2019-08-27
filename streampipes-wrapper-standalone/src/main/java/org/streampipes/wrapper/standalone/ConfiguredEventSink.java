95653e833be7d19078f1719bfab7cc1274e41c47
Dominik Riemer <riemer@fzi.de>
2019-08-27 19:51:18 +0200

    Close streampipes/ce#269: Extend standalone wrapper with external processor and sink

--- streampipes-wrapper-standalone/src/main/java/org/streampipes/wrapper/standalone/ConfiguredEventSink.java
+++ streampipes-wrapper-standalone/src/main/java/org/streampipes/wrapper/standalone/ConfiguredEventSink.java
@@ -17,29 +17,16 @@
 
 package org.streampipes.wrapper.standalone;
 
+import org.streampipes.model.graph.DataSinkInvocation;
 import org.streampipes.wrapper.params.binding.EventSinkBindingParams;
 import org.streampipes.wrapper.runtime.EventSink;
 
 import java.util.function.Supplier;
 
-public class ConfiguredEventSink<B extends EventSinkBindingParams> {
-
-  private B bindingParams;
-  private Supplier<EventSink<B>> engineSupplier;
+public class ConfiguredEventSink<B extends EventSinkBindingParams> extends AbstractConfiguredPipelineElement<DataSinkInvocation, B, EventSink<B>> {
 
   public ConfiguredEventSink(B bindingParams, Supplier<EventSink<B>> engineSupplier) {
-    this.bindingParams = bindingParams;
-    this.engineSupplier = engineSupplier;
+    super(bindingParams, engineSupplier);
   }
-
-  public B getBindingParams() {
-    return bindingParams;
-  }
-
-  public Supplier<EventSink<B>> getEngineSupplier() {
-    return engineSupplier;
-  }
-
-
 }
 
