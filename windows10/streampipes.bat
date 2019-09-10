5a210842dd19f96233301c2b58e5e28b95a6cfe2
Dominik Riemer <riemer@fzi.de>
2019-09-10 17:09:14 +0200

    Add parameter to manually assign IP address to Windows installer

--- windows10/streampipes.bat
+++ windows10/streampipes.bat
@@ -9,13 +9,19 @@ SET ThisScriptsDirectory=%~dp0
 SET PowerShellScriptPath=%ThisScriptsDirectory%streampipes.ps1
 
 if "%1"=="start" (
-	PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%' 'start'";
+
+	if "%2"=="" (
+        PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%' 'start' ''";
+    )
+	if not "%2"=="" (
+		PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%' 'start' '%2'";
+	)
 )
 
 if "%1"=="stop" (
-    PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%' 'stop'";
+    PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%' 'stop' ''";
 )
 
 if "%1"=="clean" (
-    PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%' 'clean'";
+    PowerShell -NoProfile -ExecutionPolicy Bypass -Command "& '%PowerShellScriptPath%' 'clean' ''";
 )
