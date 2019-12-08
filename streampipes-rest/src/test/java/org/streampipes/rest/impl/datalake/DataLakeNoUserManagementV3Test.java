f2ddb368b2482584eb042db9cc89fc6140e16a8d
Dominik Riemer <riemer@fzi.de>
2019-12-08 19:07:17 +0100

    STREAMPIPES-24: Update license headers

--- streampipes-rest/src/test/java/org/streampipes/rest/impl/datalake/DataLakeNoUserManagementV3Test.java
+++ streampipes-rest/src/test/java/org/streampipes/rest/impl/datalake/DataLakeNoUserManagementV3Test.java
@@ -1,11 +1,12 @@
 /*
- * Copyright 2018 FZI Forschungszentrum Informatik
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * you may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -33,21 +34,6 @@ import java.util.List;
 
 import static org.junit.Assert.*;
 
-/*
-Copyright 2019 FZI Forschungszentrum Informatik
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
 public class DataLakeNoUserManagementV3Test {
 
     Method privateMethod;
