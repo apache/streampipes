d5da585838c576609aee6d216818b59fb13b7920
Philipp Zehnder <zehnder@fzi.de>
2020-02-17 22:58:26 +0100

    Prepare to store images in directory instead of influxdb

--- streampipes-sinks-internal-jvm/src/main/java/org/apache/streampipes/sinks/internal/jvm/datalake/DataLake.java
+++ streampipes-sinks-internal-jvm/src/main/java/org/apache/streampipes/sinks/internal/jvm/datalake/DataLake.java
@@ -18,6 +18,7 @@
 
 package org.apache.streampipes.sinks.internal.jvm.datalake;
 
+import org.apache.commons.codec.binary.Base64;
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpPost;
@@ -27,13 +28,20 @@ import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.streampipes.commons.exceptions.SpRuntimeException;
 import org.apache.streampipes.logging.api.Logger;
 import org.apache.streampipes.model.runtime.Event;
+import org.apache.streampipes.model.schema.EventProperty;
 import org.apache.streampipes.model.schema.EventSchema;
 import org.apache.streampipes.serializers.json.Utils;
 import org.apache.streampipes.sinks.internal.jvm.config.SinksInternalJvmConfig;
+import org.apache.streampipes.vocabulary.SPSensor;
 import org.apache.streampipes.wrapper.context.EventSinkRuntimeContext;
 import org.apache.streampipes.wrapper.runtime.EventSink;
 
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Code is the same as InfluxDB (org.apache.streampipes.sinks.databases.jvm.influxdb) sink. Changes applied here should also be applied in the InfluxDB sink
@@ -45,31 +53,52 @@ public class DataLake implements EventSink<DataLakeParameters> {
 
   private static Logger LOG;
 
+  private List<EventProperty> imageProperties;
+
   @Override
   public void onInvocation(DataLakeParameters parameters, EventSinkRuntimeContext runtimeContext) throws SpRuntimeException {
     LOG = parameters.getGraph().getLogger(DataLake.class);
 
     this.influxDbClient = new InfluxDbClient(
-        parameters.getInfluxDbHost(),
-        parameters.getInfluxDbPort(),
-        parameters.getDatabaseName(),
-        parameters.getMeasurementName(),
-        parameters.getUsername(),
-        parameters.getPassword(),
-        parameters.getTimestampField(),
-        parameters.getBatchSize(),
-        parameters.getFlushDuration(),
-        parameters.getDimensionProperties(),
-        LOG
+            parameters.getInfluxDbHost(),
+            parameters.getInfluxDbPort(),
+            parameters.getDatabaseName(),
+            parameters.getMeasurementName(),
+            parameters.getUsername(),
+            parameters.getPassword(),
+            parameters.getTimestampField(),
+            parameters.getBatchSize(),
+            parameters.getFlushDuration(),
+            parameters.getDimensionProperties(),
+            LOG
     );
-    registerAtDataLake(parameters.getMeasurementName(), runtimeContext.getInputSchemaInfo().get(0).getEventSchema());
 
+    EventSchema schema = runtimeContext.getInputSchemaInfo().get(0).getEventSchema();
+    registerAtDataLake(parameters.getMeasurementName(), schema);
 
+    imageProperties = schema.getEventProperties().stream()
+            .filter(eventProperty -> eventProperty.getDomainProperties().size() > 0 &&
+                    eventProperty.getDomainProperties().get(0).toString().equals(SPSensor.IMAGE))
+            .collect(Collectors.toList());
+
+    // TODO
+    // get directory location for files
   }
 
   @Override
   public void onEvent(Event event) {
     try {
+
+      this.imageProperties.stream().forEach(eventProperty -> {
+        // TODO
+        // how should I name the file? (get timestamp field)
+        // 1. get image from event
+        // 2. write image to file (writeToImageFile)
+        // 3. replace property with image location
+
+
+      });
+
       influxDbClient.save(event);
     } catch (SpRuntimeException e) {
       LOG.error(e.getMessage());
@@ -79,6 +108,18 @@ public class DataLake implements EventSink<DataLakeParameters> {
   @Override
   public void onDetach() throws SpRuntimeException {
     influxDbClient.stop();
+  }
+
+  private void writeToImageFile(String image, String fileRoute) {
+    byte[] data = Base64.decodeBase64(image);
+    try (OutputStream stream = new FileOutputStream(fileRoute)) {
+      stream.write(data);
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+
   }
 
   private void registerAtDataLake(String measure, EventSchema eventSchema) throws SpRuntimeException {
