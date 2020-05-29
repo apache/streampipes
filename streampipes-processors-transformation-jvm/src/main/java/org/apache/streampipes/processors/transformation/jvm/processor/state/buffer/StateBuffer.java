bc1752e8ef0c0e4d9790cddbe011a544482a73b1
Philipp Zehnder <zehnder@fzi.de>
2020-05-29 09:10:04 +0200

    Working on state buffer

---
+++ streampipes-processors-transformation-jvm/src/main/java/org/apache/streampipes/processors/transformation/jvm/processor/state/buffer/StateBuffer.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package org.apache.streampipes.processors.transformation.jvm.processor.state.buffer;
+
+import org.apache.streampipes.logging.api.Logger;
+import org.apache.streampipes.model.runtime.Event;
+import org.apache.streampipes.wrapper.context.EventProcessorRuntimeContext;
+import org.apache.streampipes.wrapper.routing.SpOutputCollector;
+import org.apache.streampipes.wrapper.runtime.EventProcessor;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class StateBuffer implements EventProcessor<StateBufferParameters> {
+
+  private static Logger LOG;
+  private String timeProperty;
+  private String stateProperty;
+  private String sensorValueProperty;
+
+
+  @Override
+  public void onInvocation(StateBufferParameters stateBufferParameters,
+                           SpOutputCollector spOutputCollector,
+                           EventProcessorRuntimeContext runtimeContext) {
+    LOG = stateBufferParameters.getGraph().getLogger(StateBuffer.class);
+
+    this.timeProperty = stateBufferParameters.getTimeProperty();
+    this.stateProperty = stateBufferParameters.getStateProperty();
+    this.sensorValueProperty = stateBufferParameters.getSensorValueProperty();
+  }
+
+  @Override
+  public void onEvent(Event inputEvent, SpOutputCollector out) {
+      // TODO
+
+    out.collect(inputEvent);
+  }
+
+  @Override
+  public void onDetach() {
+  }
+}
