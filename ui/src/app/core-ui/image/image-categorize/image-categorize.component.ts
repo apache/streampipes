5223718b21cc9ce6a1623d8745cb6d56410f4a2e
tex <tex@fzi.de>
2020-06-08 13:07:46 +0200

    [STREAMPIPES-79] fix order of images (newest to oldest), fix page display

--- ui/src/app/core-ui/image/image-categorize/image-categorize.component.ts
+++ ui/src/app/core-ui/image/image-categorize/image-categorize.component.ts
@@ -95,8 +95,9 @@ export class ImageCategorizeComponent implements OnInit, AfterViewInit {
 
   processData(pageResult) {
     if (pageResult.rows === undefined) {
-      this.pageIndex = pageResult.pageSum;
+      this.pageIndex = pageResult.pageSum - 1;
     } else {
+      pageResult.rows = pageResult.rows.reverse();
       this.pageIndex = pageResult.page;
       this.pageSum = pageResult.pageSum;
 
