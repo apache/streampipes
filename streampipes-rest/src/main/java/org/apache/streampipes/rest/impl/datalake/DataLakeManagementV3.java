dc96e56f2505b57538963e42f33b0aa621f6c30a
Daniel Ebi <ebi@fzi.de>
2020-07-02 14:56:47 +0200

    Add selection of non-numeric properties (string columns) to linechart widget

--- streampipes-rest/src/main/java/org/apache/streampipes/rest/impl/datalake/DataLakeManagementV3.java
+++ streampipes-rest/src/main/java/org/apache/streampipes/rest/impl/datalake/DataLakeManagementV3.java
@@ -577,7 +577,7 @@ public class DataLakeManagementV3 {
     return route;
   }
 
-  public void updateLabels(String index, long startdate, long enddate, String label) {
+  public void updateLabels(String index, String labelColumn, long startdate, long enddate, String label) {
     DataResult queryResult = getEvents(index, startdate, enddate);
     Map<String, String> headerWithTypes = getHeadersWithTypes(index);
     List<String> headers = queryResult.getHeaders();
@@ -592,7 +592,7 @@ public class DataLakeManagementV3 {
 
       for (int i = 1; i < row.size(); i++) {
         String selected_header = headers.get(i);
-        if (!selected_header.equals("sp_internal_label")) {
+        if (!selected_header.equals(labelColumn)) {
           if (headerWithTypes.get(selected_header).equals("integer")) {
             p.addField(selected_header, Math.round((double) row.get(i)));
           } else if (headerWithTypes.get(selected_header).equals("string")) {
