0584d1335a1fb5d9cf7620d6fba3fe24f0e2e133
Patrick Wiener <wiener@fzi.de>
2020-08-17 23:26:09 +0200

    [STREAMPIPES-196] add helper script to upgrade to new versions

---
+++ upgrade_versions.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+VERSION=$1
+
+if [ "$#" -gt 1 ]; then
+    fatal "Illegal number of arguments"
+fi
+
+set_version(){
+  options=( "cli" "compose" "k8s" )
+
+  case "$(uname -s)" in
+      Darwin)
+        # for MacOS
+        echo "$VERSION" > ./cli/VERSION
+        sed -i '' 's/SP_VERSION=.*/SP_VERSION='$VERSION'/g' ./compose/.env
+        sed -i '' 's/  version: .*/  version: "'$VERSION'"/g' ./k8s/values.yaml
+
+        for opt in "${options[@]}"
+        do
+          sed -i '' 's/\*\*Current version:\*\* .*/\*\*Current version:\*\* '$VERSION'/g' ./$opt/README.md
+        done
+        ;;
+      *)
+        # for Linux and Windows
+        echo "$VERSION" > ./cli/VERSION
+        sed -i 's/SP_VERSION=.*/SP_VERSION='$VERSION'/g' ./compose/.env
+        sed -i 's/  version: .*/  version: "'$VERSION'"/g' ./k8s/values.yaml
+
+        for opt in "${options[@]}"
+        do
+          sed -i 's/**Current version:** .*/**Current version:** '$VERSION'/g' ./$opt/README.md
+        done
+        ;;
+  esac
+}
+
+set_version
