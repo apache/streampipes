00d3c7d14560b6207ea36fd21312ff885a894a9f
Philipp Zehnder <zehnder@fzi.de>
2020-11-19 23:30:41 +0100

    Started to move connect master REST API into backend

--- streampipes-connect-adapters/src/main/java/org/apache/streampipes/connect/ConnectAdapterInit.java
+++ streampipes-connect-adapters/src/main/java/org/apache/streampipes/connect/ConnectAdapterInit.java
@@ -99,10 +99,11 @@ public class ConnectAdapterInit extends AdapterWorkerContainer {
             .add(new FlicMQTTAdapter());
 
     String workerUrl = ConnectWorkerConfig.INSTANCE.getConnectContainerWorkerUrl();
-    String masterUrl = ConnectWorkerConfig.INSTANCE.getConnectContainerMasterUrl();
+//    String masterUrl = ConnectWorkerConfig.INSTANCE.getConnectContainerMasterUrl();
     Integer workerPort = ConnectWorkerConfig.INSTANCE.getConnectContainerWorkerPort();
 
-    new ConnectAdapterInit().init(workerUrl, masterUrl, workerPort);
+    // TODO
+    new ConnectAdapterInit().init(workerUrl, "http://localhost:8030/streampipes-backend/api/connect", workerPort);
 
   }
 }
