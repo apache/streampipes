c31ab8758c3f2bc71ffa289ad5df6f57be56ae1a
bossenti <bossenti@posteo.de>
2021-03-29 19:32:09 +0200

    [STREANPIPES-334] add UI option for label name

--- streampipes-processors-transformation-jvm/src/main/java/org/apache/streampipes/processors/transformation/jvm/processor/state/labeler/number/NumberLabelerParameters.java
+++ streampipes-processors-transformation-jvm/src/main/java/org/apache/streampipes/processors/transformation/jvm/processor/state/labeler/number/NumberLabelerParameters.java
@@ -26,14 +26,16 @@ import java.util.List;
 public class NumberLabelerParameters extends EventProcessorBindingParams {
 
     private String sensorListValueProperty;
+    private String labelName;
 
     private List<Integer> numberValues;
     private List<String> labelStrings;
     private List<String> comparators;
 
-    public NumberLabelerParameters(DataProcessorInvocation graph, String sensorListValueProperty, List<Integer> numberValues, List<String> labelStrings, List<String> comparators) {
+    public NumberLabelerParameters(DataProcessorInvocation graph, String sensorListValueProperty, String labelName, List<Integer> numberValues, List<String> labelStrings, List<String> comparators) {
         super(graph);
         this.sensorListValueProperty = sensorListValueProperty;
+        this.labelName = labelName;
         this.numberValues = numberValues;
         this.labelStrings = labelStrings;
         this.comparators = comparators;
@@ -69,5 +71,9 @@ public class NumberLabelerParameters extends EventProcessorBindingParams {
 
     public void setComparators(List<String> comparators) {
         this.comparators = comparators;
+    }
+
+    public String getLabelName() {
+        return labelName;
     }
 }
