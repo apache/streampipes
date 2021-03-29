3e8fc4e7d5d240a7cbcc02995fc342697003cb55
Sandeep Kumar <Sandeep.Kumar0@walmartlabs.com>
2021-03-29 13:53:25 +0530

    STREAMPIPES-322:Addressed review comments

--- streampipes-processors-transformation-jvm/src/main/java/org/apache/streampipes/processors/transformation/jvm/processor/booloperator/logical/enums/BooleanOperatorType.java
+++ streampipes-processors-transformation-jvm/src/main/java/org/apache/streampipes/processors/transformation/jvm/processor/booloperator/logical/enums/BooleanOperatorType.java
@@ -7,7 +7,7 @@ public enum BooleanOperatorType {
     AND("AND", "Performs AND between boolean operands"),
     NOT("NOT", "Performs NOT on boolean operand"),
     X_NOR("X-NOR", "Performs X-NOR between boolean operands"),
-    NOR("NOR","Performs NOR between boolean operands" );
+    NOR("NOR", "Performs NOR between boolean operands");
 
     private final String operator;
     private final String description;
@@ -17,13 +17,30 @@ public enum BooleanOperatorType {
         this.description = description;
     }
 
-    public String operator()
-    {
+    public String operator() {
         return operator;
     }
 
     public String description() {
         return description;
+    }
+
+    public static BooleanOperatorType getBooleanOperatorType(String operator) {
+        if (operator.equals(AND.operator())) {
+            return AND;
+        } else if (operator.equals(OR.operator())) {
+            return OR;
+        } else if (operator.equals(XOR.operator())) {
+            return XOR;
+        } else if (operator.equals(NOT.operator())) {
+            return NOT;
+        } else if (operator.equals(X_NOR.operator())) {
+            return X_NOR;
+        } else if (operator.equals(NOR.operator())) {
+            return NOR;
+        } else {
+            throw new UnsupportedOperationException("No match found for " + operator);
+        }
     }
 
 }
