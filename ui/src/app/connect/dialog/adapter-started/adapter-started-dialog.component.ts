b4457ebf55631c34aae9e50462e44b727c0c8ea2
Philipp Zehnder <zehnder@fzi.de>
2021-09-16 16:59:29 +0200

    [hotfix] Fix code style in adatper start dialog

--- ui/src/app/connect/dialog/adapter-started/adapter-started-dialog.component.ts
+++ ui/src/app/connect/dialog/adapter-started/adapter-started-dialog.component.ts
@@ -16,115 +16,114 @@
  *
  */
 
-import {Component, Inject, Input} from '@angular/core';
-import {ShepherdService} from '../../../services/tour/shepherd.service';
-import {RestService} from "../../services/rest.service";
+import { Component, Input, OnInit } from '@angular/core';
+import { ShepherdService } from '../../../services/tour/shepherd.service';
+import { RestService } from '../../services/rest.service';
 import {
     AdapterDescriptionUnion,
     GenericAdapterSetDescription,
     Message,
     SpDataStream,
     SpecificAdapterSetDescription
-} from "../../../core-model/gen/streampipes-model";
-import {DialogRef} from "../../../core-ui/dialog/base-dialog/dialog-ref";
+} from '../../../core-model/gen/streampipes-model';
+import { DialogRef } from '../../../core-ui/dialog/base-dialog/dialog-ref';
 
 @Component({
-    selector: 'sp-dialog-adapter-started-dialog',
-    templateUrl: './adapter-started-dialog.component.html',
-    styleUrls: ['./adapter-started-dialog.component.scss'],
+  selector: 'sp-dialog-adapter-started-dialog',
+  templateUrl: './adapter-started-dialog.component.html',
+  styleUrls: ['./adapter-started-dialog.component.scss']
 })
-export class AdapterStartedDialog {
+export class AdapterStartedDialog implements OnInit {
 
-    adapterInstalled: boolean = false;
-    public adapterStatus: Message;
-    public streamDescription: SpDataStream;
-    private pollingActive: boolean = false;
-    public runtimeData: any;
-    public isSetAdapter: boolean = false;
-    public isTemplate: boolean = false;
+  adapterInstalled = false;
+  public adapterStatus: Message;
+  public streamDescription: SpDataStream;
+  private pollingActive = false;
+  public runtimeData: any;
+  public isSetAdapter = false;
+  public isTemplate = false;
 
-    @Input()
-    storeAsTemplate: boolean;
+  @Input()
+  storeAsTemplate: boolean;
 
-    @Input()
-    adapter: AdapterDescriptionUnion;
+  @Input()
+  adapter: AdapterDescriptionUnion;
 
-    @Input()
-    saveInDataLake: boolean;
+  @Input()
+  saveInDataLake: boolean;
 
-    @Input()
-    dataLakeTimestampField: string;
+  @Input()
+  dataLakeTimestampField: string;
 
-    constructor(
-        public dialogRef: DialogRef<AdapterStartedDialog>,
-        private restService: RestService,
-        private ShepherdService: ShepherdService) { }
+  constructor(
+    public dialogRef: DialogRef<AdapterStartedDialog>,
+    private restService: RestService,
+    private shepherdService: ShepherdService) {
+  }
 
-    ngOnInit() {
-        this.startAdapter();
-    }
+  ngOnInit() {
+    this.startAdapter();
+  }
 
-    startAdapter() {
-        if (this.storeAsTemplate) {
+  startAdapter() {
+    if (this.storeAsTemplate) {
 
-            this.restService.addAdapterTemplate(this.adapter).subscribe(x => {
-                this.adapterStatus = x as Message;
-                this.isTemplate = true;
-                this.adapterInstalled = true;
+      this.restService.addAdapterTemplate(this.adapter).subscribe(x => {
+        this.adapterStatus = x as Message;
+        this.isTemplate = true;
+        this.adapterInstalled = true;
+      });
+
+    } else {
+
+      const newAdapter = this.adapter;
+      this.restService.addAdapter(this.adapter).subscribe(x => {
+        this.adapterInstalled = true;
+        this.adapterStatus = x;
+        if (x.success) {
+
+          // Start preview on streams and message for sets
+          if (newAdapter instanceof GenericAdapterSetDescription || newAdapter instanceof SpecificAdapterSetDescription) {
+            this.isSetAdapter = true;
+          } else {
+            this.restService.getSourceDetails(x.notifications[0].title).subscribe(st => {
+              this.streamDescription = st;
+              this.pollingActive = true;
             });
+          }
 
-        } else {
-
-            var newAdapter = this.adapter;
-            this.restService.addAdapter(this.adapter).subscribe(x => {
-                this.adapterInstalled = true;
-                this.adapterStatus = x;
-                if (x.success) {
-
-                    // Start preview on streams and message for sets
-                    if (newAdapter instanceof GenericAdapterSetDescription || newAdapter instanceof SpecificAdapterSetDescription) {
-                        this.isSetAdapter = true;
-                    } else {
-                        this.restService.getSourceDetails(x.notifications[0].title).subscribe(x => {
-                            this.streamDescription = x;
-                            this.pollingActive = true;
-                        });
-                    }
-
-                    if (this.saveInDataLake) {
-                        // TODO pipeline templates are currently not working, this should be changed to use the new UI model
-                        // const templateName = "org.apache.streampipes.manager.template.instances.DataLakePipelineTemplate";
-                        // x.notifications[0].title
-                        // this.pipelineTemplateService.getPipelineTemplateInvocation(x.notifications[0].title + "/streams", templateName)
-                        //     .subscribe(res => {
-                        //
-                        //         res.list.forEach(property => {
-                        //             if (property instanceof FreeTextStaticProperty && "domId2db_measurement" == property.internalName) {
-                        //                 property.value = this.data.adapter.label.toLowerCase().replace(" ", "_");
-                        //             } else if (property instanceof MappingPropertyUnary && "domId2timestamp_mapping" == property.internalName) {
-                        //                 property.selectedProperty = "s0::" + this.data.dataLakeTimestampField;
-                        //             }
-                        //
-                        //
-                        //         });
-                        //
-                        //         res.pipelineTemplateId = templateName;
-                        //         res.name = this.data.adapter.label;
-                        //         this.pipelineTemplateService.createPipelineTemplateInvocation(res);
-                        //     });
-                    }
-                }
-            });
-
+          if (this.saveInDataLake) {
+            // TODO pipeline templates are currently not working, this should be changed to use the new UI model
+            // const templateName = "org.apache.streampipes.manager.template.instances.DataLakePipelineTemplate";
+            // x.notifications[0].title
+            // this.pipelineTemplateService.getPipelineTemplateInvocation(x.notifications[0].title + "/streams", templateName)
+            //     .subscribe(res => {
+            //
+            //         res.list.forEach(property => {
+            //             if (property instanceof FreeTextStaticProperty && "domId2db_measurement" == property.internalName) {
+            //                 property.value = this.data.adapter.label.toLowerCase().replace(" ", "_");
+            //   } else if (property instanceof MappingPropertyUnary && "domId2timestamp_mapping" == property.internalName) {
+            //                 property.selectedProperty = "s0::" + this.data.dataLakeTimestampField;
+            //             }
+            //
+            //
+            //         });
+            //
+            //         res.pipelineTemplateId = templateName;
+            //         res.name = this.data.adapter.label;
+            //         this.pipelineTemplateService.createPipelineTemplateInvocation(res);
+            //     });
+          }
         }
+      });
+
     }
+  }
 
-
-
-    onCloseConfirm() {
-        this.pollingActive = false;
-        this.dialogRef.close('Confirm');
-        this.ShepherdService.trigger("confirm_adapter_started_button");
-    }
+  onCloseConfirm() {
+    this.pollingActive = false;
+    this.dialogRef.close('Confirm');
+    this.shepherdService.trigger('confirm_adapter_started_button');
+  }
 
 }
