1e8f1374a9162769fff557a1af5f310e4a1b6507
daniel-gomm <daniel.gomm@outlook.de>
2021-10-06 11:00:13 +0200

    introduced offloading tests

--- streampipes-performance-tests/src/main/java/org/apache/streampipes/performance/TestFactory.java
+++ streampipes-performance-tests/src/main/java/org/apache/streampipes/performance/TestFactory.java
@@ -28,19 +28,23 @@ public class TestFactory {
         EvaluationLogger logger = EvaluationLogger.getInstance();
         switch (System.getenv("TEST_TYPE")){
             case "Deployment":
-                Object[] header_deployment = {"timestampInMillis", "event", "numberOfRuns", "durationInNanos", "durationInSecs"};
-                logger.logMQTT("Deployment", header_deployment);
+                Object[] header_deployment = {"timestampInMillis", "deviceId", "event", "numberOfRuns", "durationInNanos", "durationInSecs"};
+                logger.logHeader("Deployment", header_deployment);
                 return getDeploymentTest();
             case "Latency":
                 return getLatencyTest();
             case "Migration":
-                Object[] header_migration = {"timestampInMillis", "event", "numberOfRuns", "durationInNanos", "durationInSecs", "originNode", "targetNode"};
-                logger.logMQTT("Migration", header_migration);
+                Object[] header_migration = {"timestampInMillis", "deviceId", "event", "numberOfRuns", "durationInNanos", "durationInSecs", "originNode", "targetNode"};
+                logger.logHeader("Migration", header_migration);
                 return getMigrationTest();
             case "Reconfiguration":
-                Object[] header_reconfigure = {"timestampInMillis", "event", "numberOfRuns", "reconfigurationValue"};
-                logger.logMQTT("Reconfiguration", header_reconfigure);
+                Object[] header_reconfigure = {"timestampInMillis", "deviceId", "event", "numberOfRuns", "reconfigurationValue"};
+                logger.logHeader("Reconfiguration", header_reconfigure);
                 return getReconfigurationTest();
+            case "Offloading":
+                Object[] header_offloading = {"timestampInMillis", "deviceId", "event"};
+                logger.logHeader("Offloading", header_offloading);
+                return getOffloadingTest();
             default:
                 throw new RuntimeException("No test configuration found.");
         }
@@ -59,12 +63,17 @@ public class TestFactory {
 
     public static Test getMigrationTest(){
         return new GenericTest(getPipelineName(), false, true,
-                false, 15000, 5000);
+                false, 15000, 7500);
     }
 
     public static Test getReconfigurationTest(){
         return new GenericTest(getPipelineName(), false, false,
                 true, 10000, 5000);
+    }
+
+    public static Test getOffloadingTest(){
+        return new GenericTest(getPipelineName(), false, false,
+                true, 20000, 600000);
     }
 
     //Helpers
