68d110ddb818a6dbb3d1845456bbbdb6951711df
Philipp Zehnder <zehnder@fzi.de>
2021-10-11 15:05:22 +0200

    [STREAMPIPES-438] Fix store in datalake

--- ui/cypress/tests/adapter/fileStream.ts
+++ ui/cypress/tests/adapter/fileStream.ts
@@ -31,6 +31,7 @@ describe('Test File Stream Adapter', () => {
       .create('File_Stream')
       .setName('File Stream Adapter Test')
       .setTimestampProperty('timestamp')
+      .setStoreInDataLake()
       .addProtocolInput('input', 'speed', '1')
       .addProtocolInput('checkbox', 'replaceTimestamp', 'check')
       .setFormat('csv')
