cf16ba64b555fe8b86c6ce0df7a69254fa324b02
hrushi20 <hrushithegreat@gmail.com>
2021-10-21 18:59:15 +0530

    ValueChange updated

--- streampipes-processors-enricher-jvm/src/main/java/org/apache/streampipes/processors/enricher/jvm/processor/valueChange/ValueChangeController.java
+++ streampipes-processors-enricher-jvm/src/main/java/org/apache/streampipes/processors/enricher/jvm/processor/valueChange/ValueChangeController.java
@@ -1,36 +1,60 @@
 package org.apache.streampipes.processors.enricher.jvm.processor.valueChange;
 
+import org.apache.streampipes.commons.exceptions.SpRuntimeException;
 import org.apache.streampipes.model.DataProcessorType;
 import org.apache.streampipes.model.graph.DataProcessorDescription;
-import org.apache.streampipes.model.graph.DataProcessorInvocation;
+import org.apache.streampipes.model.runtime.Event;
+import org.apache.streampipes.model.schema.PropertyScope;
 import org.apache.streampipes.sdk.builder.ProcessingElementBuilder;
 import org.apache.streampipes.sdk.builder.StreamRequirementsBuilder;
-import org.apache.streampipes.sdk.extractor.ProcessingElementParameterExtractor;
 import org.apache.streampipes.sdk.helpers.*;
 import org.apache.streampipes.sdk.utils.Assets;
-import org.apache.streampipes.wrapper.standalone.ConfiguredEventProcessor;
-import org.apache.streampipes.wrapper.standalone.declarer.StandaloneEventProcessingDeclarer;
+import org.apache.streampipes.vocabulary.SO;
+import org.apache.streampipes.wrapper.context.EventProcessorRuntimeContext;
+import org.apache.streampipes.wrapper.routing.SpOutputCollector;
+import org.apache.streampipes.wrapper.standalone.ProcessorParams;
+import org.apache.streampipes.wrapper.standalone.StreamPipesDataProcessor;
 
-public class ValueChangeController extends StandaloneEventProcessingDeclarer<ValueChangeParameters>{
+public class ValueChangeController extends StreamPipesDataProcessor {
+	private static final String CHANGEVALUE_MAPPING = "changevalue-mapping";
+	private static final String IS_CHANGED = "isChanged";
+
+	private boolean isValueChanged = false;
+	private double initValue;
+
 	@Override
 	public DataProcessorDescription declareModel() {
-		return ProcessingElementBuilder.create("org.apache.streampipes.processors.enricher.jvm.valueChange")
-				.category(DataProcessorType.ENRICH)
+		return ProcessingElementBuilder.create("org.apache.streampipes.enricher.jvm.valueChange","ValueChange","A value change data processor which return a boolean on data change")
+				.category(DataProcessorType.TRANSFORM)
 				.withAssets(Assets.DOCUMENTATION, Assets.ICON)
 				.withLocales(Locales.EN)
 				.requiredStream(StreamRequirementsBuilder
 						.create()
-						.requiredProperty(EpRequirements.anyProperty())
+						.requiredPropertyWithUnaryMapping(EpRequirements.numberReq(),
+							Labels.withId(CHANGEVALUE_MAPPING),
+							PropertyScope.NONE)
 						.build())
-				.requiredStream(StreamRequirementsBuilder.create()
-						.requiredProperty(EpRequirements.anyProperty()).build())
+				.outputStrategy(OutputStrategies.append(EpProperties.booleanEp(Labels.withId(IS_CHANGED),
+					IS_CHANGED,SO.Boolean)))
 				.build();
 	}
 
 	@Override
-	public ConfiguredEventProcessor<ValueChangeParameters>
-	onInvocation(DataProcessorInvocation graph, ProcessingElementParameterExtractor extractor) {
+	public void onInvocation(ProcessorParams processorParams, SpOutputCollector spOutputCollector, EventProcessorRuntimeContext eventProcessorRuntimeContext) throws SpRuntimeException {
+		String initValue = processorParams.extractor().mappingPropertyValue(CHANGEVALUE_MAPPING);
+		this.initValue = Double.parseDouble(initValue);
+	}
 
-		return new ConfiguredEventProcessor<>();
+	@Override
+	public void onEvent(Event event, SpOutputCollector spOutputCollector) throws SpRuntimeException {
+		Double currValue = event.getFieldBySelector(IS_CHANGED).getAsPrimitive().getAsDouble();
+		if(currValue == this.initValue)
+			event.addField(IS_CHANGED,false);
+		event.addField(IS_CHANGED,true);
+	}
+
+	@Override
+	public void onDetach() throws SpRuntimeException {
+
 	}
 }
