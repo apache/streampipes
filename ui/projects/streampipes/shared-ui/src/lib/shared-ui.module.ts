e634a44454a852ce1dfc38bfcad82c314ea0566e
Dominik Riemer <dominik.riemer@gmail.com>
2022-04-17 11:51:14 +0200

    [STREAMPIPES-531] Cleanup shared-ui library

--- ui/projects/streampipes/shared-ui/src/lib/shared-ui.module.ts
+++ ui/projects/streampipes/shared-ui/src/lib/shared-ui.module.ts
@@ -1,11 +1,29 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 import { NgModule } from '@angular/core';
 import { ConfirmDialogComponent } from './dialog/confirm-dialog/confirm-dialog.component';
 import { PanelDialogComponent } from './dialog/panel-dialog/panel-dialog.component';
 import { StandardDialogComponent } from './dialog/standard-dialog/standard-dialog.component';
-import { CommonModule } from "@angular/common";
-import { PortalModule } from "@angular/cdk/portal";
-import { MatButtonModule } from "@angular/material/button";
-import { OverlayModule } from "@angular/cdk/overlay";
+import { CommonModule } from '@angular/common';
+import { PortalModule } from '@angular/cdk/portal';
+import { MatButtonModule } from '@angular/material/button';
+import { OverlayModule } from '@angular/cdk/overlay';
 
 @NgModule({
   declarations: [
