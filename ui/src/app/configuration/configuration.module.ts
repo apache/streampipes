28131f6bdaf711df3f4d00d44d085c1d55c0909a
Dominik Riemer <dominik.riemer@gmail.com>
2022-07-07 23:09:28 +0200

    [STREAMPIPES-537] Remove obsolete configuration component

--- ui/src/app/configuration/configuration.module.ts
+++ ui/src/app/configuration/configuration.module.ts
@@ -16,45 +16,43 @@
  *
  */
 
-import { NgModule } from "@angular/core";
-import { MatButtonModule } from "@angular/material/button";
-import { MatCheckboxModule } from "@angular/material/checkbox";
-import { MatGridListModule } from "@angular/material/grid-list";
-import { MatIconModule } from "@angular/material/icon";
-import { MatInputModule } from "@angular/material/input";
-import { MatTooltipModule } from "@angular/material/tooltip";
-import { FlexLayoutModule } from "@angular/flex-layout";
-import { CommonModule } from "@angular/common";
-import { FormsModule, ReactiveFormsModule } from "@angular/forms";
-
-import { ConfigurationComponent } from "./configuration.component";
-import { ConfigurationService } from "./shared/configuration.service";
-import { ConsulServiceComponent } from "./consul-service/consul-service.component";
-import { ConsulConfigsComponent } from "./consul-configs/consul-configs.component";
-import { ConsulConfigsTextComponent } from "./consul-configs-text/consul-configs-text.component";
-import { ConsulConfigsPasswordComponent } from "./consul-configs-password/consul-configs-password.component";
-import { ConsulConfigsBooleanComponent } from "./consul-configs-boolean/consul-configs-boolean.component";
-import { ConsulConfigsNumberComponent } from "./consul-configs-number/consul-configs-number.component";
-import { CustomMaterialModule } from "../CustomMaterial/custom-material.module";
-import { PipelineElementConfigurationComponent } from "./pipeline-element-configuration/pipeline-element-configuration.component";
-import { MessagingConfigurationComponent } from "./messaging-configuration/messaging-configuration.component";
-import { DragDropModule } from "@angular/cdk/drag-drop";
-import { DatalakeConfigurationComponent } from "./datalake-configuration/datalake-configuration.component";
-import { DeleteDatalakeIndexComponent } from "./dialog/delete-datalake-index/delete-datalake-index-dialog.component";
-import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
-import { SecurityConfigurationComponent } from "./security-configuration/security-configuration.component";
-import { CoreUiModule } from "../core-ui/core-ui.module";
-import { MatDividerModule } from "@angular/material/divider";
-import { SecurityUserConfigComponent } from "./security-configuration/security-user-configuration/security-user-config.component";
-import { SecurityServiceConfigComponent } from "./security-configuration/security-service-configuration/security-service-config.component";
-import { EditUserDialogComponent } from "./security-configuration/edit-user-dialog/edit-user-dialog.component";
-import { PlatformServicesModule } from "@streampipes/platform-services";
-import { SecurityUserGroupConfigComponent } from "./security-configuration/user-group-configuration/user-group-configuration.component";
-import { EditGroupDialogComponent } from "./security-configuration/edit-group-dialog/edit-group-dialog.component";
-import { EmailConfigurationComponent } from "./email-configuration/email-configuration.component";
-import { GeneralConfigurationComponent } from "./general-configuration/general-configuration.component";
-import { SecurityAuthenticationConfigurationComponent } from "./security-configuration/authentication-configuration/authentication-configuration.component";
-import { RouterModule } from "@angular/router";
+import { NgModule } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ConfigurationService } from './shared/configuration.service';
+import { ConsulServiceComponent } from './consul-service/consul-service.component';
+import { ConsulConfigsComponent } from './consul-configs/consul-configs.component';
+import { ConsulConfigsTextComponent } from './consul-configs-text/consul-configs-text.component';
+import { ConsulConfigsPasswordComponent } from './consul-configs-password/consul-configs-password.component';
+import { ConsulConfigsBooleanComponent } from './consul-configs-boolean/consul-configs-boolean.component';
+import { ConsulConfigsNumberComponent } from './consul-configs-number/consul-configs-number.component';
+import { CustomMaterialModule } from '../CustomMaterial/custom-material.module';
+import { PipelineElementConfigurationComponent } from './pipeline-element-configuration/pipeline-element-configuration.component';
+import { MessagingConfigurationComponent } from './messaging-configuration/messaging-configuration.component';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { DatalakeConfigurationComponent } from './datalake-configuration/datalake-configuration.component';
+import { DeleteDatalakeIndexComponent } from './dialog/delete-datalake-index/delete-datalake-index-dialog.component';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { SecurityConfigurationComponent } from './security-configuration/security-configuration.component';
+import { CoreUiModule } from '../core-ui/core-ui.module';
+import { MatDividerModule } from '@angular/material/divider';
+import { SecurityUserConfigComponent } from './security-configuration/security-user-configuration/security-user-config.component';
+import { SecurityServiceConfigComponent } from './security-configuration/security-service-configuration/security-service-config.component';
+import { EditUserDialogComponent } from './security-configuration/edit-user-dialog/edit-user-dialog.component';
+import { PlatformServicesModule } from '@streampipes/platform-services';
+import { SecurityUserGroupConfigComponent } from './security-configuration/user-group-configuration/user-group-configuration.component';
+import { EditGroupDialogComponent } from './security-configuration/edit-group-dialog/edit-group-dialog.component';
+import { EmailConfigurationComponent } from './email-configuration/email-configuration.component';
+import { GeneralConfigurationComponent } from './general-configuration/general-configuration.component';
+import { SecurityAuthenticationConfigurationComponent } from './security-configuration/authentication-configuration/authentication-configuration.component';
+import { RouterModule } from '@angular/router';
 import { SharedUiModule } from '../../../dist/streampipes/shared-ui';
 
 @NgModule({
@@ -114,7 +112,6 @@ import { SharedUiModule } from '../../../dist/streampipes/shared-ui';
     SharedUiModule,
   ],
   declarations: [
-    ConfigurationComponent,
     ConsulServiceComponent,
     ConsulConfigsComponent,
     ConsulConfigsTextComponent,
