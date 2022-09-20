a032126fb59f8c1e77e834f585b121956cd77c33
Philipp Zehnder <zehnder@fzi.de>
2022-09-20 15:43:06 +0200

    [STREAMPIPES-585] Improve error handling for runtime exceptions

--- streampipes-data-explorer-commons/src/main/java/org/apache/streampipes/dataexplorer/commons/influx/InfluxStore.java
+++ streampipes-data-explorer-commons/src/main/java/org/apache/streampipes/dataexplorer/commons/influx/InfluxStore.java
@@ -42,158 +42,169 @@ import java.util.concurrent.TimeUnit;
 
 public class InfluxStore {
 
-    private static final Logger LOG = LoggerFactory.getLogger(InfluxStore.class);
+  private static final Logger LOG = LoggerFactory.getLogger(InfluxStore.class);
 
-    private InfluxDB influxDb = null;
-    DataLakeMeasure measure;
+  private InfluxDB influxDb = null;
+  DataLakeMeasure measure;
 
-    Map<String, String> targetRuntimeNames = new HashMap<>();
+  Map<String, String> targetRuntimeNames = new HashMap<>();
 
-    public InfluxStore(DataLakeMeasure measure,
-                       SpConfig configStore) throws SpRuntimeException {
+  public InfluxStore(DataLakeMeasure measure,
+                     SpConfig configStore) throws SpRuntimeException {
 
-        this.measure = measure;
-        InfluxConnectionSettings settings = InfluxConnectionSettings.from(configStore);
+    this.measure = measure;
+    InfluxConnectionSettings settings = InfluxConnectionSettings.from(configStore);
 
-        // store sanitized target property runtime names in local variable
-        measure.getEventSchema()
-                .getEventProperties()
-                .forEach(ep -> targetRuntimeNames.put(ep.getRuntimeName(),
-                        InfluxNameSanitizer.renameReservedKeywords(ep.getRuntimeName())));
+    // store sanitized target property runtime names in local variable
+    measure.getEventSchema()
+        .getEventProperties()
+        .forEach(ep -> targetRuntimeNames.put(ep.getRuntimeName(),
+            InfluxNameSanitizer.renameReservedKeywords(ep.getRuntimeName())));
 
-        connect(settings);
+    connect(settings);
+  }
+
+  /**
+   * Connects to the InfluxDB Server, sets the database and initializes the batch-behaviour
+   *
+   * @throws SpRuntimeException If not connection can be established or if the database could not
+   *                            be found
+   */
+  private void connect(InfluxConnectionSettings settings) throws SpRuntimeException {
+    // Connecting to the server
+    // "http://" must be in front
+    String urlAndPort = settings.getInfluxDbHost() + ":" + settings.getInfluxDbPort();
+    influxDb = InfluxDBFactory.connect(urlAndPort, settings.getUser(), settings.getPassword());
+
+    // Checking, if server is available
+    Pong response = influxDb.ping();
+    if (response.getVersion().equalsIgnoreCase("unknown")) {
+      throw new SpRuntimeException("Could not connect to InfluxDb Server: " + urlAndPort);
     }
 
-    /**
-     * Connects to the InfluxDB Server, sets the database and initializes the batch-behaviour
-     *
-     * @throws SpRuntimeException If not connection can be established or if the database could not
-     *                            be found
-     */
-    private void connect(InfluxConnectionSettings settings) throws SpRuntimeException {
-        // Connecting to the server
-        // "http://" must be in front
-        String urlAndPort = settings.getInfluxDbHost() + ":" + settings.getInfluxDbPort();
-        influxDb = InfluxDBFactory.connect(urlAndPort, settings.getUser(), settings.getPassword());
-
-        // Checking, if server is available
-        Pong response = influxDb.ping();
-        if (response.getVersion().equalsIgnoreCase("unknown")) {
-            throw new SpRuntimeException("Could not connect to InfluxDb Server: " + urlAndPort);
-        }
-
-        String databaseName = settings.getDatabaseName();
-        // Checking whether the database exists
-        if (!databaseExists(databaseName)) {
-            LOG.info("Database '" + databaseName + "' not found. Gets created ...");
-            createDatabase(databaseName);
-        }
-
-        // setting up the database
-        influxDb.setDatabase(databaseName);
-        int batchSize = 2000;
-        int flushDuration = 500;
-        influxDb.enableBatch(batchSize, flushDuration, TimeUnit.MILLISECONDS);
+    String databaseName = settings.getDatabaseName();
+    // Checking whether the database exists
+    if (!databaseExists(databaseName)) {
+      LOG.info("Database '" + databaseName + "' not found. Gets created ...");
+      createDatabase(databaseName);
     }
 
-    private boolean databaseExists(String dbName) {
-        QueryResult queryResult = influxDb.query(new Query("SHOW DATABASES", ""));
-        for (List<Object> a : queryResult.getResults().get(0).getSeries().get(0).getValues()) {
-            if (a.get(0).equals(dbName)) {
-                return true;
-            }
-        }
-        return false;
+    // setting up the database
+    influxDb.setDatabase(databaseName);
+    int batchSize = 2000;
+    int flushDuration = 500;
+    influxDb.enableBatch(batchSize, flushDuration, TimeUnit.MILLISECONDS);
+  }
+
+  private boolean databaseExists(String dbName) {
+    QueryResult queryResult = influxDb.query(new Query("SHOW DATABASES", ""));
+    for (List<Object> a : queryResult.getResults().get(0).getSeries().get(0).getValues()) {
+      if (a.get(0).equals(dbName)) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  /**
+   * Creates a new database with the given name
+   *
+   * @param dbName The name of the database which should be created
+   */
+  private void createDatabase(String dbName) throws SpRuntimeException {
+    if (!dbName.matches("^[a-zA-Z_]\\w*$")) {
+      throw new SpRuntimeException(
+          "Database name '" + dbName + "' not allowed. Allowed names: ^[a-zA-Z_][a-zA-Z0-9_]*$");
+    }
+    influxDb.query(new Query("CREATE DATABASE \"" + dbName + "\"", ""));
+  }
+
+  /**
+   * Saves an event to the connected InfluxDB database
+   *
+   * @param event The event which should be saved
+   * @throws SpRuntimeException If the column name (key-value of the event map) is not allowed
+   */
+  public void onEvent(Event event) throws SpRuntimeException {
+    if (event == null) {
+      throw new SpRuntimeException("event is null");
     }
 
-    /**
-     * Creates a new database with the given name
-     *
-     * @param dbName The name of the database which should be created
-     */
-    private void createDatabase(String dbName) throws SpRuntimeException {
-        if (!dbName.matches("^[a-zA-Z_]\\w*$")) {
-            throw new SpRuntimeException(
-                    "Database name '" + dbName + "' not allowed. Allowed names: ^[a-zA-Z_][a-zA-Z0-9_]*$");
-        }
-        influxDb.query(new Query("CREATE DATABASE \"" + dbName + "\"", ""));
-    }
+    Long timestampValue = event.getFieldBySelector(measure.getTimestampField()).getAsPrimitive().getAsLong();
+    Point.Builder p =
+        Point.measurement(measure.getMeasureName()).time((long) timestampValue, TimeUnit.MILLISECONDS);
 
-    /**
-     * Saves an event to the connected InfluxDB database
-     *
-     * @param event The event which should be saved
-     * @throws SpRuntimeException If the column name (key-value of the event map) is not allowed
-     */
-    public void onEvent(Event event) throws SpRuntimeException {
-        if (event == null) {
-            throw new SpRuntimeException("event is null");
-        }
+    for (EventProperty ep : measure.getEventSchema().getEventProperties()) {
+      if (ep instanceof EventPropertyPrimitive) {
+        String runtimeName = ep.getRuntimeName();
 
-        Long timestampValue = event.getFieldBySelector(measure.getTimestampField()).getAsPrimitive().getAsLong();
-        Point.Builder p =
-                Point.measurement(measure.getMeasureName()).time((long) timestampValue, TimeUnit.MILLISECONDS);
+        // timestamp should not be added as a field
+        if (!measure.getTimestampField().endsWith(runtimeName)) {
+          String preparedRuntimeName = targetRuntimeNames.get(runtimeName);
 
-        for (EventProperty ep : measure.getEventSchema().getEventProperties()) {
-            if (ep instanceof EventPropertyPrimitive) {
-                String runtimeName = ep.getRuntimeName();
+          try {
+            PrimitiveField eventPropertyPrimitiveField = event.getFieldByRuntimeName(runtimeName).getAsPrimitive();
+            if (eventPropertyPrimitiveField.getRawValue() == null) {
+              LOG.warn("Field value for {} is null, ignoring value.", preparedRuntimeName);
+            } else {
 
-                // timestamp should not be added as a field
-                if (!measure.getTimestampField().endsWith(runtimeName)) {
-                    String preparedRuntimeName = targetRuntimeNames.get(runtimeName);
-                    PrimitiveField eventPropertyPrimitiveField =
-                            event.getFieldByRuntimeName(runtimeName).getAsPrimitive();
+              // store property as tag when the field is a dimension property
+              if ("DIMENSION_PROPERTY".equals(ep.getPropertyScope())) {
+                p.tag(preparedRuntimeName, eventPropertyPrimitiveField.getAsString());
+              } else {
 
-                    // store property as tag when the field is a dimension property
-                    if ("DIMENSION_PROPERTY".equals(ep.getPropertyScope())) {
-                        p.tag(preparedRuntimeName, eventPropertyPrimitiveField.getAsString());
-                    } else {
-                        try {
-                            // Store property according to property type
-                            String runtimeType = ((EventPropertyPrimitive) ep).getRuntimeType();
-                            if (XSD._integer.toString().equals(runtimeType)) {
-                                try {
-                                    p.addField(preparedRuntimeName, eventPropertyPrimitiveField.getAsInt());
-                                } catch (NumberFormatException ef) {
-                                    p.addField(preparedRuntimeName, eventPropertyPrimitiveField.getAsFloat());
-                                }
-                            } else if (XSD._float.toString().equals(runtimeType)) {
-                                p.addField(preparedRuntimeName, eventPropertyPrimitiveField.getAsFloat());
-                            } else if (XSD._double.toString().equals(runtimeType)) {
-                                p.addField(preparedRuntimeName, eventPropertyPrimitiveField.getAsDouble());
-                            } else if (XSD._boolean.toString().equals(runtimeType)) {
-                                p.addField(preparedRuntimeName, eventPropertyPrimitiveField.getAsBoolean());
-                            } else if (XSD._long.toString().equals(runtimeType)) {
-                                try {
-                                    p.addField(preparedRuntimeName, eventPropertyPrimitiveField.getAsLong());
-                                } catch (NumberFormatException ef) {
-                                    p.addField(preparedRuntimeName, eventPropertyPrimitiveField.getAsFloat());
-                                }
-                            } else {
-                                p.addField(preparedRuntimeName, eventPropertyPrimitiveField.getAsString());
-                            }
-                        } catch (NumberFormatException e) {
-                            LOG.warn("Wrong number format for field {}, ignoring.", preparedRuntimeName);
-                        }
+                try {
+                  // Store property according to property type
+                  String runtimeType = ((EventPropertyPrimitive) ep).getRuntimeType();
+                  if (XSD._integer.toString().equals(runtimeType)) {
+                    try {
+                      p.addField(preparedRuntimeName, eventPropertyPrimitiveField.getAsInt());
+                    } catch (NumberFormatException ef) {
+                      p.addField(preparedRuntimeName, eventPropertyPrimitiveField.getAsFloat());
                     }
+                  } else if (XSD._float.toString().equals(runtimeType)) {
+                    p.addField(preparedRuntimeName, eventPropertyPrimitiveField.getAsFloat());
+                  } else if (XSD._double.toString().equals(runtimeType)) {
+                    p.addField(preparedRuntimeName, eventPropertyPrimitiveField.getAsDouble());
+                  } else if (XSD._boolean.toString().equals(runtimeType)) {
+                    p.addField(preparedRuntimeName, eventPropertyPrimitiveField.getAsBoolean());
+                  } else if (XSD._long.toString().equals(runtimeType)) {
+                    try {
+                      p.addField(preparedRuntimeName, eventPropertyPrimitiveField.getAsLong());
+                    } catch (NumberFormatException ef) {
+                      p.addField(preparedRuntimeName, eventPropertyPrimitiveField.getAsFloat());
+                    }
+                  } else {
+                    p.addField(preparedRuntimeName, eventPropertyPrimitiveField.getAsString());
+                  }
+                } catch (NumberFormatException e) {
+                  LOG.warn("Wrong number format for field {}, ignoring.", preparedRuntimeName);
                 }
+              }
             }
+          } catch (SpRuntimeException iae) {
+            LOG.warn("Field {} was missing in event and will be ignored", runtimeName, iae);
+          }
+
         }
 
-        influxDb.write(p.build());
+      }
     }
 
-    /**
-     * Shuts down the connection to the InfluxDB server
-     */
-    public void close() throws SpRuntimeException {
-        influxDb.flush();
-        try {
-            Thread.sleep(1000);
-        } catch (InterruptedException e) {
-            throw new SpRuntimeException(e);
-        }
-        influxDb.close();
+    influxDb.write(p.build());
+  }
+
+  /**
+   * Shuts down the connection to the InfluxDB server
+   */
+  public void close() throws SpRuntimeException {
+    influxDb.flush();
+    try {
+      Thread.sleep(1000);
+    } catch (InterruptedException e) {
+      throw new SpRuntimeException(e);
     }
+    influxDb.close();
+  }
 
 }
