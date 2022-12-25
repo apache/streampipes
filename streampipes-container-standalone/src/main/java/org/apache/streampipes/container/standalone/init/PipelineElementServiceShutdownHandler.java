3e47592704f9c7ad1edc449f0256e5efe467ce8a
bossenti <bossenti@posteo.de>
2022-12-25 20:53:07 +0100

    add checkstyle to streampipes-container-standalone

--- streampipes-container-standalone/src/main/java/org/apache/streampipes/container/standalone/init/PipelineElementServiceShutdownHandler.java
+++ streampipes-container-standalone/src/main/java/org/apache/streampipes/container/standalone/init/PipelineElementServiceShutdownHandler.java
@@ -18,6 +18,7 @@
 package org.apache.streampipes.container.standalone.init;
 
 import org.apache.streampipes.container.init.RunningInstances;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
