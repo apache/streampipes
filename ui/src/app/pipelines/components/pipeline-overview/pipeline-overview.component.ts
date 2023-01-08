c211f4dd242bb65422f9690c02931fa3e1aa5f9a
Tim <50115603+bossenti@users.noreply.github.com>
2023-01-08 20:43:29 +0100

    [#877] apply formatting and linting to pipelines module (#1054)

--- ui/src/app/pipelines/components/pipeline-overview/pipeline-overview.component.ts
+++ ui/src/app/pipelines/components/pipeline-overview/pipeline-overview.component.ts
@@ -17,7 +17,14 @@
  */
 
 import { Pipeline } from '@streampipes/platform-services';
-import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import {
+    Component,
+    EventEmitter,
+    Input,
+    OnInit,
+    Output,
+    ViewChild,
+} from '@angular/core';
 import { PipelineOperationsService } from '../../services/pipeline-operations.service';
 import { MatTableDataSource } from '@angular/material/table';
 import { MatPaginator } from '@angular/material/paginator';
@@ -26,110 +33,134 @@ import { AuthService } from '../../../services/auth.service';
 import { UserRole } from '../../../_enums/user-role.enum';
 import { UserPrivilege } from '../../../_enums/user-privilege.enum';
 
-
 @Component({
-  selector: 'pipeline-overview',
-  templateUrl: './pipeline-overview.component.html',
-  styleUrls: ['./pipeline-overview.component.scss']
+    selector: 'sp-pipeline-overview',
+    templateUrl: './pipeline-overview.component.html',
+    styleUrls: ['./pipeline-overview.component.scss'],
 })
 export class PipelineOverviewComponent implements OnInit {
+    _pipelines: Pipeline[];
+    _activeCategoryId: string;
 
-  _pipelines: Pipeline[];
-  _activeCategoryId: string;
+    filteredPipelinesAvailable = false;
 
-  filteredPipelinesAvailable = false;
+    @Input()
+    pipelineToStart: Pipeline;
 
-  @Input()
-  pipelineToStart: Pipeline;
+    @Output()
+    refreshPipelinesEmitter: EventEmitter<boolean> =
+        new EventEmitter<boolean>();
 
-  @Output()
-  refreshPipelinesEmitter: EventEmitter<boolean> = new EventEmitter<boolean>();
+    displayedColumns: string[] = [
+        'status',
+        'start',
+        'name',
+        'lastModified',
+        'action',
+    ];
 
-  displayedColumns: string[] = ['status', 'start', 'name', 'lastModified', 'action'];
+    dataSource: MatTableDataSource<Pipeline>;
 
-  dataSource: MatTableDataSource<Pipeline>;
+    @ViewChild(MatPaginator) paginator: MatPaginator;
+    pageSize = 1;
 
-  @ViewChild(MatPaginator) paginator: MatPaginator;
-  pageSize = 1;
+    @ViewChild(MatSort) sort: MatSort;
 
-  @ViewChild(MatSort) sort: MatSort;
+    starting: any;
+    stopping: any;
 
-  starting: any;
-  stopping: any;
+    isAdmin = false;
+    hasPipelineWritePrivileges = false;
+    hasPipelineDeletePrivileges = false;
 
-  isAdmin = false;
-  hasPipelineWritePrivileges = false;
-  hasPipelineDeletePrivileges = false;
-
-  constructor(public pipelineOperationsService: PipelineOperationsService,
-              private authService: AuthService) {
-    this.starting = false;
-    this.stopping = false;
-  }
-
-  ngOnInit() {
-    this.authService.user$.subscribe(user => {
-      this.isAdmin = user.roles.indexOf(UserRole.ROLE_ADMIN) > -1;
-      this.hasPipelineWritePrivileges = this.authService.hasRole(UserPrivilege.PRIVILEGE_WRITE_PIPELINE);
-      this.hasPipelineDeletePrivileges = this.authService.hasRole(UserPrivilege.PRIVILEGE_DELETE_PIPELINE);
-    });
-    this.toggleRunningOperation = this.toggleRunningOperation.bind(this);
-
-    if (this.pipelineToStart) {
-      if (!(this.pipelineToStart.running)) {
-        this.pipelineOperationsService.startPipeline(this.pipelineToStart._id, this.refreshPipelinesEmitter, this.toggleRunningOperation);
-      }
+    constructor(
+        public pipelineOperationsService: PipelineOperationsService,
+        private authService: AuthService,
+    ) {
+        this.starting = false;
+        this.stopping = false;
     }
-  }
 
-  toggleRunningOperation(currentOperation) {
-    if (currentOperation === 'starting') {
-      this.starting = !(this.starting);
-    } else {
-      this.stopping = !(this.stopping);
+    ngOnInit() {
+        this.authService.user$.subscribe(user => {
+            this.isAdmin = user.roles.indexOf(UserRole.ROLE_ADMIN) > -1;
+            this.hasPipelineWritePrivileges = this.authService.hasRole(
+                UserPrivilege.PRIVILEGE_WRITE_PIPELINE,
+            );
+            this.hasPipelineDeletePrivileges = this.authService.hasRole(
+                UserPrivilege.PRIVILEGE_DELETE_PIPELINE,
+            );
+        });
+        this.toggleRunningOperation = this.toggleRunningOperation.bind(this);
+
+        if (this.pipelineToStart) {
+            if (!this.pipelineToStart.running) {
+                this.pipelineOperationsService.startPipeline(
+                    this.pipelineToStart._id,
+                    this.refreshPipelinesEmitter,
+                    this.toggleRunningOperation,
+                );
+            }
+        }
     }
-  }
 
-  openPipelineNotificationsDialog(pipeline: Pipeline) {
-    this.pipelineOperationsService.showPipelineNotificationsDialog(pipeline,
-        this.refreshPipelinesEmitter);
-  }
-
-  get pipelines() {
-    return this._pipelines;
-  }
-
-  @Input()
-  set pipelines(pipelines: Pipeline[]) {
-    this._pipelines = pipelines;
-    this.addPipelinesToTable();
-  }
-
-  get activeCategoryId(): string {
-    return this._activeCategoryId;
-  }
-
-  @Input()
-  set activeCategoryId(activeCategoryId: string) {
-    this._activeCategoryId = activeCategoryId;
-    if (this._pipelines) {
-      this.addPipelinesToTable();
+    toggleRunningOperation(currentOperation) {
+        if (currentOperation === 'starting') {
+            this.starting = !this.starting;
+        } else {
+            this.stopping = !this.stopping;
+        }
     }
-  }
 
-  addPipelinesToTable() {
-    this.dataSource = new MatTableDataSource<Pipeline>(this.filterPipelines());
-    setTimeout(() => {
-      this.dataSource.paginator = this.paginator;
-      this.dataSource.sort = this.sort;
-    });
-  }
+    openPipelineNotificationsDialog(pipeline: Pipeline) {
+        this.pipelineOperationsService.showPipelineNotificationsDialog(
+            pipeline,
+            this.refreshPipelinesEmitter,
+        );
+    }
 
-  filterPipelines(): Pipeline[] {
-    const filteredPipelines: Pipeline[] = this._pipelines
-        .filter(pipeline => !(this._activeCategoryId) ||
-            (pipeline.pipelineCategories && pipeline.pipelineCategories.some(pc => pc === this.activeCategoryId)));
-    this.filteredPipelinesAvailable = filteredPipelines.length > 0;
-    return filteredPipelines.sort((a, b) => a.name.localeCompare(b.name));
-  }
+    get pipelines() {
+        return this._pipelines;
+    }
+
+    @Input()
+    set pipelines(pipelines: Pipeline[]) {
+        this._pipelines = pipelines;
+        this.addPipelinesToTable();
+    }
+
+    get activeCategoryId(): string {
+        return this._activeCategoryId;
+    }
+
+    @Input()
+    set activeCategoryId(activeCategoryId: string) {
+        this._activeCategoryId = activeCategoryId;
+        if (this._pipelines) {
+            this.addPipelinesToTable();
+        }
+    }
+
+    addPipelinesToTable() {
+        this.dataSource = new MatTableDataSource<Pipeline>(
+            this.filterPipelines(),
+        );
+        setTimeout(() => {
+            this.dataSource.paginator = this.paginator;
+            this.dataSource.sort = this.sort;
+        });
+    }
+
+    filterPipelines(): Pipeline[] {
+        const filteredPipelines: Pipeline[] = this._pipelines.filter(
+            pipeline =>
+                !this._activeCategoryId ||
+                (pipeline.pipelineCategories &&
+                    pipeline.pipelineCategories.some(
+                        pc => pc === this.activeCategoryId,
+                    )),
+        );
+        this.filteredPipelinesAvailable = filteredPipelines.length > 0;
+        return filteredPipelines.sort((a, b) => a.name.localeCompare(b.name));
+    }
 }
