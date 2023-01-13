0085b8f19fb9a52e6cc609408090a40fd552b972
Aamir Shaikh <aamirshaikh3232@outlook.com>
2023-01-13 22:15:25 +0530

    [#877] Format and lint core-model module

--- ui/src/app/core-model/coco/Coco.format.ts
+++ ui/src/app/core-model/coco/Coco.format.ts
@@ -21,20 +21,18 @@ import { Category } from './Category';
 import { Image } from './Image';
 
 export class CocoFormat {
+    info: {
+        year: number;
+        version: string;
+        description: string;
+        contributor: string;
+        url: string;
+        date_created: Date;
+    };
+    licenses: [];
+    images: Image[] = [];
+    annotations: Annotation[] = [];
+    categories: Category[] = [];
 
-  info: {
-    'year': number,
-    'version': string,
-    'description': string,
-    'contributor': string,
-    'url': string,
-    'date_created': Date,
-  };
-  licenses: [];
-  images: Image[] = [];
-  annotations: Annotation[] = [];
-  categories: Category[] = [];
-
-  constructor() { }
-
+    constructor() {}
 }
