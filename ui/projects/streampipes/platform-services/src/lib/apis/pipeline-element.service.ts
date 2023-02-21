0f6bcf55f27b1c0640dd3adf9abef7b8dbc92289
Dominik Riemer <dominik.riemer@gmail.com>
2023-02-21 14:03:35 +0100

    Rename endpoint to fetch all pipelines from API (#1333) (#1334)
    * Rename endpoint to fetch all pipelines from API (#1333)
    
    * Harmonize REST endpoints for retrieval of pipeline elements (#1333)

--- ui/projects/streampipes/platform-services/src/lib/apis/pipeline-element.service.ts
+++ ui/projects/streampipes/platform-services/src/lib/apis/pipeline-element.service.ts
@@ -38,7 +38,7 @@ export class PipelineElementService {
     ) {}
 
     getDataProcessors(): Observable<DataProcessorInvocation[]> {
-        return this.http.get(this.dataProcessorsUrl + '/own').pipe(
+        return this.http.get(this.dataProcessorsUrl).pipe(
             map(data => {
                 return (data as []).map(dpi =>
                     DataProcessorInvocation.fromData(dpi),
@@ -48,7 +48,7 @@ export class PipelineElementService {
     }
 
     getDataSinks(): Observable<DataSinkInvocation[]> {
-        return this.http.get(this.dataSinksUrl + '/own').pipe(
+        return this.http.get(this.dataSinksUrl).pipe(
             map(data => {
                 return (data as []).map(dpi =>
                     DataSinkInvocation.fromData(dpi),
@@ -58,7 +58,7 @@ export class PipelineElementService {
     }
 
     getDataStreams(): Observable<SpDataStream[]> {
-        return this.http.get(this.dataStreamsUrl + '/own').pipe(
+        return this.http.get(this.dataStreamsUrl).pipe(
             map(data => {
                 return (data as []).map(dpi => {
                     if (
