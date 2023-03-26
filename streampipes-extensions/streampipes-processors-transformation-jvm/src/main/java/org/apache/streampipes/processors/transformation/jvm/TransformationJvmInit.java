83b50d6b0d4acf4a2720c1c8bbac086f59effcd3
Liu Xiao <42756849+liuxiaocs7@users.noreply.github.com>
2023-03-26 20:27:06 +0800

    Migrate StringToState processing elements  (#1444)
    * init string to state
    
    * Migrate StringToState processing elements

--- streampipes-extensions/streampipes-processors-transformation-jvm/src/main/java/org/apache/streampipes/processors/transformation/jvm/TransformationJvmInit.java
+++ streampipes-extensions/streampipes-processors-transformation-jvm/src/main/java/org/apache/streampipes/processors/transformation/jvm/TransformationJvmInit.java
@@ -43,7 +43,7 @@ import org.apache.streampipes.processors.transformation.jvm.processor.mapper.Fie
 import org.apache.streampipes.processors.transformation.jvm.processor.measurementconverter.MeasurementUnitConverterProcessor;
 import org.apache.streampipes.processors.transformation.jvm.processor.state.labeler.number.NumberLabelerController;
 import org.apache.streampipes.processors.transformation.jvm.processor.stringoperator.counter.StringCounterProcessor;
-import org.apache.streampipes.processors.transformation.jvm.processor.stringoperator.state.StringToStateController;
+import org.apache.streampipes.processors.transformation.jvm.processor.stringoperator.state.StringToStateProcessor;
 import org.apache.streampipes.processors.transformation.jvm.processor.stringoperator.timer.StringTimerController;
 import org.apache.streampipes.processors.transformation.jvm.processor.task.TaskDurationController;
 import org.apache.streampipes.processors.transformation.jvm.processor.timestampextractor.TimestampExtractorController;
@@ -84,7 +84,7 @@ public class TransformationJvmInit extends ExtensionsModelSubmitter {
             new SignalEdgeFilterController(),
             new BooleanToStateController(),
             new NumberLabelerController(),
-            new StringToStateController(),
+            new StringToStateProcessor(),
             new StringCounterProcessor(),
             new BooleanOperatorProcessor(),
             new FiledRenameProcessor())
