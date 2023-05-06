9b0ba98e3b4135d8fc9b576b26907b2b9e972738
Tim <50115603+bossenti@users.noreply.github.com>
2023-05-06 06:26:21 +0200

    [1475] fulfill PEP 561 & exclude test files from package (#1555)

--- streampipes-client-python/setup.py
+++ streampipes-client-python/setup.py
@@ -97,7 +97,14 @@ setuptools.setup(
     python_requires=REQUIRES_PYTHON,
     url=URL,
     project_urls=PROJECT_URLS,
-    packages=setuptools.find_packages(exclude=("tests",)),
+    packages=setuptools.find_packages(exclude=("*tests*",)),
+    package_data={
+        "streampipes": [
+            "py.typed",
+            "*.pyi",
+            "**/*.pyi"
+        ]
+    },
     install_requires=base_packages,
     extras_require={
         "dev": dev_packages,
@@ -105,7 +112,6 @@ setuptools.setup(
         "docs": docs_packages,
         "all": dev_packages + docs_packages,
     },
-    include_package_data=True,
     license="Apache License 2.0",
     classifiers=[
         # Trove classifiers
