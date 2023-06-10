bad6d95c8269b4f9623087c8cebf13550cda3ea4
Stefan Obermeier <st.obermeier@seeburger.de>
2023-06-10 22:22:58 +0200

    [#1672] Make processor dimension sensitive.

--- streampipes-extensions/streampipes-processors-transformation-jvm/src/main/java/org/apache/streampipes/processors/transformation/jvm/processor/value/change/ChangedValueDetectionProcessor.java
+++ streampipes-extensions/streampipes-processors-transformation-jvm/src/main/java/org/apache/streampipes/processors/transformation/jvm/processor/value/change/ChangedValueDetectionProcessor.java
@@ -24,6 +24,7 @@ import org.apache.streampipes.extensions.api.pe.routing.SpOutputCollector;
 import org.apache.streampipes.model.DataProcessorType;
 import org.apache.streampipes.model.graph.DataProcessorDescription;
 import org.apache.streampipes.model.runtime.Event;
+import org.apache.streampipes.model.schema.EventProperty;
 import org.apache.streampipes.model.schema.PropertyScope;
 import org.apache.streampipes.sdk.builder.ProcessingElementBuilder;
 import org.apache.streampipes.sdk.builder.StreamRequirementsBuilder;
@@ -36,14 +37,25 @@ import org.apache.streampipes.sdk.utils.Assets;
 import org.apache.streampipes.wrapper.params.compat.ProcessorParams;
 import org.apache.streampipes.wrapper.standalone.StreamPipesDataProcessor;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ChangedValueDetectionProcessor
     extends StreamPipesDataProcessor {
 
+  private static Logger logger = LoggerFactory.getLogger(ChangedValueDetectionProcessor.class);
+
   public static final String COMPARE_FIELD_ID = "compare";
   public static final String CHANGE_FIELD_NAME = "change_detected";
+  private Integer maxStateSize = 5000; // Max number of stored states
 
   private String compareParameter;
-  private Object lastObject = null;
+
+  private HashMap<String, Object> dimensionsState = new HashMap<>();
 
   //TODO: Change Icon
   @Override
@@ -73,9 +85,16 @@ public class ChangedValueDetectionProcessor
   public void onEvent(Event inputEvent, SpOutputCollector collector) throws SpRuntimeException {
     Object newObject = inputEvent.getFieldBySelector(compareParameter).getRawValue();
 
+    String dimensionKey = this.getDimensionKey(inputEvent);
+    var lastObject = this.dimensionsState.get(dimensionKey);
+
+    if (lastObject == null && this.dimensionsState.size() > maxStateSize) {
+      throw new SpRuntimeException("Max state size reached " + maxStateSize);
+    }
+
     if (newObject != null) {
       if (!newObject.equals(lastObject)) {
-        lastObject = newObject;
+        dimensionsState.put(dimensionKey, newObject);
         //TODO: Better handling of multiple timestamps (if the field "change_detected" is already in the input)?
         inputEvent.addField(CHANGE_FIELD_NAME, System.currentTimeMillis());
         collector.collect(inputEvent);
@@ -83,6 +102,19 @@ public class ChangedValueDetectionProcessor
     }
   }
 
+  String getDimensionKey(Event inputEvent) {
+
+    List<EventProperty> eventProperties = inputEvent.getSchemaInfo().getEventSchema().getEventProperties();
+
+    return eventProperties.stream()
+        .filter(ep ->
+          ep.getPropertyScope() != null
+          && ep.getPropertyScope().equals(PropertyScope.DIMENSION_PROPERTY.name())
+        )
+        .map(ep -> inputEvent.getFieldByRuntimeName(ep.getRuntimeName()).getRawValue().toString())
+        .sorted()
+        .collect(Collectors.joining());
+  }
   @Override
   public void onDetach() throws SpRuntimeException {
 
