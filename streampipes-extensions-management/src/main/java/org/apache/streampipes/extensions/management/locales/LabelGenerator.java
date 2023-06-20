2839241398ba85d1bcee054b9ae3e2703d0a5c60
Zike Yang <zike@apache.org>
2023-06-20 10:48:18 +0800

    Use UTF_8 as the encoding when generating the label

--- streampipes-extensions-management/src/main/java/org/apache/streampipes/extensions/management/locales/LabelGenerator.java
+++ streampipes-extensions-management/src/main/java/org/apache/streampipes/extensions/management/locales/LabelGenerator.java
@@ -34,6 +34,9 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Properties;
 
 import static org.apache.streampipes.extensions.management.util.LocalesUtil.makePath;
@@ -126,7 +129,7 @@ public class LabelGenerator {
 
   private Properties makeProperties() throws IOException {
     Properties props = new Properties();
-    props.load(loadResource());
+    props.load(new InputStreamReader(loadResource(), StandardCharsets.UTF_8));
 
     return props;
   }
