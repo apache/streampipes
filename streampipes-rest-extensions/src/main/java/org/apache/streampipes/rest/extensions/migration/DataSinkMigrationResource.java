5c4206c4ed2c2874eef0f898bb140c3a6d92cbc0
Dominik Riemer <dominik.riemer@gmail.com>
2023-11-03 16:44:49 +0100

    feat(#2122): Reorganize extension modules and modify docker images (#2124)
    * feat(#2122): Reorganize extension modules and modify docker images
    
    * Add Dockerfile
    
    * Rename extension service in docker compose

--- streampipes-rest-extensions/src/main/java/org/apache/streampipes/rest/extensions/migration/DataSinkMigrationResource.java
+++ streampipes-rest-extensions/src/main/java/org/apache/streampipes/rest/extensions/migration/DataSinkMigrationResource.java
@@ -22,7 +22,6 @@ import org.apache.streampipes.extensions.api.extractor.IDataSinkParameterExtract
 import org.apache.streampipes.extensions.api.migration.IDataSinkMigrator;
 import org.apache.streampipes.model.extensions.migration.MigrationRequest;
 import org.apache.streampipes.model.graph.DataSinkInvocation;
-import org.apache.streampipes.rest.security.AuthConstants;
 import org.apache.streampipes.rest.shared.annotation.JacksonSerialized;
 import org.apache.streampipes.sdk.extractor.DataSinkParameterExtractor;
 
@@ -32,7 +31,6 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.ExampleObject;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import org.apache.http.HttpStatus;
-import org.springframework.security.access.prepost.PreAuthorize;
 
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
@@ -47,7 +45,6 @@ public class DataSinkMigrationResource extends MigrateExtensionsResource<
   @POST
   @Consumes(MediaType.APPLICATION_JSON)
   @JacksonSerialized
-  @PreAuthorize(AuthConstants.IS_ADMIN_ROLE)
   @Operation(
           summary = "Execute the migration for a specific data sink instance", tags = {"Extensions", "Migration"},
           responses = {
