24fcde773bc71bdd7ecbb39781750d7237a735c9
Marcelfrueh <78954450+Marcelfrueh@users.noreply.github.com>
2023-11-24 15:14:35 +0100

    Add new test for widgets

--- ui/cypress/support/utils/DashboardUtils.ts
+++ ui/cypress/support/utils/DashboardUtils.ts
@@ -43,7 +43,11 @@ export class DashboardUtils {
         cy.dataCy('edit-dashboard-' + dashboardName).click();
     }
 
-    public static addWidget(pipelineName: string, widgetType: string) {
+    public static addWidget(
+        pipelineName: string,
+        widgetType: string,
+        options?,
+    ) {
         // Add raw data widget
         cy.dataCy('dashboard-add-widget').click();
 
@@ -52,16 +56,65 @@ export class DashboardUtils {
 
         // Select widget
         cy.dataCy('dashboard-select-widget-' + widgetType).click();
+
+        if (widgetType == 'area' || widgetType == 'line') {
+            cy.dataCy('min-y-axis-key').type(options.minYaxis);
+            cy.dataCy('max-y-axis-key').type(options.maxYaxis);
+        } else if (widgetType == 'gauge') {
+            cy.dataCy('min-key').type(options.minYaxis);
+            cy.dataCy('max-key').type(options.maxYaxis);
+        } else if (widgetType == 'status') {
+            cy.dataCy('interval-key').type(options.intervalKey);
+        } else if (widgetType == 'trafficlight') {
+            cy.dataCy('critical-value-key').type(options.criticalValue);
+            cy.dataCy('warning-range').type(options.warningRange);
+        }
+
         // optional configure widget
         cy.dataCy('dashboard-new-widget-next-btn').click();
 
         // Finish edit mode
         cy.dataCy('dashboard-save-edit-mode').click();
+
+        cy.wait(1000);
     }
 
     public static validateRawWidgetEvents(amountOfEvents: number) {
         cy.dataCy('dashboard-raw-item', { timeout: 10000 })
             .its('length')
             .should('be.gte', amountOfEvents);
+    }
+
+    public static removeWidgetFromDashboard(dashboardName: string) {
+        cy.visit('#/dashboard');
+        cy.dataCy('edit-dashboard-' + dashboardName).click();
+        cy.dataCy('widget-remove-button').click();
+    }
+
+    public static testWidget(
+        widgetType: string,
+        dashboardName: string,
+        options?: {
+            minYaxis?: string;
+            maxYaxis?: string;
+            intervalKey?: string;
+            criticalValue?: string;
+            warningRange?: string;
+        },
+    ) {
+        DashboardUtils.addWidget('Persist_simulator', widgetType, options);
+        if (widgetType == 'status') {
+            cy.get(`sp-dashboard-${widgetType.toLowerCase()}-widget`).should(
+                'be.visible',
+            );
+        } else if (widgetType == 'trafficlight') {
+            cy.get(`sp-traffic-light-widget`).should('be.visible');
+        } else {
+            cy.get(`sp-${widgetType.toLowerCase()}-widget`).should(
+                'be.visible',
+            );
+        }
+
+        DashboardUtils.removeWidgetFromDashboard(dashboardName);
     }
 }
