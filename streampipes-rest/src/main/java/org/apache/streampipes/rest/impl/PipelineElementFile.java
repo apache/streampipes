ae17e5155ec13e0affd1f635fd8197b21b2e0ace
Muyang Ye <muyangye@usc.edu>
2023-12-11 19:46:20 -0800

    Update streampipes-rest/src/main/java/org/apache/streampipes/rest/impl/PipelineElementFile.java
    Co-authored-by: Tim <50115603+bossenti@users.noreply.github.com>

--- streampipes-rest/src/main/java/org/apache/streampipes/rest/impl/PipelineElementFile.java
+++ streampipes-rest/src/main/java/org/apache/streampipes/rest/impl/PipelineElementFile.java
@@ -128,6 +128,5 @@ public class PipelineElementFile extends AbstractAuthGuardedRestResource {
   public Response getAllOriginalFilenames() {
     return ok(FileManager.getAllFiles().stream().map(fileMetadata -> fileMetadata.getOriginalFilename().toLowerCase())
         .toList();
-            Collectors.toList()));
   }
 }
