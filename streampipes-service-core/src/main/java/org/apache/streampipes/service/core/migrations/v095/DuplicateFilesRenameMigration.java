830ba8f6f08f911dffe977cdbecac0a4010cd815
Philipp Zehnder <tenthe@users.noreply.github.com>
2023-12-13 09:30:55 +0100

    refactor(#2181): Refactor DuplicateFilesRenameMigration

--- streampipes-service-core/src/main/java/org/apache/streampipes/service/core/migrations/v095/DuplicateFilesRenameMigration.java
+++ streampipes-service-core/src/main/java/org/apache/streampipes/service/core/migrations/v095/DuplicateFilesRenameMigration.java
@@ -22,10 +22,9 @@ import org.apache.streampipes.model.file.FileMetadata;
 import org.apache.streampipes.service.core.migrations.Migration;
 import org.apache.streampipes.storage.management.StorageDispatcher;
 
-
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class DuplicateFilesRenameMigration implements Migration {
   @Override
@@ -35,34 +34,107 @@ public class DuplicateFilesRenameMigration implements Migration {
 
   @Override
   public void executeMigration() {
-    var duplicateFiles = new HashMap<String, List<FileMetadata>>();
-    var fileMetadataStorage = StorageDispatcher.INSTANCE.getNoSqlStore().getFileMetadataStorage();
-    for (var metadata : fileMetadataStorage
-        .getAllFileMetadataDescriptions()) {
-      var originalFilename = metadata.getOriginalFilename().toLowerCase();
-      if (!duplicateFiles.containsKey(originalFilename)) {
-        duplicateFiles.put(originalFilename, new ArrayList<FileMetadata>());
-      }
-      duplicateFiles.get(originalFilename).add(metadata);
-    }
+    var fileMetadataStorage = StorageDispatcher.INSTANCE.getNoSqlStore()
+                                                        .getFileMetadataStorage();
 
-    for (var metadataList : duplicateFiles.values()) {
-      for (var i = 1; i < metadataList.size(); ++i) {
-        var metadata = metadataList.get(i);
-        var oldOriginalFilename = metadata.getOriginalFilename();
-        var indexBeforeFileType = oldOriginalFilename.lastIndexOf('.');
-        var newOriginalFilename =
-            String.format("%s(%d)%s", oldOriginalFilename.substring(0, indexBeforeFileType), i + 1,
-                oldOriginalFilename.substring(indexBeforeFileType));
-        metadata.setOriginalFilename(newOriginalFilename);
-        fileMetadataStorage.updateFileMetadata(metadata);
-      }
-    }
+    var filesToUpdate = getFilesToUpdate(fileMetadataStorage.getAllFileMetadataDescriptions());
 
+    filesToUpdate.forEach(fileMetadata -> fileMetadataStorage.updateFileMetadata(fileMetadata));
+  }
+
+
+  /**
+   * Takes the list of files and groups all files with the same name together.
+   * The result is a list for each file name that has more than one file associated with it.
+   */
+  private List<List<FileMetadata>> getListsOfFilesWithSameName(List<FileMetadata> filesWithOldName) {
+    var duplicateFileMap = filesWithOldName.stream()
+                                           .collect(
+                                               Collectors.groupingBy(file -> file.getOriginalFilename()
+                                                                                 .toLowerCase()));
+    return duplicateFileMap.values()
+                           .stream()
+                           .filter(files -> files.size() > 1)
+                           .toList();
   }
 
   @Override
   public String getDescription() {
     return "Rename files with duplicate names.";
+  }
+
+  /**
+   * Takes the files searches for duplicates and renames them and returns the files that must be updated
+   */
+  protected List<FileMetadata> getFilesToUpdate(List<FileMetadata> filesWithOldName) {
+
+    var groupsOfFilesWithSameName = getListsOfFilesWithSameName(filesWithOldName);
+
+    return groupsOfFilesWithSameName.stream()
+                                    .flatMap(filesWithSameName ->
+                                                 renameFilesWithSameName(filesWithSameName).stream())
+                                    .collect(Collectors.toList());
+  }
+
+
+  /**
+   * Takes a list of files with the same name renames them and returns the updated list
+   */
+  private List<FileMetadata> renameFilesWithSameName(List<FileMetadata> filesWithSameName) {
+    return IntStream.range(1, filesWithSameName.size())
+                    .mapToObj(i -> {
+                      var metadata = filesWithSameName.get(i);
+                      return renameFile(metadata, i);
+                    })
+                    .toList();
+  }
+
+
+  /**
+   * Takes a file and renames it with a new name based the number of occurrences of the file name
+   */
+  private FileMetadata renameFile(FileMetadata fileMetadata, int index) {
+    var oldFilename = fileMetadata.getOriginalFilename();
+
+    var fileNameWithoutType = removeFileType(oldFilename);
+    var fileTypeSuffix = getFileType(oldFilename);
+
+    var newFileName = createNewFileName(index, fileNameWithoutType, fileTypeSuffix);
+
+    fileMetadata.setOriginalFilename(newFileName);
+
+    return fileMetadata;
+  }
+
+  /**
+   * Creates the new file name for a file with a duplicate name.
+   */
+  private String createNewFileName(
+      int index,
+      String fileName,
+      String fileType
+  ) {
+    return String.format(
+        "%s(%d)%s",
+        fileName,
+        index + 1,
+        fileType
+    );
+  }
+
+  /**
+   * Returns file name without file type suffix.
+   */
+  private String removeFileType(String fileName) {
+    var indexBeforeFileType = fileName.lastIndexOf('.');
+    return fileName.substring(0, indexBeforeFileType);
+  }
+
+  /**
+   * Returns the file type a given file name.
+   */
+  private String getFileType(String fileName) {
+    var indexBeforeFileType = fileName.lastIndexOf('.');
+    return fileName.substring(indexBeforeFileType);
   }
 }
