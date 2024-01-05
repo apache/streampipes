2564090b48729e17419ee9041b0f4fd45b57b97c
Dominik Riemer <dominik.riemer@gmail.com>
2024-01-05 12:53:04 +0100

    fix: Add request body annotation to unit resource

--- streampipes-rest/src/main/java/org/apache/streampipes/rest/impl/connect/UnitResource.java
+++ streampipes-rest/src/main/java/org/apache/streampipes/rest/impl/connect/UnitResource.java
@@ -27,6 +27,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -44,7 +45,7 @@ public class UnitResource extends AbstractAdapterResource<UnitMasterManagement> 
       consumes = MediaType.APPLICATION_JSON_VALUE,
       produces = MediaType.APPLICATION_JSON_VALUE
   )
-  public ResponseEntity<?> getFittingUnits(UnitDescription unitDescription) {
+  public ResponseEntity<?> getFittingUnits(@RequestBody UnitDescription unitDescription) {
     try {
       String resultingJson = managementService.getFittingUnits(unitDescription);
       return ok(resultingJson);
