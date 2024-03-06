58bf687a7d2d6126a0424c979049fa69e223016a
Isaak <krutisaak099@gmail.com>
2024-03-06 12:48:22 -0500

    2312 updated INotificationDataSinkMigratorTest

--- streampipes-extensions/streampipes-sinks-notifications-jvm/src/test/java/org/apache/streampipes/sinks/notifications/jvm/migrations/INotificationDataSinkMigratorTest.java
+++ streampipes-extensions/streampipes-sinks-notifications-jvm/src/test/java/org/apache/streampipes/sinks/notifications/jvm/migrations/INotificationDataSinkMigratorTest.java
@@ -3,11 +3,13 @@ package org.apache.streampipes.sinks.notifications.jvm.migrations;
 import org.apache.streampipes.extensions.api.extractor.IDataSinkParameterExtractor;
 import org.apache.streampipes.model.graph.DataSinkInvocation;
 import org.apache.streampipes.model.migration.MigrationResult;
+import org.apache.streampipes.model.staticproperty.FreeTextStaticProperty;
 
 import org.junit.Test;
 
 import java.util.ArrayList;
 
+import static org.apache.streampipes.wrapper.standalone.StreamPipesNotificationSink.DEFAULT_WAITING_TIME_MINUTES;
 import static org.apache.streampipes.wrapper.standalone.StreamPipesNotificationSink.KEY_SILENT_PERIOD;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.mock;
@@ -16,7 +18,7 @@ import static org.mockito.Mockito.spy;
 public class INotificationDataSinkMigratorTest {
 
   @Test
-  public void migrate() {
+  public void testNotificationDataSinkMigration() {
 
     INotificationDataSinkMigrator migrator = spy(INotificationDataSinkMigrator.class);
     IDataSinkParameterExtractor extractor = mock(IDataSinkParameterExtractor.class);
@@ -26,7 +28,10 @@ public class INotificationDataSinkMigratorTest {
 
     MigrationResult<DataSinkInvocation> result = migrator.migrate(dataSinkInvocation, extractor);
 
-    assertEquals(result.element().getStaticProperties().size(), 1);
-    assertEquals(result.element().getStaticProperties().get(0).getInternalName(), KEY_SILENT_PERIOD);
+    assertEquals(1, result.element().getStaticProperties().size());
+
+    var property = (FreeTextStaticProperty) result.element().getStaticProperties().get(0);
+    assertEquals(KEY_SILENT_PERIOD, property.getInternalName());
+    assertEquals(DEFAULT_WAITING_TIME_MINUTES, Integer.valueOf(property.getValue()).intValue());
   }
 }
