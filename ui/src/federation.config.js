6a9cb6fa67c7c7534139c1271723e170aa9b8cdd
Dominik Riemer <dominik.riemer@gmail.com>
2024-03-30 12:39:02 +0100

    Fix tsconfig

--- ui/src/federation.config.js
+++ ui/src/federation.config.js
@@ -18,292 +18,35 @@
 
 const {
     withNativeFederation,
-    shareAll,
+    share,
 } = require('@angular-architects/native-federation/config');
 
 module.exports = withNativeFederation({
-    shared: {
-        // ...shareAll({
-        //     singleton: true,
-        //     strictVersion: true,
-        //     requiredVersion: 'auto',
-        // }),
-        '@angular/animations': {
-            singleton: true,
-            strictVersion: true,
-            requiredVersion: 'auto',
-            eager: true,
-        },
-        '@angular/animations/browser': {
-            singleton: true,
-            strictVersion: true,
-            requiredVersion: 'auto',
-            eager: true,
-        },
-        '@angular/core': {
-            singleton: true,
-            strictVersion: true,
-            requiredVersion: 'auto',
-            eager: true,
-        },
-        '@angular/common': {
-            singleton: true,
-            strictVersion: true,
-            requiredVersion: 'auto',
-            eager: true,
-        },
-        '@angular/common/http': {
-            singleton: true,
-            strictVersion: true,
-            requiredVersion: 'auto',
-            eager: true,
-        },
-        '@angular/compiler': {
-            singleton: true,
-            strictVersion: true,
-            requiredVersion: 'auto',
-            eager: true,
-        },
-        '@angular/forms': {
-            singleton: true,
-            strictVersion: true,
-            requiredVersion: 'auto',
-            eager: true,
-        },
-        '@angular/platform-browser': {
-            singleton: true,
-            strictVersion: true,
-            requiredVersion: 'auto',
-            eager: true,
-        },
-        '@angular/platform-browser/animations': {
-            singleton: true,
-            strictVersion: true,
-            requiredVersion: 'auto',
-            eager: true,
-        },
-        '@angular/platform-browser-dynamic': {
-            singleton: true,
-            strictVersion: true,
-            requiredVersion: 'auto',
-            eager: true,
-        },
-        '@angular/router': {
-            singleton: true,
-            strictVersion: true,
-            requiredVersion: 'auto',
-            eager: true,
-        },
-        // '@angular/cdk': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/cdk/a11y': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/cdk/accordion': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/cdk/bidi': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/cdk/coercion': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/cdk/collections': {
-        //   singleton: true,
-        //   strictVersion: false,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/cdk/keycodes': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/cdk/layout': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/cdk/observers': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/cdk/platform': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/cdk/scrolling': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/cdk/stepper': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/cdk/table': {
-        //   singleton: true,
-        //   strictVersion: false,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/cdk/overlay': {
-        //   singleton: true,
-        //   strictVersion: false,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/cdk/portal': {
-        //   singleton: true,
-        //   strictVersion: false,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material/autocomplete': {
-        //   singleton: true,
-        //   strictVersion: false,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material/core': {
-        //   singleton: true,
-        //   strictVersion: false,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material/button': {
-        //   singleton: true,
-        //   strictVersion: false,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material/checkbox': {
-        //   singleton: true,
-        //   strictVersion: false,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material/grid-list': {
-        //   singleton: true,
-        //   strictVersion: false,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material/icon': {
-        //   singleton: true,
-        //   strictVersion: false,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material/input': {
-        //   singleton: true,
-        //   strictVersion: false,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material/list': {
-        //   singleton: true,
-        //   strictVersion: false,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material/menu': {
-        //   singleton: true,
-        //   strictVersion: false,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material/progress-spinner': {
-        //   singleton: true,
-        //   strictVersion: false,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material/tabs': {
-        //   singleton: true,
-        //   strictVersion: false,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material/tooltip': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material/dialog': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material/divider': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material/select': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        // '@angular/material/form-field': {
-        //   singleton: true,
-        //   strictVersion: true,
-        //   requiredVersion: 'auto',
-        //   eager: true,
-        // },
-        '@angular/core/primitives/signals': {
-            singleton: true,
-            strictVersion: true,
-            requiredVersion: 'auto',
-            eager: true,
-        },
-    },
+    shared: share(
+        [
+            '@angular/animations',
+            '@angular/core',
+            '@angular/common',
+            '@angular/compiler',
+            '@angular/forms',
+            '@angular/platform-browser',
+            '@angular/platform-browser-dynamic',
+            '@angular/router',
+            '@angular/cdk',
+            '@angular/material',
+        ].reduce(
+            (acc, name) => ({
+                ...acc,
+                [name]: {
+                    singleton: true,
+                    strictVersion: true,
+                    requiredVersion: 'auto',
+                    includeSecondaries: true,
+                },
+            }),
+            {},
+        ),
+    ),
 
-    skip: [
-        // pkg => {
-        //     return (
-        //         (!pkg.startsWith('@angular') &&
-        //             !pkg.startsWith('@ngbracket') &&
-        //             !pkg.startsWith('@streampipes')) ||
-        //         pkg.startsWith('@angular/platform-server') ||
-        //         pkg.startsWith('@ngbracket/ngx-layout/server')
-        //     );
-        // },
-        // Add further packages you don't need at runtime
-    ],
+    skip: [],
 });
