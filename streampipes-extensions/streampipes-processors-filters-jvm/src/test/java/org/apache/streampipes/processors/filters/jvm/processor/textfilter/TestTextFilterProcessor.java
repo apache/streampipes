399f3f48f33ca9231d96819d8a792d974104da10
Paulo Johannes Ambrocio <pambrocio@gmail.com>
2024-04-25 12:50:46 +0800

    2773 fix TestSwingingDoorTrendingFilterProcessor

--- streampipes-extensions/streampipes-processors-filters-jvm/src/test/java/org/apache/streampipes/processors/filters/jvm/processor/textfilter/TestTextFilterProcessor.java
+++ streampipes-extensions/streampipes-processors-filters-jvm/src/test/java/org/apache/streampipes/processors/filters/jvm/processor/textfilter/TestTextFilterProcessor.java
@@ -18,101 +18,117 @@
 
 package org.apache.streampipes.processors.filters.jvm.processor.textfilter;
 
-//@RunWith(Parameterized.class)
+import org.apache.streampipes.model.graph.DataProcessorDescription;
+import org.apache.streampipes.model.graph.DataProcessorInvocation;
+import org.apache.streampipes.model.runtime.Event;
+import org.apache.streampipes.model.runtime.EventFactory;
+import org.apache.streampipes.model.runtime.SchemaInfo;
+import org.apache.streampipes.model.runtime.SourceInfo;
+import org.apache.streampipes.model.staticproperty.FreeTextStaticProperty;
+import org.apache.streampipes.model.staticproperty.MappingPropertyUnary;
+import org.apache.streampipes.model.staticproperty.OneOfStaticProperty;
+import org.apache.streampipes.test.extensions.api.StoreEventCollector;
+import org.apache.streampipes.test.generator.EventStreamGenerator;
+import org.apache.streampipes.test.generator.InvocationGraphGenerator;
+import org.apache.streampipes.test.generator.grounding.EventGroundingGenerator;
+import org.apache.streampipes.wrapper.params.compat.ProcessorParams;
+
+import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class TestTextFilterProcessor {
 
-//  private static final Logger LOG = LoggerFactory.getLogger(TestTextFilterProcessor.class);
-//
-//  @org.junit.runners.Parameterized.Parameters
-//  public static Iterable<Object[]> data() {
-//    return Arrays.asList(new Object[][] {
-//        {"TestLowerCaseMatch", "keyword", StringOperator.MATCHES, Arrays.asList("keyword", "KeyWord", "KEYWORD"), 1},
-//        {"TestUpperCaseMatch", "KEYWORD", StringOperator.MATCHES, Arrays.asList("keyword", "KeyWord", "KEYWORD"), 1},
-//        {"TestMixMatch", "KeyWord", StringOperator.MATCHES, Arrays.asList("keyword", "KeyWord", "KEYWORD"), 1},
-//        {"TestEmptyMatch", "keYWord", StringOperator.MATCHES, Arrays.asList("keyword", "KeyWord", "KEYWORD"), 0},
-//        {"TestMultipleMatch", "KeyWord", StringOperator.MATCHES,
-//            Arrays.asList("keyword", "KeyWord", "KEYWORD", "KeyWord"), 2},
-//
-//        {"TestContainsWord", "keyword", StringOperator.CONTAINS,
-//            Arrays.asList("text contains keyword", "text doesn't have word"), 1},
-//        {"TestNotContainsWord", "keyword", StringOperator.CONTAINS,
-//            Arrays.asList("text is empty", "text doesn't have word"), 0},
-//    });
-//  }
-//
-//  @org.junit.runners.Parameterized.Parameter
-//  public String selectedFieldName;
-//
-//  @org.junit.runners.Parameterized.Parameter(1)
-//  public String keyword;
-//
-//  @org.junit.runners.Parameterized.Parameter(2)
-//  public StringOperator stringOperator;
-//
-//  @org.junit.runners.Parameterized.Parameter(3)
-//  public List<String> eventStrings;
-//
-//  @org.junit.runners.Parameterized.Parameter(4)
-//  public int expectedFilteredTextCount;
-//
-//  @Test
-//  public void testTextFilter() {
-//    TextFilterProcessor textFilterProcessor = new TextFilterProcessor();
-//    DataProcessorDescription originalGraph = textFilterProcessor.declareModel();
-//    originalGraph.setSupportedGrounding(EventGroundingGenerator.makeDummyGrounding());
-//
-//    DataProcessorInvocation graph = InvocationGraphGenerator.makeEmptyInvocation(originalGraph);
-//    graph.setInputStreams(Collections
-//        .singletonList(EventStreamGenerator
-//            .makeStreamWithProperties(Collections.singletonList(selectedFieldName))));
-//    graph.setOutputStream(EventStreamGenerator
-//    .makeStreamWithProperties(Collections.singletonList(selectedFieldName)));
-//    graph.getOutputStream().getEventGrounding().getTransportProtocol().getTopicDefinition()
-//        .setActualTopicName("output-topic");
-//
-//    graph.getStaticProperties().stream()
-//        .filter(p -> p instanceof MappingPropertyUnary)
-//        .map(p -> (MappingPropertyUnary) p)
-//        .filter(p -> p.getInternalName().equals(TextFilterProcessor.MAPPING_PROPERTY_ID))
-//        .findFirst().get().setSelectedProperty("s0::" + selectedFieldName);
-//    ProcessorParams params = new ProcessorParams(graph);
-//    params.extractor().getStaticPropertyByName(TextFilterProcessor.OPERATION_ID, OneOfStaticProperty.class)
-//        .getOptions()
-//        .stream().filter(o -> o.getName().equals(stringOperator.name())).findFirst().get().setSelected(true);
-//    params.extractor().getStaticPropertyByName(TextFilterProcessor.KEYWORD_ID, FreeTextStaticProperty.class)
-//        .setValue(keyword);
-//    StoreEventCollector collector = new StoreEventCollector();
-//
-//    textFilterProcessor.onInvocation(params, collector, null);
-//    int result = sendEvents(textFilterProcessor, collector);
-//
-//    LOG.info("Expected filtered text count is {}", expectedFilteredTextCount);
-//    LOG.info("Actual filtered text count is {}", result);
-//    assertEquals(expectedFilteredTextCount, result);
-//
-//  }
-//
-//  private int sendEvents(TextFilterProcessor textFilterProcessor, StoreEventCollector collector) {
-//    List<Event> events = makeEvents();
-//    for (Event e : events) {
-//      textFilterProcessor.onEvent(e, collector);
-//    }
-//    return collector.getEvents().size();
-//  }
-//
-//  private List<Event> makeEvents() {
-//    List<Event> events = new ArrayList<>();
-//    for (String eventString : eventStrings) {
-//      events.add(makeEvent(eventString));
-//    }
-//    return events;
-//  }
-//
-//  private Event makeEvent(String eventString) {
-//    Map<String, Object> map = Maps.newHashMap();
-//    map.put(selectedFieldName, eventString);
-//    return EventFactory.fromMap(map,
-//        new SourceInfo("test", "s0"),
-//        new SchemaInfo(null, Lists.newArrayList()));
-//  }
+  private static final Logger LOG = LoggerFactory.getLogger(TestTextFilterProcessor.class);
+
+  public static Iterable<Object[]> data() {
+    return Arrays.asList(new Object[][]{
+        {"TestLowerCaseMatch", "keyword", StringOperator.MATCHES, Arrays.asList("keyword", "KeyWord", "KEYWORD"), 1},
+        {"TestUpperCaseMatch", "KEYWORD", StringOperator.MATCHES, Arrays.asList("keyword", "KeyWord", "KEYWORD"), 1},
+        {"TestMixMatch", "KeyWord", StringOperator.MATCHES, Arrays.asList("keyword", "KeyWord", "KEYWORD"), 1},
+        {"TestEmptyMatch", "keYWord", StringOperator.MATCHES, Arrays.asList("keyword", "KeyWord", "KEYWORD"), 0},
+        {"TestMultipleMatch", "KeyWord", StringOperator.MATCHES,
+            Arrays.asList("keyword", "KeyWord", "KEYWORD", "KeyWord"), 2},
+
+        {"TestContainsWord", "keyword", StringOperator.CONTAINS,
+            Arrays.asList("text contains keyword", "text doesn't have word"), 1},
+        {"TestNotContainsWord", "keyword", StringOperator.CONTAINS,
+            Arrays.asList("text is empty", "text doesn't have word"), 0},
+    });
+  }
+
+  @ParameterizedTest
+  @MethodSource("data")
+  public void testTextFilter(String selectedFieldName, String keyword, StringOperator stringOperator,
+                             List<String> eventStrings, int expectedFilteredTextCount) {
+    TextFilterProcessor textFilterProcessor = new TextFilterProcessor();
+    DataProcessorDescription originalGraph = textFilterProcessor.declareModel();
+    originalGraph.setSupportedGrounding(EventGroundingGenerator.makeDummyGrounding());
+
+    DataProcessorInvocation graph = InvocationGraphGenerator.makeEmptyInvocation(originalGraph);
+    graph.setInputStreams(Collections
+        .singletonList(EventStreamGenerator
+            .makeStreamWithProperties(Collections.singletonList(selectedFieldName))));
+    graph.setOutputStream(EventStreamGenerator
+        .makeStreamWithProperties(Collections.singletonList(selectedFieldName)));
+    graph.getOutputStream().getEventGrounding().getTransportProtocol().getTopicDefinition()
+        .setActualTopicName("output-topic");
+
+    graph.getStaticProperties().stream()
+        .filter(p -> p instanceof MappingPropertyUnary)
+        .map(p -> (MappingPropertyUnary) p)
+        .filter(p -> p.getInternalName().equals(TextFilterProcessor.MAPPING_PROPERTY_ID))
+        .findFirst().get().setSelectedProperty("s0::" + selectedFieldName);
+    ProcessorParams params = new ProcessorParams(graph);
+    params.extractor().getStaticPropertyByName(TextFilterProcessor.OPERATION_ID, OneOfStaticProperty.class)
+        .getOptions()
+        .stream().filter(o -> o.getName().equals(stringOperator.name())).findFirst().get().setSelected(true);
+    params.extractor().getStaticPropertyByName(TextFilterProcessor.KEYWORD_ID, FreeTextStaticProperty.class)
+        .setValue(keyword);
+    StoreEventCollector collector = new StoreEventCollector();
+
+    textFilterProcessor.onInvocation(params, collector, null);
+    int result = sendEvents(textFilterProcessor, collector, eventStrings, selectedFieldName);
+
+    LOG.info("Expected filtered text count is {}", expectedFilteredTextCount);
+    LOG.info("Actual filtered text count is {}", result);
+    assertEquals(expectedFilteredTextCount, result);
+
+  }
+
+  private int sendEvents(TextFilterProcessor textFilterProcessor, StoreEventCollector collector,
+                         List<String> eventStrings, String selectedFieldName) {
+    List<Event> events = makeEvents(eventStrings, selectedFieldName);
+    for (Event e : events) {
+      textFilterProcessor.onEvent(e, collector);
+    }
+    return collector.getEvents().size();
+  }
+
+  private List<Event> makeEvents(List<String> eventStrings, String selectedFieldName) {
+    List<Event> events = new ArrayList<>();
+    for (String eventString : eventStrings) {
+      events.add(makeEvent(eventString, selectedFieldName));
+    }
+    return events;
+  }
+
+  private Event makeEvent(String eventString, String selectedFieldName) {
+    Map<String, Object> map = Maps.newHashMap();
+    map.put(selectedFieldName, eventString);
+    return EventFactory.fromMap(map,
+        new SourceInfo("test", "s0"),
+        new SchemaInfo(null, Lists.newArrayList()));
+  }
 }
