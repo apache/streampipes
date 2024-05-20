cd1f2d1412d25f4d729ed920e9d58ded7227a399
Isaak <krutisaak099@gmail.com>
2024-05-19 22:47:20 -0400

    #2661 moved error handling to backend

--- ui/src/app/login/components/register/register.component.ts
+++ ui/src/app/login/components/register/register.component.ts
@@ -81,11 +81,7 @@ export class RegisterComponent implements OnInit {
             error => {
                 this.registrationInProcess = false;
                 this.registrationSuccess = false;
-                if (error.error.notifications) {
-                    this.registrationError = error.error.notifications[0].title;
-                } else {
-                    this.registrationError = error.error.error;
-                }
+                this.registrationError = error.error.notifications[0].title;
             },
         );
     }
