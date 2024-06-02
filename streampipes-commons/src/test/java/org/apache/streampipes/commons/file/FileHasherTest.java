ead9deb0e6fa6c2876c74b8e9b56ab63c10655e9
Dominik Riemer <dominik.riemer@gmail.com>
2024-06-02 16:45:19 +0200

    Disable FileHasher test

--- streampipes-commons/src/test/java/org/apache/streampipes/commons/file/FileHasherTest.java
+++ streampipes-commons/src/test/java/org/apache/streampipes/commons/file/FileHasherTest.java
@@ -19,6 +19,7 @@
 package org.apache.streampipes.commons.file;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -27,6 +28,7 @@ import java.io.IOException;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+@Disabled
 class FileHasherTest {
 
   private FileHasher fileHasher;
