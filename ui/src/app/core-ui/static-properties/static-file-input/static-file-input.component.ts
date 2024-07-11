060bcdc62248c7436e62350b26c2076b71ce72e0
Philipp Zehnder <tenthe@users.noreply.github.com>
2024-07-11 21:57:37 +0200

    fix(#3014): Fix linting

--- ui/src/app/core-ui/static-properties/static-file-input/static-file-input.component.ts
+++ ui/src/app/core-ui/static-properties/static-file-input/static-file-input.component.ts
@@ -16,12 +16,7 @@
  *
  */
 
-import {
-    Component,
-    EventEmitter,
-    OnInit,
-    Output,
-} from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { HttpEventType, HttpResponse } from '@angular/common/http';
 import {
     FilesService,
