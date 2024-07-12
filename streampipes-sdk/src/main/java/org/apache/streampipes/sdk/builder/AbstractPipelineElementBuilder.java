345975b4bd4b4af3c2e3d4a2334cfbaa77e5ab8b
Philipp Zehnder <tenthe@users.noreply.github.com>
2024-07-12 16:32:33 +0200

    refactor: Remove method providesAssets from AbstractPipelineElementBuilder

--- streampipes-sdk/src/main/java/org/apache/streampipes/sdk/builder/AbstractPipelineElementBuilder.java
+++ streampipes-sdk/src/main/java/org/apache/streampipes/sdk/builder/AbstractPipelineElementBuilder.java
@@ -45,14 +45,6 @@ public abstract class AbstractPipelineElementBuilder<X extends AbstractPipelineE
     this.elementDescription.setAppId(appId);
   }
 
-  @Deprecated
-  /**
-   * @deprecated: Use {@link #withAssets(String...)} instead
-   */
-  public X providesAssets(String... assets) {
-    return withAssets(assets);
-  }
-
   public X withAssets(String... assets) {
     this.elementDescription.setIncludesAssets(true);
     this.elementDescription.setIncludedAssets(Arrays.asList(assets));
