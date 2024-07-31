e2ed7d8c8fb9e0d32f381f3588338debf49461fa
Philipp Zehnder <tenthe@users.noreply.github.com>
2024-07-31 10:29:12 +0200

    refactor(#3074): Refactor OAuthConfigurationParser

--- streampipes-commons/src/main/java/org/apache/streampipes/commons/environment/parser/OAuthConfigurationParser.java
+++ streampipes-commons/src/main/java/org/apache/streampipes/commons/environment/parser/OAuthConfigurationParser.java
@@ -26,6 +26,25 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+/**
+ * The {@code OAuthConfigurationParser} class is responsible for parsing OAuth provider configurations
+ * from environment variables and converting them into a list of {@link OAuthConfiguration} objects.
+ *
+ * <p>This class expects the environment variables to follow a specific naming convention:
+ * {@code SP_OAUTH_{provider}_{settings}}. The parser identifies each provider by its unique
+ * identifier (e.g., "github" or "azure") and maps the settings (such as "CLIENT_ID", "CLIENT_SECRET")
+ * to their corresponding properties in the {@link OAuthConfiguration} object.</p>
+ *
+ * <p>Since environment variables cannot be structured as lists, the configuration for each provider
+ * is derived from prefixed variables. For example, settings for a provider "github" could be
+ * specified as:
+ * <ul>
+ *   <li>SP_OAUTH_GITHUB_CLIENT_ID=example-client-id</li>
+ *   <li>SP_OAUTH_GITHUB_CLIENT_SECRET=example-client-secret</li>
+ *   <li>...</li>
+ * </ul>
+ * The parser then groups these settings into a {@link OAuthConfiguration} object for "github".</p>
+ */
 public class OAuthConfigurationParser {
 
   private static final String OAUTH_PREFIX = "SP_OAUTH_PROVIDER";
@@ -33,63 +52,73 @@ public class OAuthConfigurationParser {
   public List<OAuthConfiguration> parse(Map<String, String> env) {
     Map<String, OAuthConfiguration> oAuthConfigurationsMap = new HashMap<>();
 
-    for (Map.Entry<String, String> entry : env.entrySet()) {
-      String key = entry.getKey();
+
+    env.forEach((key, value) -> {
       if (key.startsWith(OAUTH_PREFIX)) {
-        String[] parts = key.split("_");
-        if (parts.length >= 5) {
-          String registrationId = parts[3].toLowerCase();
-          String settingName = String.join("_", Arrays.copyOfRange(parts, 4, parts.length));
-
-          OAuthConfiguration config = oAuthConfigurationsMap.getOrDefault(registrationId, new OAuthConfiguration());
-          config.setRegistrationId(registrationId);
-
-          switch (settingName) {
-            case "AUTHORIZATION_URI":
-              config.setAuthorizationUri(entry.getValue());
-              break;
-            case "CLIENT_NAME":
-              config.setClientName(entry.getValue());
-              break;
-            case "CLIENT_ID":
-              config.setClientId(entry.getValue());
-              break;
-            case "CLIENT_SECRET":
-              config.setClientSecret(entry.getValue());
-              break;
-            case "FULL_NAME_ATTRIBUTE_NAME":
-              config.setFullNameAttributeName(entry.getValue());
-              break;
-            case "ISSUER_URI":
-              config.setIssuerUri(entry.getValue());
-              break;
-            case "JWK_SET_URI":
-              config.setJwkSetUri(entry.getValue());
-              break;
-            case "SCOPES":
-              config.setScopes(entry.getValue().split(","));
-              break;
-            case "TOKEN_URI":
-              config.setTokenUri(entry.getValue());
-              break;
-            case "USER_INFO_URI":
-              config.setUserInfoUri(entry.getValue());
-              break;
-            case "EMAIL_ATTRIBUTE_NAME":
-              config.setEmailAttributeName(entry.getValue());
-              break;
-            case "USER_ID_ATTRIBUTE_NAME":
-              config.setUserIdAttributeName(entry.getValue());
-              break;
-            case "NAME":
-              config.setRegistrationName(entry.getValue());
-          }
-
-          oAuthConfigurationsMap.put(registrationId, config);
-        }
+        parseEnvironmentVariable(key, value, oAuthConfigurationsMap);
       }
-    }
+    });
 
     return new ArrayList<>(oAuthConfigurationsMap.values());
+  }
+
+  private void parseEnvironmentVariable(
+      String key,
+      String value,
+      Map<String, OAuthConfiguration> oAuthConfigurationsMap
+  ) {
+    var parts = getParts(key);
+    if (parts.length >= 5) {
+      // containst the identifier of the provider (e.g. azure, github, ...)
+      var registrationId = getRegistrationId(parts);
+      var settingName = getSettingName(parts);
+
+      var oAuthConfiguration = getOrCreateOAuthConfiguration(oAuthConfigurationsMap, registrationId);
+      oAuthConfiguration.setRegistrationId(registrationId);
+
+      switch (settingName) {
+        case "AUTHORIZATION_URI" -> oAuthConfiguration.setAuthorizationUri(value);
+        case "CLIENT_NAME" -> oAuthConfiguration.setClientName(value);
+        case "CLIENT_ID" -> oAuthConfiguration.setClientId(value);
+        case "CLIENT_SECRET" -> oAuthConfiguration.setClientSecret(value);
+        case "FULL_NAME_ATTRIBUTE_NAME" -> oAuthConfiguration.setFullNameAttributeName(value);
+        case "ISSUER_URI" -> oAuthConfiguration.setIssuerUri(value);
+        case "JWK_SET_URI" -> oAuthConfiguration.setJwkSetUri(value);
+        case "SCOPES" -> oAuthConfiguration.setScopes(value.split(","));
+        case "TOKEN_URI" -> oAuthConfiguration.setTokenUri(value);
+        case "USER_INFO_URI" -> oAuthConfiguration.setUserInfoUri(value);
+        case "EMAIL_ATTRIBUTE_NAME" -> oAuthConfiguration.setEmailAttributeName(value);
+        case "USER_ID_ATTRIBUTE_NAME" -> oAuthConfiguration.setUserIdAttributeName(value);
+        case "NAME" -> oAuthConfiguration.setRegistrationName(value);
+      }
+    }
+  }
+
+  private static String[] getParts(String key) {
+    return key.split("_");
+  }
+
+  private static String getSettingName(String[] parts) {
+    return String.join("_", Arrays.copyOfRange(parts, 4, parts.length));
+  }
+
+  private static String getRegistrationId(String[] parts) {
+    return parts[3].toLowerCase();
+  }
+
+  /**
+   * Retrieves an existing OAuthConfiguration for the given providerId or creates a new one if it does not exist.
+   *
+   * @param oAuthConfigurationsMap The map containing existing OAuthConfiguration objects.
+   * @param registrationId         The identifier of the OAuth provider.
+   * @return The existing or newly created OAuthConfiguration for the given providerId.
+   */
+  private OAuthConfiguration getOrCreateOAuthConfiguration(
+      Map<String, OAuthConfiguration> oAuthConfigurationsMap,
+      String registrationId
+  ) {
+    var oAuthConfiguration = oAuthConfigurationsMap.computeIfAbsent(registrationId, k -> new OAuthConfiguration());
+    oAuthConfiguration.setRegistrationId(registrationId);
+    return oAuthConfiguration;
   }
 }
