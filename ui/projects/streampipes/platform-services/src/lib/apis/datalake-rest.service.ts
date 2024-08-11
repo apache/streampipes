ef289fb3bb9f0933bc6ec75670e1b1941a1d657a
Dominik Riemer <dominik.riemer@gmail.com>
2024-08-11 22:45:43 +0200

    Cleanup

--- ui/projects/streampipes/platform-services/src/lib/apis/datalake-rest.service.ts
+++ ui/projects/streampipes/platform-services/src/lib/apis/datalake-rest.service.ts
@@ -17,17 +17,11 @@
  */
 
 import { Injectable } from '@angular/core';
-import {
-    HttpClient,
-    HttpContext,
-    HttpParams,
-    HttpRequest,
-} from '@angular/common/http';
+import { HttpClient, HttpParams, HttpRequest } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { DataLakeMeasure, SpQueryResult } from '../model/gen/streampipes-model';
 import { map } from 'rxjs/operators';
 import { DatalakeQueryParameters } from '../model/datalake/DatalakeQueryParameters';
-import { NGX_LOADING_BAR_IGNORED } from '@ngx-loading-bar/http-client';
 
 @Injectable({
     providedIn: 'root',
