5a267aea9f54ed631684298348eedd2ef9e9e015
ittuann <ittuann@outlook.com>
2024-08-12 14:25:54 +0800

    Update pre-commit hooks
    Signed-off-by: ittuann <ittuann@outlook.com>

--- streampipes-client-python/tests/functions/test_function_handler.py
+++ streampipes-client-python/tests/functions/test_function_handler.py
@@ -373,9 +373,7 @@ class TestFunctionHandler(TestCase):
             )
         )
 
-        output_stream = create_data_stream("test",
-                                           attributes={"number": RuntimeType.INTEGER.value},
-                                           stream_id='a1d')
+        output_stream = create_data_stream("test", attributes={"number": RuntimeType.INTEGER.value}, stream_id="a1d")
         test_function = TestFunctionOutput(
             function_definition=FunctionDefinition(
                 consumed_streams=["urn:streampipes.apache.org:eventstream:uPDKLI"]
@@ -438,10 +436,7 @@ class TestFunctionHandler(TestCase):
         )
 
         output_stream = create_data_stream(
-            "test",
-            stream_id='a1c',
-            attributes={"number": RuntimeType.INTEGER.value},
-            broker=SupportedBroker.KAFKA
+            "test", stream_id="a1c", attributes={"number": RuntimeType.INTEGER.value}, broker=SupportedBroker.KAFKA
         )
         test_function = TestFunctionOutput(
             function_definition=FunctionDefinition(
