4f76a69c13c434917e823e4bb57b6688e3f7d51f
Marcelfrueh <78954450+Marcelfrueh@users.noreply.github.com>
2024-10-28 09:59:00 +0100

    Modify imports

--- ui/projects/streampipes/shared-ui/src/public-api.ts
+++ ui/projects/streampipes/shared-ui/src/public-api.ts
@@ -41,6 +41,7 @@ export * from './lib/components/sp-table/sp-table.component';
 export * from './lib/components/warning-box/warning-box.component';
 export * from './lib/components/time-selector/time-range-selector.component';
 export * from './lib/components/time-selector/time-selector-menu/time-selector-menu.component';
+export * from './lib/components/time-selector/time-selector-menu/custom-time-range-selection/custom-time-range-selection.component';
 
 export * from './lib/models/sp-navigation.model';
 
