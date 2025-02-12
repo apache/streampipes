30d1ef312802d1a4c3588371e4748a687d918e45
Stefan Obermeier <st.obermeier@seeburger.de>
2025-02-12 16:52:54 +0100

    [#3446] Remove nginx default.conf SP 0.93.0 -> 0.95.0 migration code (#3455)
    * Remove nginx default.conf SP 0.93.0 -> 0.95.0 migration code
    
    Fixes #3446
    
    * Remove default.conf backup function

--- ui/docker-entrypoint.sh
+++ ui/docker-entrypoint.sh
@@ -19,21 +19,6 @@ if [ ! -z "$NGINX_SSL" ] && [ "$NGINX_SSL" = "true" ]; then
     rm /etc/nginx/conf.d/default.conf
     ln -s /app/nginx-confs/ssl.conf /etc/nginx/conf.d/default.conf
 elif [ ! -z "$SP_HTTP_SERVER_ADAPTER_ENDPOINT" ]; then
-
-    if [ ! -f /etc/nginx/conf.d/default.conf ]
-    then
-        DEFAULT_CONF_BACKUP="/etc/nginx/conf.d/default.conf_$(date +%s).bak"
-        echo "Create backup of old configuration $DEFAULT_CONF_BACKUP"
-        cp /etc/nginx/conf.d/default.conf $DEFAULT_CONF_BACKUP
-    fi
-
-    # Required for migration from 0.93.0 to 0.95.0. Could be removed after the release of 0.95.0.
-    if [ ! -f /etc/nginx/conf.d/default.conf.template ]
-    then
-        echo "Migrate to new nginx template"
-        cp /etc/opt/nginx/default.conf.template /etc/nginx/conf.d/default.conf.template
-    fi
-
     rm  -f /etc/nginx/conf.d/default.conf
     envsubst '\$SP_HTTP_SERVER_ADAPTER_ENDPOINT' < /etc/nginx/conf.d/default.conf.template > /etc/nginx/conf.d/default.conf
 fi
