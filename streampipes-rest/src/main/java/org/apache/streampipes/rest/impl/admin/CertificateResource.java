1289f3f174f23955cc2da1312755353b46a7e642
Dominik Riemer <dominik.riemer@gmail.com>
2025-08-07 08:22:42 +0200

    Fix checkstyle

--- streampipes-rest/src/main/java/org/apache/streampipes/rest/impl/admin/CertificateResource.java
+++ streampipes-rest/src/main/java/org/apache/streampipes/rest/impl/admin/CertificateResource.java
@@ -27,7 +27,6 @@ import org.apache.streampipes.storage.management.StorageDispatcher;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 import org.springframework.http.MediaType;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
