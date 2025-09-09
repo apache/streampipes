05d04f56156a9007e5e6fea890a3da21be182dcc
Jacqueline Höllig <j.hoellig@gmx.net>
2025-09-09 15:40:33 +0200

    mvn ran successful

--- streampipes-service-core/src/main/java/org/apache/streampipes/service/core/scheduler/DataLakeScheduler.java
+++ streampipes-service-core/src/main/java/org/apache/streampipes/service/core/scheduler/DataLakeScheduler.java
@@ -84,7 +84,7 @@ public class DataLakeScheduler {
 
             String providerType =  exportProviderSettings.providerType();
 
-            LOG.info("Write to " +System.getenv("SP_RETENTION_LOCAL_DIR"));
+            LOG.info("Write to " + System.getenv("SP_RETENTION_LOCAL_DIR"));
 
         
             IObjectStorage exportProvider = ExportProviderFactory.createExportProvider(
