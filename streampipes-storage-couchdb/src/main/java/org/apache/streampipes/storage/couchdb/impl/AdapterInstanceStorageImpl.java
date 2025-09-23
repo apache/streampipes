efd8c9c3c6c7b13770bb143970174dc53609749c
Jacqueline Höllig <j.hoellig@gmx.net>
2025-09-23 07:45:29 +0200

    eliminated logger

--- streampipes-storage-couchdb/src/main/java/org/apache/streampipes/storage/couchdb/impl/AdapterInstanceStorageImpl.java
+++ streampipes-storage-couchdb/src/main/java/org/apache/streampipes/storage/couchdb/impl/AdapterInstanceStorageImpl.java
@@ -31,8 +31,6 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import org.lightcouch.CouchDbClient;
 import org.lightcouch.View;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
