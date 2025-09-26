29cc50f47f6866a9264ba60509799242d33ce1d5
Jacqueline Höllig <j.hoellig@gmx.net>
2025-09-26 15:24:42 +0200

    clean up

--- streampipes-connect-management/src/main/java/org/apache/streampipes/connect/management/management/AdapterMasterManagement.java
+++ streampipes-connect-management/src/main/java/org/apache/streampipes/connect/management/management/AdapterMasterManagement.java
@@ -18,6 +18,9 @@
 
 package org.apache.streampipes.connect.management.management;
 
+import java.util.List;
+import java.util.NoSuchElementException;
+
 import org.apache.streampipes.commons.exceptions.NoServiceEndpointsAvailableException;
 import org.apache.streampipes.commons.exceptions.SepaParseException;
 import org.apache.streampipes.commons.exceptions.connect.AdapterException;
@@ -36,14 +39,6 @@ import org.apache.streampipes.svcdiscovery.api.model.SpServiceUrlProvider;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.FileWriter;
-import java.io.PrintWriter;
-import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.NoSuchElementException;
 
 /**
  * This class is responsible for managing all the adapter instances which are
@@ -105,48 +100,9 @@ public class AdapterMasterManagement {
   }
 
   public AdapterDescription getAdapter(String elementId) throws AdapterException {
-
-    Map<String, Long> timings = new HashMap<>();
-
-    long t1 = System.nanoTime();
     AdapterDescription allAdapters = adapterInstanceStorage.getElementById(elementId);
-    timings.put("Overall", System.nanoTime() - t1);
-    saveTimingToCSV(timings);
     return allAdapters;
   }
-
-
-  private void saveTimingToCSV(Map<String, Long> timings) {
-    String filePath = "./logs/compact_adapter_timings.csv";
-    java.io.File file = new java.io.File(filePath);
-
-    // Create logs directory if it doesn't exist
-    file.getParentFile().mkdirs();
-
-    boolean fileExists = file.exists();
-
-    try (FileWriter fw = new FileWriter(file, true);
-         PrintWriter pw = new PrintWriter(fw)) {
-
-        // Write header only if file doesn't exist or is empty
-        if (!fileExists || file.length() == 0) {
-            pw.print("timestamp");
-            for (String key : timings.keySet()) {
-                pw.print("," + key);
-            }
-            pw.println();
-        }
-
-        pw.print(LocalDateTime.now());
-        for (Long timeNs : timings.values()) {
-            pw.print("," + timeNs / 1_000_000); // convert to milliseconds
-        }
-        pw.println();
-
-    } catch (Exception e) {
-        e.printStackTrace(); // Consider using proper logging instead
-    }
-}
 
   /**
    * First the adapter is stopped removed, then the corresponding data source is
