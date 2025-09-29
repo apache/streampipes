943cedde86ef6e2f774b2159042f5822f73eda2c
Jacqueline Höllig <j.hoellig@gmx.net>
2025-09-29 13:52:32 +0200

    first working fraft

--- ui/src/app/connect/services/adapter-asset-configuration.service.ts
+++ ui/src/app/connect/services/adapter-asset-configuration.service.ts
@@ -16,7 +16,7 @@
  *
  */
 
-import { Injectable } from '@angular/core';
+import { Injectable, Output, EventEmitter } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError, map } from 'rxjs/operators';
 import {
@@ -46,75 +46,72 @@ export class AssetSaveService {
     currentAsset: SpAssetModel;
     constructor(private assetService: AssetManagementService) {}
 
+    @Output() adapterStartedEmitter: EventEmitter<void> =
+        new EventEmitter<void>();
+
     // Method to save multiple assets and handle asset linkages
     saveSelectedAssets(
         selectedAssets: Asset[],
         linkageData: LinkageData[],
-    ): Observable<any>[] {
-        const updateObservables: Observable<any>[] = [];
+    ): void {
+        //const updateObservables: Observable<any>[] = [];
         console.log('selected assets', selectedAssets);
+        console.log('linkage Data', linkageData);
 
         // Loop through each selected asset and create the observables
         selectedAssets.forEach(asset => {
-            const updateObservable = this.assetService
-                .getAsset(asset.assetId)
-                .pipe(
-                    map(current => {
-                        this.currentAsset = current; // Store the current asset
-                        const links = this.buildLinks(linkageData); // Build linkage data for this asset
+            this.assetService.getAsset(asset.id).subscribe({
+                next: current => {
+                    this.currentAsset = current;
 
-                        // Find target asset based on assetId
-                        const targetAsset = this.findAssetById(
-                            asset.assetId,
-                            this.currentAsset,
-                        );
+                    const links = this.buildLinks(linkageData);
+                    console.log(links);
+                    const targetAsset = this.findAssetById(
+                        asset.assetId,
+                        current,
+                    );
 
-                        if (!targetAsset) {
-                            console.warn('Asset not found:', asset.assetId);
-                            return null; // No need to process further if asset not found
-                        }
+                    targetAsset.assetLinks = [
+                        ...(targetAsset.assetLinks ?? []),
+                        ...links,
+                    ];
 
-                        targetAsset.assetLinks = [
-                            ...(targetAsset.assetLinks ?? []),
-                            ...links,
-                        ];
+                    console.log(targetAsset);
+                    const updateObservable = targetAsset._id
+                        ? this.assetService.updateAsset(targetAsset)
+                        : this.updateNestedAsset(targetAsset, current);
 
-                        // Prepare the observable for updating the asset
-                        return targetAsset._id
-                            ? this.assetService.updateAsset(targetAsset) // If asset has _id, update it
-                            : this.updateNestedAsset(
-                                  targetAsset,
-                                  this.currentAsset,
-                              ); // If no _id, update the nested asset
-                    }),
-                    catchError(error => {
-                        console.error('Error fetching asset:', error);
-                        return of(null); // In case of error, return an observable of null
-                    }),
-                );
+                    console.log(updateObservable);
+
+                    updateObservable?.subscribe({
+                        next: updated => {
+                            console.log('Updated ASSER', updated);
+                            this.adapterStartedEmitter.emit();
+                        },
+                    });
+                },
+            });
 
             // Push the update observable for this asset to the list
-            updateObservables.push(updateObservable);
+            //updateObservables.push(updateObservable);
         });
 
-        return updateObservables; // Return the array of observables after the loop
+        //return updateObservables; // Return the array of observables after the loop
     }
 
     // Helper function to build links based on the linkage data
     private buildLinks(data: LinkageData[]): AssetLink[] {
-        return data
-            .filter(item => item.selected)
-            .map(item => {
-                const linkType = this.getAssetLinkTypeById(item.type);
-                return {
-                    linkLabel: item.name,
-                    linkType: item.type,
-                    editingDisabled: false,
-                    queryHint: item.type,
-                    navigationActive: linkType?.navigationActive ?? false,
-                    resourceId: item.id,
-                };
-            });
+        return data.map(item => {
+            const linkType = this.getAssetLinkTypeById(item.type);
+            return {
+                linkLabel: item.name,
+                linkType: item.type,
+                editingDisabled: false,
+                queryHint: item.type,
+                navigationActive: linkType?.navigationActive ?? false,
+                resourceId: item.id,
+            };
+        });
     }
 
     // Helper function to get asset link type by ID
@@ -124,6 +121,8 @@ export class AssetSaveService {
 
     // Helper function to find the asset by ID
     private findAssetById(assetId: string, currentAsset: SpAssetModel): any {
+        console.log('assetID', assetId);
+        console.log('current', currentAsset);
         if (currentAsset?.assetId === assetId) return currentAsset;
         return this.findSubAssetById(currentAsset?.assets ?? [], assetId);
     }
@@ -138,10 +137,7 @@ export class AssetSaveService {
     }
 
     // Function to update a nested asset (if no _id is available)
-    private updateNestedAsset(
-        assetToUpdate: any,
-        currentAsset: SpAssetModel,
-    ): Observable<SpAssetModel> | null {
+    private updateNestedAsset(assetToUpdate: any, currentAsset: SpAssetModel) {
         const index = currentAsset?.assets?.findIndex(
             (asset: any) => asset.assetId === assetToUpdate.assetId,
         );
@@ -149,6 +145,9 @@ export class AssetSaveService {
         if (index === -1 || index === undefined) return null;
 
         currentAsset.assets[index] = assetToUpdate;
-        return this.assetService.updateAsset(currentAsset);
+        console.log('update asset');
+        this.assetService.updateAsset(currentAsset).subscribe(() => {
+            console.log('Asset 2');
+        });
     }
 }
