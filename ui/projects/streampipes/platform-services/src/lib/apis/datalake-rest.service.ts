cf32ecb1d27d10a1f77d47ab28c062f043d44008
Jacqueline Höllig <j.hoellig@gmx.net>
2025-10-07 16:58:41 +0200

    removed console log

--- ui/projects/streampipes/platform-services/src/lib/apis/datalake-rest.service.ts
+++ ui/projects/streampipes/platform-services/src/lib/apis/datalake-rest.service.ts
@@ -163,7 +163,6 @@ export class DatalakeRestService {
 
     cleanup(index: string, config: any) {
         const url = `${this.dataLakeUrl}/${index}/cleanup`;
-        console.log(config);
         const request = new HttpRequest('POST', url, config, {
             headers: new HttpHeaders({ 'Content-Type': 'application/json' }), // optional if already handled globally
         });
