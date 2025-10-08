990ca1b5a9b43d7bc63c40775763ef58795ccdb2
Jacqueline Höllig <j.hoellig@gmx.net>
2025-10-08 17:43:02 +0200

    delete function

--- streampipes-rest/src/main/java/org/apache/streampipes/rest/impl/admin/ExportProviderConfigurationResource.java
+++ streampipes-rest/src/main/java/org/apache/streampipes/rest/impl/admin/ExportProviderConfigurationResource.java
@@ -22,11 +22,14 @@ import org.apache.streampipes.model.configuration.ExportProviderSettings;
 import org.apache.streampipes.rest.core.base.impl.AbstractAuthGuardedRestResource;
 import org.apache.streampipes.rest.security.AuthConstants;
 import org.apache.streampipes.user.management.encryption.SecretEncryptionManager;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -34,11 +37,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/v2/admin/exportprovider-config")
 public class ExportProviderConfigurationResource extends AbstractAuthGuardedRestResource {
 
+    private static final Logger LOG = LoggerFactory.getLogger(ExportProviderConfigurationResource.class);
 
   @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
   @PreAuthorize(AuthConstants.IS_ADMIN_ROLE)
@@ -50,6 +55,8 @@ public class ExportProviderConfigurationResource extends AbstractAuthGuardedRest
   @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
   @PreAuthorize(AuthConstants.IS_ADMIN_ROLE)
   public ResponseEntity<Void> updateExportProviderConfiguration(@RequestBody ExportProviderSettings config) {
+
+    LOG.info("PUT");
   
     if (!config.isSecretEncrypted()) {
         config.setSecretKey(SecretEncryptionManager.encrypt(config.getSecretKey()));
@@ -74,14 +81,34 @@ public class ExportProviderConfigurationResource extends AbstractAuthGuardedRest
             break;
         }
     }
-
+    LOG.info("Updated" + updated);
     if (!updated) {
         providerSettings.add(config);
     }
+    LOG.info("Provider Settings" + providerSettings.size());
 
     cfg.setExportProviderSettings(providerSettings);
     storage.updateElement(cfg);
 
     return ok();
   }
+
+
+  @DeleteMapping(value = "/{providerId}", produces = MediaType.APPLICATION_JSON_VALUE)
+  @PreAuthorize(AuthConstants.IS_ADMIN_ROLE)
+  public ResponseEntity<Void> deleteExportProviderConfiguration(@PathVariable String providerId) {
+
+    List<ExportProviderSettings> allProviders = getSpCoreConfigurationStorage().get().getExportProviderSettings();
+
+    List<ExportProviderSettings> filteredProviders = allProviders.stream()
+        .filter(provider -> !provider.getProviderId().equals(providerId))
+        .collect(Collectors.toList());
+
+    var storage = getSpCoreConfigurationStorage();
+    var cfg = storage.get();
+    cfg.setExportProviderSettings(filteredProviders);
+    storage.updateElement(cfg);    
+    return ok();
+  }
+
 }
