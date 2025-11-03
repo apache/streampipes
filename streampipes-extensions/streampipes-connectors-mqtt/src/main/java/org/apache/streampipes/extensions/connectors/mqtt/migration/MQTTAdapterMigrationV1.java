a80f3c7a26897d3354e384b2193ad446485690d8
Jacqueline Höllig <j.hoellig@gmx.net>
2025-11-03 11:03:02 +0100

    cleanup

--- streampipes-extensions/streampipes-connectors-mqtt/src/main/java/org/apache/streampipes/extensions/connectors/mqtt/migration/MQTTAdapterMigrationV1.java
+++ streampipes-extensions/streampipes-connectors-mqtt/src/main/java/org/apache/streampipes/extensions/connectors/mqtt/migration/MQTTAdapterMigrationV1.java
@@ -1,10 +1,5 @@
 package org.apache.streampipes.extensions.connectors.mqtt.migration;
 
-import static org.apache.streampipes.extensions.connectors.mqtt.shared.MqttConnectUtils.getBrokerUrlLabel;
-
-import java.util.Arrays;
-import java.util.List;
-
 import org.apache.streampipes.extensions.api.extractor.IStaticPropertyExtractor;
 import org.apache.streampipes.extensions.api.migration.IAdapterMigrator;
 import org.apache.streampipes.extensions.connectors.mqtt.adapter.MqttProtocol;
@@ -19,9 +14,10 @@ import org.apache.streampipes.model.staticproperty.StaticPropertyAlternative;
 import org.apache.streampipes.model.staticproperty.StaticPropertyAlternatives;
 import org.apache.streampipes.sdk.StaticProperties;
 
+import java.util.List;
 
 public class MQTTAdapterMigrationV1 implements IAdapterMigrator {
-    
+
     @Override
     public ModelMigratorConfig config() {
         return new ModelMigratorConfig(
@@ -33,32 +29,32 @@ public class MQTTAdapterMigrationV1 implements IAdapterMigrator {
     }
 
     @Override
-  public MigrationResult<AdapterDescription> migrate(AdapterDescription element,
-                                                     IStaticPropertyExtractor extractor) throws RuntimeException {
+    public MigrationResult<AdapterDescription> migrate(AdapterDescription element,
+            IStaticPropertyExtractor extractor) throws RuntimeException {
 
-var url = (FreeTextStaticProperty) element.getConfig().get(0);
-url.setDescription("Example: tcp://test-server.com:1883 (Protocol required. Port required), with TLS ssl://test-server.com:8883 (Protocol required. Port required)");
-element.getConfig().set(0, url);
+        var url = (FreeTextStaticProperty) element.getConfig().get(0);
+        url.setDescription(
+                "Example: tcp://test-server.com:1883 (Protocol required. Port required), with TLS ssl://test-server.com:8883 (Protocol required. Port required)");
+        element.getConfig().set(0, url);
 
-migrateSecurity((StaticPropertyAlternatives) element.getConfig().get(1));
+        migrateSecurity((StaticPropertyAlternatives) element.getConfig().get(1));
 
-element.getConfig().add(2, makeTLS());
-       return MigrationResult.success(element);
-  }
+        element.getConfig().add(2, makeTLS());
+        return MigrationResult.success(element);
+    }
 
-private SlideToggleStaticProperty makeTLS() {
+    private SlideToggleStaticProperty makeTLS() {
         var tlsAlternative = StaticProperties.toggleAlternative(MqttConnectUtils.getTLS(), false);
         return tlsAlternative;
     }
 
-public void migrateSecurity(StaticPropertyAlternatives securityAlternatives) {
-    migrateGroup(securityAlternatives.getAlternatives());
-  }
+    public void migrateSecurity(StaticPropertyAlternatives securityAlternatives) {
+        migrateGroup(securityAlternatives.getAlternatives());
+    }
 
-  private void migrateGroup(List<StaticPropertyAlternative> alternatives) {
-    alternatives.add(MqttConnectUtils.getAlternativesThree());
-    
-  }
+    private void migrateGroup(List<StaticPropertyAlternative> alternatives) {
+        alternatives.add(MqttConnectUtils.getAlternativesThree());
 
+    }
 
 }
