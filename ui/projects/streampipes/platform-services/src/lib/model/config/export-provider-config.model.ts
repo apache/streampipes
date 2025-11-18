7314a590a6498f7d334aeae97bde46c9ec743c7f
Jacqueline Höllig <j.hoellig@gmx.net>
2025-11-18 10:36:05 +0100

    moved model

--- ui/projects/streampipes/platform-services/src/lib/model/config/export-provider-config.model.ts
+++ ui/projects/streampipes/platform-services/src/lib/model/config/export-provider-config.model.ts
@@ -1,5 +1,3 @@
-import { ExportProviderSettings } from '@streampipes/platform-services';
-
 /*
  * Licensed to the Apache Software Foundation (ASF) under one or more
  * contributor license agreements.  See the NOTICE file distributed with
@@ -17,6 +15,8 @@ import { ExportProviderSettings } from '@streampipes/platform-services';
  * limitations under the License.
  *
  */
+
+import { ExportProviderSettings } from '../gen/streampipes-model';
 export interface ExportProviderResponse {
     filePath: string;
     setting: ExportProviderSettings;
