d95cc923b3aa9ca2279b9422c520f9a635d9d15d
Dominik Riemer <dominik.riemer@gmail.com>
2025-11-24 19:50:10 +0100

    fix: Update default roles and privileges

--- streampipes-service-core/src/main/java/org/apache/streampipes/service/core/migrations/AvailableMigrations.java
+++ streampipes-service-core/src/main/java/org/apache/streampipes/service/core/migrations/AvailableMigrations.java
@@ -34,6 +34,7 @@ import org.apache.streampipes.service.core.migrations.v0980.ModifyAssetLinksMigr
 import org.apache.streampipes.service.core.migrations.v099.AddAssetManagementViewMigration;
 import org.apache.streampipes.service.core.migrations.v099.CreateAssetPermissionMigration;
 import org.apache.streampipes.service.core.migrations.v099.MoveAssetContentMigration;
+import org.apache.streampipes.service.core.migrations.v099.RemoveObsoletePrivilegesMigration;
 import org.apache.streampipes.service.core.migrations.v970.AddDataLakePipelineTemplateMigration;
 import org.apache.streampipes.service.core.migrations.v970.AddLinkSettingsMigration;
 import org.apache.streampipes.service.core.migrations.v970.AddRolesToUserDbMigration;
@@ -68,7 +69,8 @@ public class AvailableMigrations {
         new FixImportedPermissionsMigration(),
         new AddAssetManagementViewMigration(),
         new MoveAssetContentMigration(),
-        new CreateAssetPermissionMigration()
+        new CreateAssetPermissionMigration(),
+        new RemoveObsoletePrivilegesMigration()
     );
   }
 }
