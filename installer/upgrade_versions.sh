2a6c65e801f812ebe56f6b58afec32e02638b204
Philipp Zehnder <tenthe@users.noreply.github.com>
2025-11-28 09:28:52 +0100

    feat: Update version in installer directory

--- installer/upgrade_versions.sh
+++ installer/upgrade_versions.sh
@@ -14,9 +14,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-NEW_VERSION=$1
+NEW_VERSION=$2
 # get old version from ./cli/.env
-OLD_VERSION=$(grep -oP '^SP_VERSION=\K\S*' ./cli/.env)
+OLD_VERSION=$1
 
 FILES=(
   # cli
