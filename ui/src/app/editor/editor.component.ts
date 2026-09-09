432b43f2db4210b70df1c26b9fe4c5590b85aba7
Sven Oehler <oehler.sven@web.de>
2026-09-09 17:55:12 +0200

    Rename 'Clone' to 'Create from existing' for pipelines

--- ui/src/app/editor/editor.component.ts
+++ ui/src/app/editor/editor.component.ts
@@ -156,7 +156,11 @@ export class EditorComponent implements OnInit {
                     this.breadcrumbService.updateBreadcrumb([
                         SpPipelineRoutes.BASE,
                         { label: this.originalPipeline.name },
-                        { label: this.cloneMode ? 'Clone' : 'Modify' },
+                        {
+                            label: this.cloneMode
+                                ? 'Create from existing'
+                                : 'Modify',
+                        },
                     ]);
                     this.rawPipelineModel = this.jsplumbService.makeRawPipeline(
                         this.originalPipeline,
