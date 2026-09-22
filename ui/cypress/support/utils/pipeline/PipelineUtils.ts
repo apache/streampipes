404a1987e3804693e45bc766046d6f9435b6ec59
Philipp Zehnder <tenthe@users.noreply.github.com>
2026-09-22 16:45:58 +0200

    test: wait for customize dialog close before save (#5018)

--- ui/cypress/support/utils/pipeline/PipelineUtils.ts
+++ ui/cypress/support/utils/pipeline/PipelineUtils.ts
@@ -124,8 +124,14 @@ export class PipelineUtils {
         OutputStrategyUtils.input(processingElement.output);
     }
 
+    /**
+     * Saves the element configuration and waits until the customize dialog
+     * is removed from the DOM. The dialog slides out with an animation and
+     * would otherwise cover the pipeline save button for the next click.
+     */
     private static savePipelineElementConfiguration() {
         PipelineBtns.saveElementConfigBtn().click();
+        PipelineBtns.saveElementConfigBtn().should('not.exist');
     }
 
     private static configurePipeline(pipelineInput: PipelineInput) {
@@ -150,7 +156,7 @@ export class PipelineUtils {
         StaticPropertyUtils.input(pipelineInput.dataSink.config);
 
         // Save sink configuration
-        PipelineBtns.saveElementConfigBtn().click();
+        this.savePipelineElementConfiguration();
     }
 
     public static startPipeline(pipelineInput?: PipelineInput) {
